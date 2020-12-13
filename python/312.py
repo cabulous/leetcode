dad9b73b5f04898dae0153be1b816ca6fb77eb11
Carson <dragonchen66@gmail.com>
2020-12-13 13:32:23 -0500

    practice: daily challenge

---
+++ python/312.py
@@ -0,0 +1,33 @@
+from functools import lru_cache
+
+
+# DP top-down
+class Solution:
+    def maxCoins(self, nums: [int]) -> int:
+        nums = [1] + nums + [1]
+
+        @lru_cache(None)
+        def dp(left, right):
+            if left + 1 == right:
+                return 0
+            return max(
+                nums[left] * nums[i] * nums[right] + dp(left, i) + dp(i, right) for i in range(left + 1, right)
+            )
+
+        return dp(0, len(nums) - 1)
+
+
+# DP bottom-up
+class Solution:
+    def maxCoins(self, nums: [int]) -> int:
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+
+        for left in range(n - 2, -1, -1):
+            for right in range(left + 2, n):
+                dp[left][right] = max(
+                    nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right] for i in range(left + 1, right)
+                )
+
+        return dp[0][n - 1]
