5b8e1e8f4c56d7fd2f0e7898d6f5d28c1b092548
Carson <dragonchen66@gmail.com>
2020-12-24 10:20:50 -0500

    practice: daily challenge and revisit

--- java/nextGreaterElement.java
+++ java/nextGreaterElement.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class nextGreaterElement {
+// 556. Next Greater Element III
+// https://leetcode.com/problems/next-greater-element-iii/
+
+public class Solution {
+    public int nextGreaterElement(int n) {
+        char[] a = ("" + n).toCharArray();
+        int i = a.length - 2;
+        while (i >= 0 && a[i + 1] <= a[i]) {
+            i--;
+        }
+        if (i < 0) {
+            return -1;
+        }
+        int j = a.length - 1;
+        while (j >= 0 && a[j] <= a[i]) {
+            j--;
+        }
+
+        swap(a, i, j);
+        reverse(a, i + 1);
+
+        try {
+            return Integer.parseInt(new String(a));
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private void swap(char[] a, int i, int j) {
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private void reverse(char[] a, int start) {
+        int i = start;
+        int j = a.length - 1;
+        while (i < j) {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+    }
 }
