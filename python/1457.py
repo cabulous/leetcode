b151ac1ecaa5fd1ad64eb456fa1c59d5249a8ec2
Carson <dragonchen66@gmail.com>
2020-12-29 10:50:42 -0500

    practice: daily challenge and revisit

---
+++ python/1457.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pseudoPalindromicPaths(self, root) -> int:
+        count = 0
+        stack = [(root, 0)]
+        while stack:
+            node, path = stack.pop()
+            if node:
+                path ^= (1 << node.val)
+                if not node.left and not node.right:
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:
+                    stack.append((node.left, path))
+                    stack.append((node.right, path))
+        return count
