e32e57e3864ada81c2300d34c17e8f592b09ca6d
Carson <dragonchen66@gmail.com>
2021-01-12 11:09:08 -0500

    practice: daily challenge and revisit

--- python/2.py
+++ python/2.py
@@ -10,7 +10,7 @@ class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         carry = 0
         sentinel = ListNode(0)
-        n = sentinel
+        cur = sentinel
         while l1 or l2 or carry:
             if l1:
                 carry += l1.val
@@ -19,5 +19,5 @@ class Solution:
                 carry += l2.val
                 l2 = l2.next
             carry, val = divmod(carry, 10)
-            n.next = n = ListNode(val)
+            cur.next = cur = ListNode(val)
         return sentinel.next
