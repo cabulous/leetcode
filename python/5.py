f3e2c816c93b47058db2c533e1d26d99995ae685
Carson <dragonchen66@gmail.com>
2021-01-19 10:48:08 -0500

    practice: daily challenge and revisit

--- python/5.py
+++ python/5.py
@@ -1,31 +1,32 @@
 # https://leetcode.com/problems/longest-palindromic-substring/discuss/2954/Python-easy-to-understand-solution-with-comments-(from-middle-to-two-ends).
 class Solution:
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return s[l + 1:r]
-
     def longestPalindrome(self, s: str) -> str:
+        def helper(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l + 1:r]
+
+        n = len(s)
         res = ""
-        for i in range(len(s)):
-            res = max(self.helper(s, i, i), self.helper(s, i, i + 1), res, key=len)
+        for i in range(n):
+            res = max(helper(i, i), helper(i, i + 1), res, key=len)
         return res
 
 
 # dp
-# https://leetcode.com/problems/longest-palindromic-substring/discuss/121496/Python-DP-solution
+# https://leetcode.com/problems/longest-palindromic-substring/discuss/121496/Python-DP-solution/194444
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
         dp = [[0] * n for _ in range(n)]
-        ans = ""
+        res = ""
         maxi = 0
         for i in range(n - 1, -1, -1):
             for j in range(i, n):
                 if s[i] == s[j] and (j - i < 3 or dp[i + 1][j - 1] == 1):
                     dp[i][j] = 1
-                    if ans == "" or maxi < j - i + 1:
-                        ans = s[i:j + 1]
+                    if res == "" or maxi < j - i + 1:
                         maxi = j - i + 1
-        return ans
+                        res = s[i:j + 1]
+        return res
