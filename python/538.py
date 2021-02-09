aa1339aac990d7a6b87be209f6d52a1e448debbf
Carson <dragonchen66@gmail.com>
2021-02-09 10:50:54 -0500

    practice: daily challenge and revisit

---
+++ python/538.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# dfs
+class Solution:
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
+
+
+# dfs + stack
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        total = 0
+        node = root
+        stack = []
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.right
+            node = stack.pop()
+            total += node.val
+            node.val = total
+            node = node.left
+        return root
