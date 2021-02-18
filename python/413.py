e0d6d2fa1617721e78287067a9be9aba8b212a8e
Carson <dragonchen66@gmail.com>
2021-02-18 10:50:34 -0500

    practice: daily challenge and revisit

---
+++ python/413.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+# https://leetcode.com/problems/arithmetic-slices/discuss/90112/Python-DP-solution/94658
+class Solution:
+    def numberOfArithmeticSlices(self, A: List[int]) -> int:
+        curr, sum = 0, 0
+        for i in range(2, len(A)):
+            if A[i] - A[i - 1] == A[i - 1] - A[i - 2]:
+                curr += 1
+                sum += curr
+            else:
+                curr = 0
+        return sum
