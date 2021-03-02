db90a30776de4589336e37b2cdf86ce02c13cf8e
Carson <dragonchen66@gmail.com>
2021-03-02 10:29:11 -0500

    practice: daily challenge and revisit

---
+++ python/645.py
@@ -0,0 +1,7 @@
+from typing import List
+
+# https://leetcode.com/problems/set-mismatch/discuss/105558/Oneliner-Python
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        unique_sum = sum(set(nums))
+        return [sum(nums) - unique_sum, sum(range(1, len(nums) + 1)) - unique_sum]
