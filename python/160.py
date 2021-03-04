16ccda199873879da880c6332776b232c8c172e4
Carson <dragonchen66@gmail.com>
2021-03-04 10:41:41 -0500

    practice: daily challenge and revisit

---
+++ python/160.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# two pointer
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pa, pb = headA, headB
+        while pa != pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+        return pa
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        seen = set()
+        while headB:
+            seen.add(headB)
+            headB = headB.next
+        while headA:
+            if headA in seen:
+                return headA
+            headA = headA.next
+        return None
