c650875c14a80df7955642bc58023287d6ecc2a2
Carson <dragonchen66@gmail.com>
2021-03-24 10:06:35 -0400

    practice: daily challenge and revisit

---
+++ python/870.py
@@ -0,0 +1,13 @@
+from typing import List
+from collections import defaultdict
+
+
+# https://leetcode.com/problems/advantage-shuffle/discuss/149842/Python-Greedy-Solution-Using-Sort
+class Solution:
+    def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
+        A = sorted(A)
+        take = defaultdict(list)
+        for b in sorted(B)[::-1]:
+            if b < A[-1]:
+                take[b].append(A.pop())
+        return [(take[b] or A).pop() for b in B]
