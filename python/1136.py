97f1dcf5c3db8527bbf3f688b1ea66dacd9634c6
Carson <dragonchen66@gmail.com>
2021-03-30 10:21:04 -0400

    practice: daily challenge and revisit

--- python/1136.py
+++ python/1136.py
@@ -1,6 +1,39 @@
 from typing import List
 
 
+# bfs
+class Solution:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        graph = {i: [] for i in range(1, n + 1)}
+        in_degree = {i: 0 for i in range(1, n + 1)}
+
+        for start_node, end_node in relations:
+            graph[start_node].append(end_node)
+            in_degree[end_node] += 1
+
+        queue = []
+
+        for node in graph:
+            if in_degree[node] == 0:
+                queue.append(node)
+
+        step = 0
+        studied_count = 0
+
+        while queue:
+            step += 1
+            next_queue = []
+            for node in queue:
+                studied_count += 1
+                for end_node in graph[node]:
+                    in_degree[end_node] -= 1
+                    if in_degree[end_node] == 0:
+                        next_queue.append(end_node)
+            queue = next_queue
+
+        return step if studied_count == n else -1
+
+
 # dfs
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
@@ -33,36 +66,3 @@ class Solution:
             max_length = max(max_length, length)
 
         return max_length
-
-
-# bfs
-class Solution:
-    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        graph = {i: [] for i in range(1, n + 1)}
-        in_degree = {i: 0 for i in range(1, n + 1)}
-
-        for start_node, end_node in relations:
-            graph[start_node].append(end_node)
-            in_degree[end_node] += 1
-
-        queue = []
-
-        for node in graph:
-            if in_degree[node] == 0:
-                queue.append(node)
-
-        step = 0
-        studied_count = 0
-
-        while queue:
-            step += 1
-            next_queue = []
-            for node in queue:
-                studied_count += 1
-                for end_node in graph[node]:
-                    in_degree[end_node] -= 1
-                    if in_degree[end_node] == 0:
-                        next_queue.append(end_node)
-            queue = next_queue
-
-        return step if studied_count == n else -1
---
+++ python/354.py
@@ -0,0 +1,19 @@
+from bisect import bisect_left
+from typing import List
+
+
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+
+        def lis(nums):
+            dp = []
+            for i in range(len(nums)):
+                idx = bisect_left(dp, nums[i])
+                if idx == len(dp):
+                    dp.append(nums[i])
+                else:
+                    dp[idx] = nums[i]
+            return len(dp)
+
+        return lis([i[1] for i in envelopes])
