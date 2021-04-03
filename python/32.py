13a05607464e61b5077681b484e874ef0f7f8d12
Carson <dragonchen66@gmail.com>
2021-04-03 10:12:32 -0400

    practice: daily challenge and revisit

--- python/32.py
+++ python/32.py
@@ -6,17 +6,17 @@ class Solution:
 
         n = len(s)
         dp = [0] * n
-        max_ans = 0
+        res = 0
 
         for i in range(1, n):
             if s[i] == ')':
-                if s[i - 1] == '(':
-                    dp[i] = 2 + (dp[i - 2] if i >= 2 else 0)
-                elif i - dp[i - 1] >= 1 and s[i - dp[i - 1] - 1] == '(':
-                    dp[i] = 2 + dp[i - 1] + (dp[i - dp[i - 1] - 2] if i - dp[i - 1] >= 2 else 0)
-                max_ans = max(max_ans, dp[i])
+                if i - 1 >= 0 and s[i - 1] == '(':
+                    dp[i] = 2 + (dp[i - 2] if i - 2 >= 0 else 0)
+                elif i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(':
+                    dp[i] = 2 + dp[i - 1] + (dp[i - dp[i - 1] - 2] if i - dp[i - 1] - 2 >= 0 else 0)
+                res = max(res, dp[i])
 
-        return max_ans
+        return res
 
 
 # stack
@@ -25,18 +25,18 @@ class Solution:
         if not s:
             return 0
         n = len(s)
-        max_ans = 0
         stack = [-1]
+        res = 0
         for i in range(n):
             if s[i] == '(':
                 stack.append(i)
             else:
                 stack.pop()
                 if stack:
-                    max_ans = max(max_ans, i - stack[-1])
+                    res = max(res, i - stack[-1])
                 else:
                     stack.append(i)
-        return max_ans
+        return res
 
 
 # left + right scan
@@ -44,26 +44,26 @@ class Solution:
     def longestValidParentheses(self, s: str) -> int:
         if not s:
             return 0
-        max_ans = 0
-        left, right = 0, 0
         n = len(s)
+        res = 0
+        left = right = 0
         for i in range(n):
             if s[i] == '(':
                 left += 1
             else:
                 right += 1
             if left == right:
-                max_ans = max(max_ans, 2 * right)
+                res = max(res, right * 2)
             elif right > left:
-                left, right = 0, 0
-        left, right = 0, 0
+                left = right = 0
+        left = right = 0
         for i in reversed(range(n)):
             if s[i] == '(':
                 left += 1
             else:
                 right += 1
             if left == right:
-                max_ans = max(max_ans, 2 * right)
+                res = max(res, left * 2)
             elif left > right:
-                left, right = 0, 0
-        return max_ans
+                left = right = 0
+        return res
