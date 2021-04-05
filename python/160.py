88593e040683ee66f4236419aa59ebb10819b9b6
Carson <dragonchen66@gmail.com>
2021-04-05 10:48:45 -0400

    practice: daily challenge and revisit

--- python/160.py
+++ python/160.py
@@ -10,8 +10,8 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         pa, pb = headA, headB
         while pa != pb:
-            pa = headB if pa is None else pa.next
-            pb = headA if pb is None else pb.next
+            pa = pa.next if pa else headB
+            pb = pb.next if pb else headA
         return pa
 
 
