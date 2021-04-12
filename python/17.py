64ef6dd38748b108435f94ae59347d4f6107e41f
Carson <dragonchen66@gmail.com>
2021-04-12 10:18:23 -0400

    practice: daily challenge and revisit

--- python/17.py
+++ python/17.py
@@ -4,11 +4,16 @@ from typing import List
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/discuss/8063/Python-solution/284974
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+
         letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
                    "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-        cmb = [''] if digits else []
+        cmb = ['']
+
         for d in digits:
             cmb = [p + q for p in cmb for q in letters[d]]
+
         return cmb
 
 
@@ -26,8 +31,7 @@ class Solution:
             if len(path) == len(digits):
                 cmb.append(''.join(path))
                 return
-            possible_letters = letters[digits[index]]
-            for letter in possible_letters:
+            for letter in letters[digits[index]]:
                 path.append(letter)
                 backtrack(index + 1, path)
                 path.pop()
