a039adaeef08400192c28cae7d019cf3c5f8e4ec
Carson <dragonchen66@gmail.com>
2021-04-16 09:54:26 -0400

    practice: daily challenge and revisit

--- python/535.py
+++ python/535.py
@@ -4,8 +4,9 @@ import string
 
 # https://leetcode.com/problems/encode-and-decode-tinyurl/discuss/100268/Two-solutions-and-thoughts
 class Codec:
-    alphabet = string.ascii_letters + '0123456789'
     prefix = 'http://tinyurl.com/'
+    alphabet = string.ascii_letters + '0123456789'
+    code_len = 6
 
     def __init__(self):
         self.url2code = {}
@@ -13,11 +14,11 @@ class Codec:
 
     def encode(self, longUrl: str) -> str:
         while longUrl not in self.url2code:
-            code = ''.join(random.choice(Codec.alphabet) for _ in range(6))
+            code = ''.join(random.choice(Codec.alphabet) for _ in range(Codec.code_len))
             if code not in self.code2url:
                 self.code2url[code] = longUrl
                 self.url2code[longUrl] = code
         return Codec.prefix + self.url2code[longUrl]
 
     def decode(self, shortUrl: str) -> str:
-        return self.code2url[shortUrl[-6:]]
+        return self.code2url[shortUrl[-Codec.code_len:]]
