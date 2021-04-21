ecc0301f26efdbcc30088e9c393fd7e870691ebc
Carson <dragonchen66@gmail.com>
2021-04-21 09:53:03 -0400

    practice: daily challenge and revisit

--- python/1074.py
+++ python/1074.py
@@ -6,10 +6,13 @@ class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
         r, c = len(matrix), len(matrix[0])
         ps = [[0] * (c + 1) for _ in range(r + 1)]
+
         for i in range(1, r + 1):
             for j in range(1, c + 1):
                 ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + matrix[i - 1][j - 1]
+
         count = 0
+
         for r1 in range(1, r + 1):
             for r2 in range(r1, r + 1):
                 h = defaultdict(int)
@@ -18,4 +21,5 @@ class Solution:
                     curr_sum = ps[r2][col] - ps[r1 - 1][col]
                     count += h[curr_sum - target]
                     h[curr_sum] += 1
+
         return count
