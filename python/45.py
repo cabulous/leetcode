749c09f4002716b2f65f6c2df501c346418ded20
Carson <dragonchen66@gmail.com>
2021-05-05 10:10:01 -0400

    practice: daily challenge and revisit

---
+++ python/45.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+# https://leetcode.com/problems/jump-game-ii/discuss/170518/8-Lines-in-Python!-Easiest-Solution!
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return 0
+        l, r = 0, nums[0]
+        times = 1
+        while r < len(nums) - 1:
+            times += 1
+            nxt = max(i + nums[i] for i in range(l, r + 1))
+            l, r = r + 1, nxt
+        return times
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        curr_end = 0
+        farthest = 0
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == curr_end:
+                jumps += 1
+                curr_end = farthest
+        return jumps
