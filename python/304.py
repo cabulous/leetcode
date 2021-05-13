24a1954489124dc1374b6238a6b2354380beddf8
Carson <dragonchen66@gmail.com>
2021-05-13 10:13:21 -0400

    practice: daily challenge and revisit

--- python/304.py
+++ python/304.py
@@ -10,7 +10,7 @@ class NumMatrix:
         self.sum = [[0] * (n + 1) for _ in range(m + 1)]
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                self.sum[i][j] = matrix[i - 1][j - 1] + self.sum[i - 1][j] + self.sum[i][j - 1] - self.sum[i - 1][j - 1]
+                self.sum[i][j] = self.sum[i - 1][j] + self.sum[i][j - 1] - self.sum[i - 1][j - 1] + matrix[i - 1][j - 1]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         row1, col1 = row1 + 1, col1 + 1
---
+++ python/816.py
@@ -0,0 +1,36 @@
+import itertools
+from typing import List
+
+
+# https://leetcode.com/problems/ambiguous-coordinates/discuss/123851/C%2B%2BJavaPython-Solution-with-Explanation
+class Solution:
+    def ambiguousCoordinates(self, s: str) -> List[str]:
+        s = s[1:-1]
+
+        def find(s):
+            if not s or len(s) > 1 and s[0] == s[-1] == '0':
+                return []
+            if s[-1] == '0':
+                return [s]
+            if s[0] == '0':
+                return [s[0] + '.' + s[1:]]
+            return [s] + [s[:i] + '.' + s[i:] for i in range(1, len(s))]
+
+        return ['(%s, %s)' % (a, b) for i in range(len(s)) for a, b in itertools.product(find(s[:i]), find(s[i:]))]
+
+
+# Cartesian Product
+class Solution:
+    def ambiguousCoordinates(self, s: str) -> List[str]:
+        def make(frag):
+            n = len(frag)
+            for d in range(1, n + 1):
+                left = frag[:d]
+                right = frag[d:]
+                if (not left.startswith('0') or left == '0') and (not right.endswith('0')):
+                    yield left + ('.' if d != n else '') + right
+
+        s = s[1:-1]
+        return ['({}, {})'.format(*cand)
+                for i in range(1, len(s))
+                for cand in itertools.product(make(s[:i]), make(s[i:]))]
