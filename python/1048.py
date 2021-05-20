de504a6f10848f8e16187adda3b380a42e0a113f
Carson <dragonchen66@gmail.com>
2021-05-20 09:53:14 -0400

    practice: daily challenge and revisit

--- python/1048.py
+++ python/1048.py
@@ -6,5 +6,5 @@ class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         dp = {}
         for w in sorted(words, key=len):
-            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))
+            dp[w] = max(1 + dp.get(w[:i] + w[i + 1:], 0) for i in range(len(w)))
         return max(dp.values())
