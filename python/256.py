cae58bcff7fa980ca1f82e1d906975faf5647c15
Carson <dragonchen66@gmail.com>
2021-06-02 10:13:39 -0400

    practice: daily challenge and revisit

--- python/256.py
+++ python/256.py
@@ -18,18 +18,18 @@ class Solution:
 
     @lru_cache(maxsize=None)
     def paint_cost(self, house_idx, color):
-        total_cost = self.costs[house_idx][color]
+        cost = self.costs[house_idx][color]
 
         if house_idx == len(self.costs) - 1:
             pass
         elif color == 0:
-            total_cost += min(self.paint_cost(house_idx + 1, 1), self.paint_cost(house_idx + 1, 2))
+            cost += min(self.paint_cost(house_idx + 1, 1), self.paint_cost(house_idx + 1, 2))
         elif color == 1:
-            total_cost += min(self.paint_cost(house_idx + 1, 0), self.paint_cost(house_idx + 1, 2))
+            cost += min(self.paint_cost(house_idx + 1, 0), self.paint_cost(house_idx + 1, 2))
         elif color == 2:
-            total_cost += min(self.paint_cost(house_idx + 1, 0), self.paint_cost(house_idx + 1, 1))
+            cost += min(self.paint_cost(house_idx + 1, 0), self.paint_cost(house_idx + 1, 1))
 
-        return total_cost
+        return cost
 
 
 # dp
@@ -51,16 +51,16 @@ class Solution:
         if not costs:
             return 0
 
-        prev_row = costs[-1]
+        prev = costs[-1]
 
         for n in reversed(range(len(costs) - 1)):
-            curr_row = copy.deepcopy(costs[n])
-            curr_row[0] += min(prev_row[1], prev_row[2])
-            curr_row[1] += min(prev_row[0], prev_row[2])
-            curr_row[2] += min(prev_row[0], prev_row[1])
-            prev_row = curr_row
+            curr = copy.deepcopy(costs[n])
+            curr[0] += min(prev[1], prev[2])
+            curr[1] += min(prev[0], prev[2])
+            curr[2] += min(prev[0], prev[1])
+            prev = curr
 
-        return min(prev_row)
+        return min(prev)
 
 
 # https://leetcode.com/problems/paint-house/discuss/68209/1%2B-lines-Ruby-Python
--- python/695.py
+++ python/695.py
@@ -11,9 +11,13 @@ class Solution:
         max_row, max_col = len(grid), len(grid[0])
 
         def dfs(row, col):
-            if 0 <= row < max_row and 0 <= col < max_col and (row, col) not in seen and grid[row][col] == 1:
-                seen.add((row, col))
-                return 1 + dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
-            return 0
+            if row < 0 or row >= max_row or col < 0 or col >= max_col:
+                return 0
+            if (row, col) in seen:
+                return 0
+            if grid[row][col] == 0:
+                return 0
+            seen.add((row, col))
+            return 1 + dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
 
         return max(dfs(row, col) for row in range(max_row) for col in range(max_col))
