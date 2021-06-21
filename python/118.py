3e79f15bd60c82850a74ecd20c451a6ac720ccbe
Carson <dragonchen66@gmail.com>
2021-06-21 09:31:25 -0400

    practice: daily challenge and revisit

---
+++ python/118.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+
+        for row_num in range(numRows):
+            row = [None for _ in range(row_num + 1)]
+            row[0], row[-1] = 1, 1
+            for col in range(1, len(row) - 1):
+                row[col] = triangle[row_num - 1][col - 1] + triangle[row_num - 1][col]
+            triangle.append(row)
+
+        return triangle
