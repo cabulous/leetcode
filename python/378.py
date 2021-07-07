c14fbd6d717766fedbebd54e8876ee45db27faf9
Carson <dragonchen66@gmail.com>
2021-07-07 09:38:17 -0400

    practice: daily challenge and revisit

---
+++ python/378.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        heap = []
+        res = 0
+
+        for r in range(min(k, n)):
+            heapq.heappush(heap, (matrix[r][0], r, 0))
+
+        while k > 0:
+            res, r, c = heapq.heappop(heap)
+            if c < n - 1:
+                heapq.heappush(heap, (matrix[r][c + 1], r, c + 1))
+            k -= 1
+
+        return res
