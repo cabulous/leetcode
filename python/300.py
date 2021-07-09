f8f20bfd3ed478164e473071243014fdc08097fa
Carson <dragonchen66@gmail.com>
2021-07-09 10:13:14 -0400

    practice: daily challenge and revisit

--- python/300.py
+++ python/300.py
@@ -1,40 +1,29 @@
 # DP
+import bisect
+
+
 class Solution:
     def lengthOfLIS(self, nums):
-        if not nums:
-            return 0
-        m = len(nums)
-        dp = [0] * m
-        dp[0] = 1
-        ans = 1
-        for i in range(1, m):
-            cur_max = 0
+        dp = [1] * len(nums)
+
+        for i in range(1, len(nums)):
             for j in range(i):
                 if nums[i] > nums[j]:
-                    cur_max = max(cur_max, dp[j])
-            dp[i] = cur_max + 1
-            ans = max(ans, dp[i])
-        return ans
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
 
 
-# DP - binary
-# https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
+# binary search
 class Solution:
     def lengthOfLIS(self, nums):
-        if not nums:
-            return 0
-        tails = [0] * len(nums)
-        size = 0
-        for n in nums:
-            left = 0
-            right = size
-            while left < right:
-                mid = left + (right - left) // 2
-                if tails[mid] < n:
-                    left = mid + 1
-                else:
-                    right = mid
-            tails[left] = n
-            if left == size:
-                size += 1
-        return size
+        sub = []
+
+        for num in nums:
+            idx = bisect.bisect_left(sub, num)
+            if idx == len(sub):
+                sub.append(num)
+            else:
+                sub[idx] = num
+
+        return len(sub)
