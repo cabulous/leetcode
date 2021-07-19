71d6787b1d58eca556869a9f859721ca29f413a8
Carson <dragonchen66@gmail.com>
2021-07-19 09:57:04 -0400

    practice: daily challenge and revisit

---
+++ python/235.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        parent_val, p_val, q_val = root.val, p.val, q.val
+
+        if p_val > parent_val and q_val > parent_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        if p_val < parent_val and q_val < parent_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        return root
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        p_val, q_val = p.val, q.val
+        node = root
+
+        while node:
+            parent_val = node.val
+            if p_val > parent_val and q_val > parent_val:
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                node = node.left
+            else:
+                return node
