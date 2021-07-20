d50cbfd173ad9e97994ee3bfa23cf170368b7621
Carson <dragonchen66@gmail.com>
2021-07-20 10:18:40 -0400

    practice: daily challenge and revisit

--- python/235.py
+++ python/235.py
@@ -24,10 +24,10 @@ class Solution:
         node = root
 
         while node:
-            parent_val = node.val
-            if p_val > parent_val and q_val > parent_val:
+            node_val = node.val
+            if p_val > node_val and q_val > node_val:
                 node = node.right
-            elif p_val < parent_val and q_val < parent_val:
+            elif p_val < node_val and q_val < node_val:
                 node = node.left
             else:
                 return node
---
+++ python/384.py
@@ -0,0 +1,37 @@
+import random
+from typing import List
+
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self) -> List[int]:
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self) -> List[int]:
+        for i in range(len(self.array)):
+            swap_idx = random.randrange(i, len(self.array))
+            self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
+        return self.array
+
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self) -> List[int]:
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self) -> List[int]:
+        aux = list(self.array)
+        for i in range(len(self.array)):
+            remove_idx = random.randrange(len(aux))
+            self.array[i] = aux.pop(remove_idx)
+        return self.array
