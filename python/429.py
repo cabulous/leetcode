b99b0a935ad46fcfa12b568bfae342451c9b1ba4
Carson <dragonchen66@gmail.com>
2021-08-06 08:59:05 -0400

    practice: daily challenge and revisit

---
+++ python/429.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+
+        res = []
+        level = [root]
+        while level:
+            res.append([node.val for node in level])
+            level = [kid for node in level for kid in node.children if kid]
+
+        return res
