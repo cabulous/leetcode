00de7ab60068168685c208bfa889b5ad900f5b12
Carson <dragonchen66@gmail.com>
2021-08-17 10:27:31 -0400

    practice: daily challenge and revisit

---
+++ python/1448.py
@@ -0,0 +1,43 @@
+import math
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        res = 0
+        stack = [(root, -math.inf)]
+
+        while stack:
+            node, max_so_far = stack.pop()
+            if node.val >= max_so_far:
+                res += 1
+            if node.left:
+                stack.append((node.left, max(max_so_far, node.val)))
+            if node.right:
+                stack.append((node.right, max(max_so_far, node.val)))
+
+        return res
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        res = 0
+
+        def dfs(node, max_so_far):
+            nonlocal res
+            if node.val >= max_so_far:
+                res += 1
+            if node.left:
+                dfs(node.left, max(max_so_far, node.val))
+            if node.right:
+                dfs(node.right, max(max_so_far, node.val))
+
+        dfs(root, -math.inf)
+
+        return res
--- python/235.py
+++ python/235.py
@@ -9,11 +9,10 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         parent_val, p_val, q_val = root.val, p.val, q.val
 
-        if p_val > parent_val and q_val > parent_val:
-            return self.lowestCommonAncestor(root.right, p, q)
-
         if p_val < parent_val and q_val < parent_val:
             return self.lowestCommonAncestor(root.left, p, q)
+        if p_val > parent_val and q_val > parent_val:
+            return self.lowestCommonAncestor(root.right, p, q)
 
         return root
 
@@ -24,10 +23,9 @@ class Solution:
         node = root
 
         while node:
-            node_val = node.val
-            if p_val > node_val and q_val > node_val:
-                node = node.right
-            elif p_val < node_val and q_val < node_val:
+            if p_val < node.val and q_val < node.val:
                 node = node.left
+            elif p_val > node.val and q_val > node.val:
+                node = node.right
             else:
                 return node
