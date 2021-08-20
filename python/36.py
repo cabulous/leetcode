e2d79d2ce568d7af72c8097d30e28466a7c26b3b
Carson <dragonchen66@gmail.com>
2021-08-20 09:34:46 -0400

    practice: daily challenge and revisit

--- python/36.py
+++ python/36.py
@@ -12,11 +12,11 @@ class Solution:
                 num = board[i][j]
                 if num != '.':
                     num = int(num)
-                    box_index = (i // 3) * 3 + j // 3
+                    box_idx = (i // 3) * 3 + j // 3
                     rows[i][num] = rows[i].get(num, 0) + 1
                     cols[j][num] = cols[j].get(num, 0) + 1
-                    boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
-                    if rows[i][num] > 1 or cols[j][num] > 1 or boxes[box_index][num] > 1:
+                    boxes[box_idx][num] = boxes[box_idx].get(num, 0) + 1
+                    if rows[i][num] > 1 or cols[j][num] > 1 or boxes[box_idx][num] > 1:
                         return False
 
         return True
