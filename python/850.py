333a9d49cec4a656acfed11a5e1268d7199e9537
Carson <dragonchen66@gmail.com>
2021-08-23 10:05:20 -0400

    practice: daily challenge and revisit

--- python/850.py
+++ python/850.py
@@ -4,27 +4,28 @@ from typing import List
 # https://leetcode.com/problems/rectangle-area-ii/discuss/137914/JavaC%2B%2BPython-Discretization-and-O(NlogN)
 class Solution:
     def rectangleArea(self, rectangles: List[List[int]]) -> int:
+        mod = 10 ** 9 + 7
         xs = sorted(set([x for x1, y1, x2, y2 in rectangles for x in [x1, x2]]))
         x_i = {x: i for i, x in enumerate(xs)}
-        count = [0] * (len(x_i))
-        l = []
+        count = [0] * len(x_i)
+        lst = []
 
         for x1, y1, x2, y2 in rectangles:
-            l.append([y1, x1, x2, 1])
-            l.append([y2, x1, x2, -1])
+            lst.append([y1, x1, x2, 1])
+            lst.append([y2, x1, x2, -1])
 
-        l.sort()
+        lst.sort()
 
         cur_y = cur_x_sum = area = 0
 
-        for y, x1, x2, sig in l:
+        for y, x1, x2, sig in lst:
             area += (y - cur_y) * cur_x_sum
             cur_y = y
             for i in range(x_i[x1], x_i[x2]):
                 count[i] += sig
             cur_x_sum = sum(x2 - x1 if c else 0 for x1, x2, c in zip(xs, xs[1:], count))
 
-        return area % (10 ** 9 + 7)
+        return area % mod
 
 
 class Solution:
