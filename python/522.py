ee6db9b53f748df3cbe0b07a7cff00f2926b54a3
Carson <dragonchen66@gmail.com>
2021-08-27 09:59:28 -0400

    practice: daily challenge and revisit

---
+++ python/522.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+# https://leetcode.com/problems/longest-uncommon-subsequence-ii/discuss/99453/Python-Simple-Explanation/103517
+class Solution:
+    def findLUSlength(self, strs: List[str]) -> int:
+        for word1 in sorted(strs, key=len, reverse=True):
+            if sum(self.is_sub_sequence(word1, word2) for word2 in strs) == 1:
+                return len(word1)
+        return -1
+
+    def is_sub_sequence(self, word1, word2):
+        word2 = iter(word2)
+        return all(c in word2 for c in word1)
