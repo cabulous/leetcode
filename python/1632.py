f609e00ee1d38cbc436963c619089687582b5112
Carson <dragonchen66@gmail.com>
2021-08-30 10:12:17 -0400

    practice: daily challenge and revisit

--- python/1632.py
+++ python/1632.py
@@ -14,13 +14,14 @@ class DSU:
         return self.p[x]
 
     def union(self, x, y):
-        self.p[self.find(x)] = self.find(y)
+        x, y = self.find(x), self.find(y)
+        self.p[x] = y
 
     def groups(self):
-        ans = defaultdict(list)
+        groups = defaultdict(list)
         for el in self.p.keys():
-            ans[self.find(el)].append(el)
-        return ans
+            groups[self.find(el)].append(el)
+        return groups
 
 
 class Solution:
@@ -45,32 +46,3 @@ class Solution:
                 matrix[i][j] = rank[i]
 
         return matrix
-
-
-# https://leetcode.com/problems/rank-transform-of-a-matrix/discuss/909142/Python-Union-Find
-class Solution:
-    def matrixRankTransform(self, matrix: List[List[int]]) -> List[List[int]]:
-        n, m = len(matrix), len(matrix[0])
-        rank = [0] * (m + n)
-        d = defaultdict(list)
-
-        for i in range(n):
-            for j in range(m):
-                d[matrix[i][j]].append([i, j])
-
-        for val in sorted(d):
-            p = [i for i in range(m + n)]
-            rank2 = rank[:]
-            for i, j in d[val]:
-                i, j = self.find(p, i), self.find(p, j + n)
-                p[i] = j
-                rank2[j] = max(rank2[i], rank2[j])
-            for i, j in d[val]:
-                rank[i] = rank[j + n] = matrix[i][j] = rank2[self.find(p, i)] + 1
-
-        return matrix
-
-    def find(self, p, i):
-        if p[i] != i:
-            p[i] = self.find(p, p[i])
-        return p[i]
