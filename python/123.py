84f9dbe4c8f23b99f3ab1528d57e7b44bd47360c
Carson <dragonchen66@gmail.com>
2021-10-20 10:02:09 -0400

    practice: daily challenge and revisit

--- python/123.py
+++ python/123.py
@@ -13,30 +13,3 @@ class Solution:
             t2_profit = max(t2_profit, price - t2_cost)
 
         return t2_profit
-
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) <= 1:
-            return 0
-
-        left_min = prices[0]
-        right_max = prices[-1]
-
-        length = len(prices)
-        left_profits = [0] * length
-        right_profits = [0] * (length + 1)
-
-        for left in range(1, length):
-            left_profits[left] = max(left_profits[left - 1], prices[left] - left_min)
-            left_min = min(left_min, prices[left])
-
-            right = length - left - 1
-            right_profits[right] = max(right_profits[right + 1], right_max - prices[right])
-            right_max = max(right_max, prices[right])
-
-        max_profit = 0
-        for i in range(length):
-            max_profit = max(max_profit, left_profits[i] + right_profits[i + 1])
-
-        return max_profit
