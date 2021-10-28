34146b331d67be12e9c5f323995ea94e3c3955a1
Carson <dragonchen66@gmail.com>
2021-10-28 09:56:11 -0400

    practice: daily challenge and revisit

--- python/15.py
+++ python/15.py
@@ -1,61 +1,72 @@
+from typing import List
+
+
 # two pointer
 class Solution:
-    def threeSum(self, nums: [int]) -> [[int]]:
-        if not nums:
-            return []
+    def __init__(self):
+        self.res = []
 
-        def twoSum(i):
-            lo = i + 1
-            hi = n - 1
-            while lo < hi:
-                sum = nums[i] + nums[lo] + nums[hi]
-                if sum < 0:
-                    lo += 1
-                elif sum > 0:
-                    hi -= 1
-                else:
-                    res.append([nums[i], nums[lo], nums[hi]])
-                    lo += 1
-                    hi -= 1
-                    while lo < hi and nums[lo] == nums[lo - 1]:
-                        lo += 1
-
-        res = []
-        n = len(nums)
-
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        for i in range(n):
+        for i in range(len(nums)):
             if nums[i] > 0:
                 break
             if i == 0 or nums[i - 1] != nums[i]:
-                twoSum(i)
+                self.two_sum(nums, i)
+        return self.res
 
-        return res
+    def two_sum(self, nums, i):
+        lo, hi = i + 1, len(nums) - 1
+        while lo < hi:
+            total = nums[i] + nums[lo] + nums[hi]
+            if total < 0:
+                lo += 1
+            elif total > 0:
+                hi -= 1
+            else:
+                self.res.append([nums[i], nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+                while lo < hi and nums[lo] == nums[lo - 1]:
+                    lo += 1
 
 
-# hashset
 class Solution:
-    def threeSum(self, nums: [int]) -> [[int]]:
-        def twoSum(i):
-            seen = set()
-            j = i + 1
-            while j < n:
-                complement = -nums[i] - nums[j]
-                if complement in seen:
-                    res.append([nums[i], nums[j], complement])
-                    while j + 1 < n and nums[j] == nums[j + 1]:
-                        j += 1
-                seen.add(nums[j])
-                j += 1
+    def __init__(self):
+        self.res = []
 
-        res = []
-        n = len(nums)
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-
-        for i in range(n):
+        for i in range(len(nums)):
             if nums[i] > 0:
                 break
             if i == 0 or nums[i - 1] != nums[i]:
-                twoSum(i)
+                self.two_sum(nums, i)
+        return self.res
 
+    def two_sum(self, nums, i):
+        seen = set()
+        j = i + 1
+        while j < len(nums):
+            complement = -nums[i] - nums[j]
+            if complement in seen:
+                self.res.append([nums[i], nums[j], complement])
+                while j + 1 < len(nums) and nums[j] == nums[j + 1]:
+                    j += 1
+            seen.add(nums[j])
+            j += 1
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i + 1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
         return res
