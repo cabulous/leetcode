59365034e6d92fee4ded324120dc704df594403c
Carson <dragonchen66@gmail.com>
2021-11-11 09:53:31 -0500

    practice: daily challenge and revisit

---
+++ python/1413.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        min_val = total = 0
+
+        for num in nums:
+            total += num
+            min_val = min(min_val, total)
+
+        return -min_val + 1
