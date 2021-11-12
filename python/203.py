a321902baeb892b1406572200e4ced85f7d6e656
Carson <dragonchen66@gmail.com>
2021-11-12 10:12:50 -0500

    practice: daily challenge and revisit

---
+++ python/203.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        sentinel = ListNode(0, head)
+        prev, curr = sentinel, head
+
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+
+        return sentinel.next
