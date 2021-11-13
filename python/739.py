1a4f808f9d8d3e23fac7ce9593d6784ec011f000
Carson <dragonchen66@gmail.com>
2021-11-13 10:15:33 -0500

    practice: daily challenge and revisit

---
+++ python/739.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        stack = []
+
+        for curr_day, curr_temp in enumerate(temperatures):
+            while stack and curr_temp > temperatures[stack[-1]]:
+                prev_day = stack.pop()
+                ans[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+
+        return ans
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        hottest = 0
+
+        for curr_day in reversed(range(len(temperatures))):
+            curr_temp = temperatures[curr_day]
+            if curr_temp >= hottest:
+                hottest = curr_temp
+                continue
+            days = 1
+            while temperatures[curr_day + days] <= curr_temp:
+                days += ans[curr_day + days]
+            ans[curr_day] = days
+
+        return ans
