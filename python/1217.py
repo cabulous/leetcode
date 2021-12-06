7d45e476cbf505990d18891aaf7d7f93d1f3d885
Carson <dragonchen66@gmail.com>
2021-12-06 09:59:03 -0500

    practice: daily challenge and revisit

---
+++ python/1217.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        even_cnt = odd_cnt = 0
+
+        for i in position:
+            if i % 2 == 0:
+                even_cnt += 1
+            else:
+                odd_cnt += 1
+
+        return min(even_cnt, odd_cnt)
