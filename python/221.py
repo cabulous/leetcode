785f092e7ba41434a4dcc05ae5811c0bd12bada4
Carson <dragonchen66@gmail.com>
2021-12-17 10:21:40 -0500

    practice: daily challenge and revisit

---
+++ python/221.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0] * cols for _ in range(rows)]
+        res = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1])
+                    res = max(res, dp[i][j] ** 2)
+
+        return res
