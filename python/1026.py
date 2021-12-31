ed351ac9d3963735bd11227c4870a717461ebe86
Carson <dragonchen66@gmail.com>
2021-12-30 21:54:17 -0500

    practice: daily challenge and revisit

---
+++ python/1026.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return self.helper(root, root.val, root.val)
+
+    def helper(self, node, cur_max, cur_min):
+        if not node:
+            return cur_max - cur_min
+
+        cur_max = max(cur_max, node.val)
+        cur_min = min(cur_min, node.val)
+
+        left = self.helper(node.left, cur_max, cur_min)
+        right = self.helper(node.right, cur_max, cur_min)
+
+        return max(left, right)
