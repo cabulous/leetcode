a2c2176e8305436ef870443a4c31ea0803e04264
Carson <dragonchen66@gmail.com>
2022-01-27 10:48:26 -0500

    practice: daily challenge and revisit

--- python/2003.py
+++ python/2003.py
@@ -3,33 +3,38 @@ from typing import List
 
 # https://leetcode.com/problems/smallest-missing-genetic-value-in-each-subtree/discuss/1458486/Python-Strict-O(n)-Solution
 class Solution:
+
+    def __init__(self):
+        self.nums = []
+        self.seen = [0] * 100010
+        self.children = []
+
     def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
+        self.nums = nums
         parent_count = len(parents)
-        seen = [0] * 100010
         res = [1] * parent_count
 
         if 1 not in nums:
             return res
 
-        children = [[] for _ in range(parent_count)]
-
+        self.children = [[] for _ in range(parent_count)]
         for i in range(1, parent_count):
-            children[parents[i]].append(i)
-
-        def dfs(parent):
-            if seen[nums[parent]] == 0:
-                for next_parent in children[parent]:
-                    dfs(next_parent)
-                seen[nums[parent]] = 1
+            self.children[parents[i]].append(i)
 
         index = nums.index(1)
         miss = 1
 
         while index >= 0:
-            dfs(index)
-            while seen[miss]:
+            self.dfs(index)
+            while self.seen[miss]:
                 miss += 1
             res[index] = miss
             index = parents[index]
 
         return res
+
+    def dfs(self, parent):
+        if self.seen[self.nums[parent]] == 0:
+            for next_parent in self.children[parent]:
+                self.dfs(next_parent)
+            self.seen[self.nums[parent]] = 1
