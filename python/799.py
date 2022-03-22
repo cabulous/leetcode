8d999fe0c5ea5109ab09555de34479ad25a798e1
Carson <dragonchen66@gmail.com>
2022-03-22 10:44:32 -0400

    practice: daily challenge and revisit

--- python/799.py
+++ python/799.py
@@ -2,7 +2,9 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         res = [poured] + [0] * query_row
+
         for row in range(1, query_row + 1):
             for i in reversed(range(row + 1)):
                 res[i] = max(res[i] - 1, 0) / 2 + max(res[i - 1] - 1, 0) / 2
+
         return min(res[query_glass], 1)
