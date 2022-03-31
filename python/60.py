f1e6502c8f38ac8444e59ceceae60f32f79c24ff
Carson <dragonchen66@gmail.com>
2022-03-31 10:35:10 -0400

    practice: daily challenge and revisit

---
+++ python/60.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        factorials = [1]
+        nums = ['1']
+        for i in range(1, n):
+            factorials.append(factorials[i - 1] * i)
+            nums.append(str(i + 1))
+
+        k -= 1
+        res = []
+
+        for i in reversed(range(n)):
+            index = k // factorials[i]
+            k -= index * factorials[i]
+            res.append(nums[index])
+            del nums[index]
+
+        return ''.join(res)
