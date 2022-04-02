54f4c6062eaa725f0e9357b313bf5975a131bb3f
Carson <dragonchen66@gmail.com>
2022-04-02 10:39:35 -0400

    practice: daily challenge and revisit

--- python/2025.py
+++ python/2025.py
@@ -3,11 +3,13 @@ from itertools import accumulate
 from typing import List
 
 
+# https://leetcode.com/problems/maximum-number-of-ways-to-partition-an-array/discuss/1507271/Pythoncpp-Explanation-with-pictures.-O(N)
 class Solution:
     def waysToPartition(self, nums: List[int], k: int) -> int:
+        prefix_sum = list(accumulate(nums))
         nums_sum = sum(nums)
         pre_sum = defaultdict(int)
-        suf_sum = Counter(pre_sum)
+        suf_sum = Counter(prefix_sum)
         suf_sum[nums_sum] -= 1
 
         res = 0
@@ -15,19 +17,17 @@ class Solution:
         if nums_sum % 2 == 0:
             res = suf_sum[nums_sum // 2]
 
-        prefix_sum = list(accumulate(nums))
-
         for i in range(len(nums)):
             curr = 0
             if nums[i] != k:
                 new_sum = nums_sum + k - nums[i]
                 if new_sum % 2 == 0:
                     if i > 0:
-                        curr += prefix_sum[new_sum // 2]
+                        curr += pre_sum[new_sum // 2]
                     if i < len(nums) - 1:
                         curr += suf_sum[new_sum // 2 + nums[i] - k]
             res = max(res, curr)
-            suf_sum[prefix_sum[i]] -= 1
             pre_sum[prefix_sum[i]] += 1
+            suf_sum[prefix_sum[i]] -= 1
 
         return res
