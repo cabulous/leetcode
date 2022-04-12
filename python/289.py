8daa702413d568e565d581ee63c214743f6dcebc
Carson <dragonchen66@gmail.com>
2022-04-12 10:29:24 -0400

    practice: daily challenge and revisit

--- python/289.py
+++ python/289.py
@@ -1,70 +1,71 @@
-import collections
+import copy
+from collections import Counter
+from typing import List
 
 
-# infinite
 class Solution:
-    def infinite(self, live):
-        ctr = collections.Counter(
-            (I, J)
-            for i, j in live
-            for I in range(i - 1, i + 2)
-            for J in range(j - 1, j + 2)
-            if J != j or I != i
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        live_cells = {(r, c) for r, row in enumerate(board) for c, live in enumerate(row) if live}
+        live_cells = self.next_round(live_cells)
+        for r, row in enumerate(board):
+            for c in range(len(row)):
+                row[c] = int((r, c) in live_cells)
+
+    def next_round(self, live):
+        count = Counter(
+            (nr, nc)
+            for r, c in live
+            for nr in range(r - 1, r + 2)
+            for nc in range(c - 1, c + 2)
+            if nr != r or nc != c
         )
+
         return {
-            ij
-            for ij in ctr
-            if ctr[ij] == 3 or (ctr[ij] == 2 and ij in live)
+            pair
+            for pair in count
+            if count[pair] == 3 or (count[pair] == 2 and pair in live)
         }
 
-    def gameOfLife(self, board: [[int]]) -> None:
-        live = {(i, j) for i, row in enumerate(board) for j, live in enumerate(row) if live}
-        live = self.infinite(live)
-        for i, row in enumerate(board):
-            for j in range(len(row)):
-                row[j] = int((i, j) in live)
-
 
 class Solution:
-    def gameOfLife(self, board: [[int]]) -> None:
-        neighbors = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
         rows, cols = len(board), len(board[0])
-        copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+        origin_board = copy.deepcopy(board)
 
         for row in range(rows):
             for col in range(cols):
                 live_neighbors = 0
-                for n in neighbors:
-                    r, c = row + n[0], col + n[1]
-                    if 0 <= r < rows and 0 <= c < cols and copy_board[r][c] == 1:
+                for dr, dc in neighbors:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and origin_board[nr][nc] == 1:
                         live_neighbors += 1
-                if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                if origin_board[row][col] == 1 and (live_neighbors < 2 or 3 < live_neighbors):
                     board[row][col] = 0
-                if copy_board[row][col] == 0 and live_neighbors == 3:
+                elif origin_board[row][col] == 0 and live_neighbors == 3:
                     board[row][col] = 1
 
 
-# in place
 class Solution:
-    def gameOfLife(self, board: [[int]]) -> None:
-        neighbors = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        neighbors = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
         rows, cols = len(board), len(board[0])
 
         for row in range(rows):
             for col in range(cols):
                 live_neighbors = 0
-                for n in neighbors:
-                    r, c = row + n[0], col + n[1]
-                    if 0 <= r < rows and 0 <= c < cols and abs(board[r][c]) == 1:
+                for dr, dc in neighbors:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and abs(board[nr][nc]) == 1:
                         live_neighbors += 1
-                if board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                if board[row][col] == 1 and (live_neighbors < 2 or 3 < live_neighbors):
                     board[row][col] = -1
                 if board[row][col] == 0 and live_neighbors == 3:
                     board[row][col] = 2
 
         for row in range(rows):
             for col in range(cols):
-                if board[row][col] > 0:
+                if board[row][col] == 2:
                     board[row][col] = 1
-                else:
+                elif board[row][col] == -1:
                     board[row][col] = 0
