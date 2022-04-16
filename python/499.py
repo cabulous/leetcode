0c89f3f9dff49d65eda549a3df005417f23d4fb1
Carson <dragonchen66@gmail.com>
2022-04-16 10:47:35 -0400

    practice: daily challenge and revisit

---
+++ python/499.py
@@ -0,0 +1,49 @@
+import heapq
+from typing import List
+
+
+# https://leetcode.com/problems/the-maze-iii/discuss/150550/Python-Short-PriorityQueue-solution-beats-100
+class Solution:
+
+    def __init__(self):
+        self.maze = []
+        self.rows = 0
+        self.cols = 0
+        self.hole = []
+
+    def findShortestWay(self, maze: List[List[int]], ball: List[int], hole: List[int]) -> str:
+        self.maze = maze
+        self.rows, self.cols = len(maze), len(maze[0])
+        self.hole = hole
+
+        queue = [(0, '', ball[0], ball[1])]
+        stopped = {(ball[0], ball[1]): [0, '']}
+
+        while queue:
+            dist, pattern, row, col = heapq.heappop(queue)
+
+            if [row, col] == hole:
+                return pattern
+
+            for next_row, next_col, delta_pattern, delta_dist in self.get_next_step(row, col):
+                next_dist = dist + delta_dist
+                next_pattern = pattern + delta_pattern
+
+                if (next_row, next_col) in stopped and [next_dist, next_pattern] >= stopped[(next_row, next_col)]:
+                    continue
+
+                stopped[(next_row, next_col)] = [next_dist, next_pattern]
+                heapq.heappush(queue, (next_dist, next_pattern, next_row, next_col))
+
+        return 'impossible'
+
+    def get_next_step(self, row, col):
+        for dr, dc, pattern in ((-1, 0, 'u'), (1, 0, 'd'), (0, -1, 'l'), (0, 1, 'r')):
+            nr, nc, dist = row, col, 0
+            while 0 <= nr + dr < self.rows and 0 <= nc + dc < self.cols and self.maze[nr + dr][nc + dc] != 1:
+                nr += dr
+                nc += dc
+                dist += 1
+                if [nr, nc] == self.hole:
+                    break
+            yield nr, nc, pattern, dist
--- python/538.py
+++ python/538.py
@@ -1,4 +1,6 @@
-# Definition for a binary tree node.
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -6,32 +8,40 @@ class TreeNode:
         self.right = right
 
 
-# dfs
 class Solution:
+
     def __init__(self):
         self.total = 0
 
-    def convertBST(self, root: TreeNode) -> TreeNode:
-        if root:
-            self.convertBST(root.right)
-            self.total += root.val
-            root.val = self.total
-            self.convertBST(root.left)
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        self.convertBST(root.right)
+
+        self.total += root.val
+        root.val = self.total
+
+        self.convertBST(root.left)
+
         return root
 
 
-# dfs + stack
 class Solution:
-    def convertBST(self, root: TreeNode) -> TreeNode:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         total = 0
-        node = root
         stack = []
+        node = root
+
         while stack or node:
             while node:
                 stack.append(node)
                 node = node.right
+
             node = stack.pop()
             total += node.val
             node.val = total
+
             node = node.left
+
         return root
