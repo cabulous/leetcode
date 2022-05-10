d9dbee93cb6da444a367d14af68bd70ad6e9b53a
Carson <dragonchen66@gmail.com>
2022-05-10 10:52:00 -0400

    practice: daily challenge and revisit

--- python/1467.py
+++ python/1467.py
@@ -6,18 +6,17 @@ from typing import List
 # https://leetcode.com/problems/probability-of-a-two-boxes-having-the-same-number-of-distinct-balls/discuss/661757/Python-10-Lines-90-Multionomial-coefficients-explained
 class Solution:
     def getProbability(self, balls: List[int]) -> float:
-        k = len(balls)
-        n = sum(balls) // 2
-        q = 0
+        box_sum = sum(balls) // 2
+        res = 0
 
-        arrays = [range(0, i + 1) for i in balls]
+        arrays = [range(i + 1) for i in balls]
         t = list(product(*arrays))
 
         for i in range(len(t)):
-            if sum(t[i]) == n and t[i].count(0) == t[-i - 1].count(0):
-                q += self.multinomial(t[i]) * self.multinomial(t[-i - 1])
+            if sum(t[i]) == box_sum and t[i].count(0) == t[-i - 1].count(0):
+                res += self.multinomial(t[i]) * self.multinomial(t[-i - 1])
 
-        return q / self.multinomial(list(balls))
+        return res / self.multinomial(list(balls))
 
     def multinomial(self, n):
         return factorial(sum(n)) / prod([factorial(i) for i in n])
