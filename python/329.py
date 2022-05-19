9bbd2bc90eb3f431cfa149be1c7e87ec4dbb4460
Carson <dragonchen66@gmail.com>
2022-05-19 10:27:10 -0400

    practice: daily challenge and revisit

--- python/329.py
+++ python/329.py
@@ -3,22 +3,35 @@ from typing import List
 
 # https://leetcode.com/problems/longest-increasing-path-in-a-matrix/discuss/78334/Python-solution-memoization-dp-288ms
 class Solution:
+
+    def __init__(self):
+        self.matrix = []
+        self.rows = 0
+        self.cols = 0
+        self.dp = []
+
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        if not matrix or not matrix[0]:
+        if not any(matrix):
             return 0
 
-        max_row, max_col = len(matrix), len(matrix[0])
-        dp = [[0] * max_col for _ in range(max_row)]
+        self.matrix = matrix
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
+        self.dp = [[0] * self.cols for _ in range(self.rows)]
 
-        def dfs(row, col):
-            if not dp[row][col]:
-                val = matrix[row][col]
-                dp[row][col] = 1 + max(
-                    dfs(row + 1, col) if row < max_row - 1 and matrix[row + 1][col] > val else 0,
-                    dfs(row - 1, col) if row > 0 and matrix[row - 1][col] > val else 0,
-                    dfs(row, col + 1) if col < max_col - 1 and matrix[row][col + 1] > val else 0,
-                    dfs(row, col - 1) if col > 0 and matrix[row][col - 1] > val else 0,
-                )
-            return dp[row][col]
+        return max(self.dfs(row, col) for col in range(self.cols) for row in range(self.rows))
 
-        return max(dfs(row, col) for col in range(max_col) for row in range(max_row))
+    def dfs(self, row, col):
+        if self.dp[row][col] != 0:
+            return self.dp[row][col]
+
+        val = self.matrix[row][col]
+
+        self.dp[row][col] = 1 + max(
+            self.dfs(row - 1, col) if 0 <= row - 1 and val < self.matrix[row - 1][col] else 0,
+            self.dfs(row + 1, col) if row + 1 < self.rows and val < self.matrix[row + 1][col] else 0,
+            self.dfs(row, col - 1) if 0 <= col - 1 and val < self.matrix[row][col - 1] else 0,
+            self.dfs(row, col + 1) if col + 1 < self.cols and val < self.matrix[row][col + 1] else 0,
+        )
+
+        return self.dp[row][col]
