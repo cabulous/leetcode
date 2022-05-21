9016215433ed1a7fbea896dbdbaf66d24dbdfe25
Carson <dragonchen66@gmail.com>
2022-05-21 09:42:33 -0400

    practice: daily challenge and revisit

--- python/322.py
+++ python/322.py
@@ -1,10 +1,14 @@
-# DP
+import math
+
+
 # https://leetcode.com/problems/coin-change/discuss/77372/Clean-dp-python-code
 class Solution:
     def coinChange(self, coins, amount):
-        dp = [0.0] + [float('inf')] * amount
+        dp = [0] + [math.inf] * amount
+
         for i in range(1, amount + 1):
             for coin in coins:
                 if i - coin >= 0:
-                    dp[i] = min(dp[i], dp[i - coin] + 1)
-        return int(dp[-1]) if dp[-1] != float('inf') else -1
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+
+        return dp[-1] if dp[-1] < math.inf else -1
