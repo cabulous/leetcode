57be71156a5b97b2ad7f8370087b9aac3db755a9
Carson <dragonchen66@gmail.com>
2022-05-23 10:09:23 -0400

    practice: daily challenge and revisit

--- python/647.py
+++ python/647.py
@@ -5,9 +5,9 @@ class Solution:
         dp = [[0] * n for _ in range(n)]
         res = 0
 
-        for i in range(n - 1, -1, -1):
-            for j in range(i, n):
-                dp[i][j] = s[i] == s[j] and (j - i + 1 < 3 or dp[i + 1][j - 1])
-                res += dp[i][j]
+        for start in range(n - 1, -1, -1):
+            for end in range(start, n):
+                dp[start][end] = s[start] == s[end] and (end - start + 1 < 3 or dp[start + 1][end - 1])
+                res += dp[start][end]
 
         return res
