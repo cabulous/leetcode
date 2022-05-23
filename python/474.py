d6c2473c92ea7b1a8c8affd058c5300526b5732a
Carson <dragonchen66@gmail.com>
2022-05-23 09:56:41 -0400

    practice: daily challenge and revisit

--- python/474.py
+++ python/474.py
@@ -5,13 +5,13 @@ class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        def count(s):
-            return sum(1 for c in s if c == '0'), sum(1 for c in s if c == '1')
-
-        for z, o in [count(s) for s in strs]:
-            for x in range(m, -1, -1):
-                for y in range(n, -1, -1):
-                    if x >= z and y >= o:
-                        dp[x][y] = max(dp[x][y], 1 + dp[x - z][y - o])
+        for zero, one in [self.count(s) for s in strs]:
+            for zero_max in range(m, -1, -1):
+                for one_max in range(n, -1, -1):
+                    if zero <= zero_max and one <= one_max:
+                        dp[zero_max][one_max] = max(dp[zero_max][one_max], 1 + dp[zero_max - zero][one_max - one])
 
         return dp[m][n]
+
+    def count(self, s):
+        return sum(1 for c in s if c == '0'), sum(1 for c in s if c == '1')
