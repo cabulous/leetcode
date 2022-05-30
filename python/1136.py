64fcf37be9a9d998463e5e0ee4f9d353c6a4a026
Carson <dragonchen66@gmail.com>
2022-05-30 10:24:51 -0400

    practice: daily challenge and revisit

--- python/1136.py
+++ python/1136.py
@@ -4,19 +4,18 @@ from typing import List
 
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        graph = {i: [] for i in range(1, n + 1)}
+        graph = defaultdict(list)
         in_degree = defaultdict(int)
 
         for start, end in relations:
             graph[start].append(end)
             in_degree[end] += 1
 
-        queue = [node for node in graph if in_degree[node] == 0]
+        queue = [node for node in range(1, n + 1) if in_degree[node] == 0]
         step = 0
         studied_count = 0
 
         while queue:
-            step += 1
             next_queue = []
             for node in queue:
                 studied_count += 1
@@ -24,6 +23,7 @@ class Solution:
                     in_degree[next_node] -= 1
                     if in_degree[next_node] == 0:
                         next_queue.append(next_node)
+            step += 1
             queue = next_queue
 
         return step if studied_count == n else -1
