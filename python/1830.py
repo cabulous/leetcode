bc1e0e678fc37e4783994314c235e81e2a349293
Carson <dragonchen66@gmail.com>
2022-05-31 10:36:27 -0400

    practice: daily challenge and revisit

--- python/1830.py
+++ python/1830.py
@@ -13,16 +13,16 @@ class Solution:
         for i in range(n - 1, -1, -1):
             ind = ord(s[i]) - ord('a')
             count[ind] += 1
-            ans = sum(count[:ind]) * self.f(n - i - 1)
+            curr = sum(count[:ind]) * self.f(n - i - 1)
             for j in range(26):
-                ans = ans * self.inv(self.f(count[j])) % MOD
-            res += ans
+                curr *= self.inv(self.f(count[j])) % MOD
+            res += curr
 
         return res % MOD
 
     @lru_cache(None)
     def f(self, i):
-        return 1 if i <= 1 else (self.f(i - 1) * i) % MOD
+        return 1 if i <= 1 else self.f(i - 1) * i % MOD
 
     @lru_cache(None)
     def inv(self, i):
