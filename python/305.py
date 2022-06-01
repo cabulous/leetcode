577a9766e219d57b2363ec0cfafd58516134731f
Carson <dragonchen66@gmail.com>
2022-06-01 10:53:41 -0400

    practice: daily challenge and revisit

--- python/305.py
+++ python/305.py
@@ -8,12 +8,6 @@ class UnionFind:
         self.parent = {}
         self.count = 0
 
-    def add(self, position):
-        if position in self.parent:
-            return
-        self.parent[position] = position
-        self.count += 1
-
     def find(self, x):
         if x != self.parent[x]:
             self.parent[x] = self.find(self.parent[x])
@@ -22,25 +16,35 @@ class UnionFind:
     def union(self, x, y):
         x_root = self.find(x)
         y_root = self.find(y)
+        if x_root != y_root:
+            self.parent[x_root] = y_root
+            self.count -= 1
 
-        if x_root == y_root:
+    def contains(self, row, col):
+        return (row, col) in self.parent
+
+    def add(self, row, col):
+        if self.contains(row, col):
             return
+        self.parent[row, col] = (row, col)
+        self.count += 1
 
-        self.parent[x_root] = y_root
-        self.count -= 1
+    def get_count(self):
+        return self.count
 
 
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         islands = UnionFind()
         res = []
 
         for row, col in positions:
-            islands.add((row, col))
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            islands.add(row, col)
+            for dr, dc in directions:
                 new_row, new_col = row + dr, col + dc
-                if (new_row, new_col) in islands.parent:
+                if islands.contains(new_row, new_col):
                     islands.union((row, col), (new_row, new_col))
-            res.append(islands.count)
+            res.append(islands.get_count())
 
         return res
