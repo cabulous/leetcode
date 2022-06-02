3bc745118bf9bac59e275ea6fa881a7a55fca82b
Carson <dragonchen66@gmail.com>
2022-06-02 10:37:19 -0400

    practice: daily challenge and revisit

--- python/1723.py
+++ python/1723.py
@@ -15,25 +15,25 @@ class Solution:
         left, right = max(jobs), sum(jobs)
         while left <= right:
             mid = left + (right - left) // 2
-            cap = [mid] * k
-            if self.dfs(0, cap, mid):
+            time = [mid] * k
+            if self.can_reach(0, time, mid):
                 right = mid - 1
             else:
                 left = mid + 1
 
         return left
 
-    def dfs(self, i, cap, target):
-        if i == len(self.jobs):
+    def can_reach(self, curr_job_index, time, target):
+        if curr_job_index == len(self.jobs):
             return True
 
-        for j in range(self.k):
-            if cap[j] >= self.jobs[i]:
-                cap[j] -= self.jobs[i]
-                if self.dfs(i + 1, cap, target):
+        for i in range(self.k):
+            if time[i] >= self.jobs[curr_job_index]:
+                time[i] -= self.jobs[curr_job_index]
+                if self.can_reach(curr_job_index + 1, time, target):
                     return True
-                cap[j] += self.jobs[i]
-            if cap[j] == target:
+                time[i] += self.jobs[curr_job_index]
+            if time[i] == target:
                 break
 
         return False
