9a00e4ed2752ab3042a17c47800cc1bdd535ae87
Carson <dragonchen66@gmail.com>
2022-06-04 09:33:45 -0400

    practice: daily challenge and revisit

--- python/51.py
+++ python/51.py
@@ -2,20 +2,23 @@ from typing import List
 
 
 class Solution:
-    def __init__(self):
-        self.n = 0
-        self.solution = []
 
-    def dfs(self, queens, xy_sum, xy_dif):
-        row = len(queens)
-        if row == self.n:
-            self.solution.append(queens)
-            return
-        for col in range(self.n):
-            if col not in queens and row + col not in xy_sum and row - col not in xy_dif:
-                self.dfs(queens + [col], xy_sum + [row + col], xy_dif + [row - col])
+    def __init__(self):
+        self.queen_count = 0
+        self.res = []
 
     def solveNQueens(self, n: int) -> List[List[str]]:
-        self.n = n
+        self.queen_count = n
         self.dfs([], [], [])
-        return [['.' * i + 'Q' + '.' * (n - i - 1) for i in sol] for sol in self.solution]
+        return [['.' * i + 'Q' + '.' * (n - i - 1) for i in item] for item in self.res]
+
+    def dfs(self, queens, xy_sum, xy_diff):
+        row = len(queens)
+
+        if row == self.queen_count:
+            self.res.append(queens)
+            return
+
+        for col in range(self.queen_count):
+            if col not in queens and row + col not in xy_sum and row - col not in xy_diff:
+                self.dfs(queens + [col], xy_sum + [row + col], xy_diff + [row - col])
