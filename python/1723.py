e867f7df0e17d6eb1c95305ab5f6ed86739fa869
Carson <dragonchen66@gmail.com>
2022-06-06 10:07:13 -0400

    practice: daily challenge and revisit

--- python/1723.py
+++ python/1723.py
@@ -6,33 +6,33 @@ class Solution:
 
     def __init__(self):
         self.jobs = []
-        self.k = 0
+        self.bucket_count = 0
 
     def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
         self.jobs = jobs
-        self.k = k
+        self.bucket_count = k
 
         left, right = max(jobs), sum(jobs)
         while left <= right:
             mid = left + (right - left) // 2
-            if self.can_reach(0, [mid] * self.k, mid):
+            if self.can_reach(0, [mid] * k, mid):
                 right = mid - 1
             else:
                 left = mid + 1
 
         return left
 
-    def can_reach(self, curr_job_index, time, origin_val):
+    def can_reach(self, curr_job_index, bucket, origin_val):
         if curr_job_index == len(self.jobs):
             return True
 
-        for i in range(self.k):
-            if time[i] >= self.jobs[curr_job_index]:
-                time[i] -= self.jobs[curr_job_index]
-                if self.can_reach(curr_job_index + 1, time, origin_val):
+        for i in range(self.bucket_count):
+            if bucket[i] >= self.jobs[curr_job_index]:
+                bucket[i] -= self.jobs[curr_job_index]
+                if self.can_reach(curr_job_index + 1, bucket, origin_val):
                     return True
-                time[i] += self.jobs[curr_job_index]
-            if time[i] == origin_val:
+                bucket[i] += self.jobs[curr_job_index]
+            if bucket[i] == origin_val:
                 return False
 
         return False
