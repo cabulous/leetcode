e271cbdfdb32b6719de915c8e95dda28073181ae
Carson <dragonchen66@gmail.com>
2022-06-10 10:05:18 -0400

    practice: daily challenge and revisit

--- python/3.py
+++ python/3.py
@@ -1,11 +1,33 @@
+from collections import defaultdict
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        mp = {}
-        ans = 0
-        i = 0
-        for j in range(len(s)):
-            if s[j] in mp:
-                i = max(i, mp[s[j]])
-            ans = max(ans, j - i + 1)
-            mp[s[j]] = j + 1
-        return ans
+        seen = defaultdict(int)
+        res = 0
+        left = 0
+
+        for right in range(len(s)):
+            if s[right] in seen:
+                left = max(left, seen[s[right]])
+            res = max(res, right - left + 1)
+            seen[s[right]] = right + 1
+
+        return res
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        left = right = 0
+        res = 0
+
+        while right < len(s):
+            while s[right] in seen:
+                seen.discard(s[left])
+                left += 1
+            res = max(res, right - left + 1)
+            seen.add(s[right])
+            right += 1
+
+        return res
