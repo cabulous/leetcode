88627fab59ae3d6a0a32efc11b054fbe67a28a2b
Carson <dragonchen66@gmail.com>
2022-06-10 10:36:14 -0400

    practice: daily challenge and revisit

--- python/1298.py
+++ python/1298.py
@@ -6,16 +6,16 @@ class Solution:
     def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]],
                    initialBoxes: List[int]) -> int:
         boxes = set(initialBoxes)
-        opened_boxes = [i for i in boxes if status[i] == 1]
+        open_boxes = [i for i in boxes if status[i] == 1]
 
-        for box in opened_boxes:
+        for box in open_boxes:
             for next_box in containedBoxes[box]:
                 boxes.add(next_box)
                 if status[next_box] == 1:
-                    opened_boxes.append(next_box)
+                    open_boxes.append(next_box)
             for next_box in keys[box]:
                 if status[next_box] == 0 and next_box in boxes:
-                    opened_boxes.append(next_box)
+                    open_boxes.append(next_box)
                 status[next_box] = 1
 
-        return sum(candies[i] for i in opened_boxes)
+        return sum(candies[i] for i in open_boxes)
