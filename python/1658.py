d63b674cbd27d3ad41cc4291947d5c3681635c51
Carson <dragonchen66@gmail.com>
2022-06-11 10:23:10 -0400

    practice: daily challenge and revisit

--- python/1658.py
+++ python/1658.py
@@ -1,41 +1,42 @@
-from math import inf
+import math
 from typing import List
 
 
-# two pointer, indirectly
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        curr = sum(nums)
+        res = math.inf
+        left = 0
+
+        for right in range(len(nums)):
+            curr -= nums[right]
+            while curr < x and left <= right:
+                curr += nums[left]
+                left += 1
+            if curr == x:
+                res = min(res, len(nums) - (right - left + 1))
+
+        return res if res < math.inf else -1
+
+
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         total = sum(nums)
-        n = len(nums)
-        maxi = -1
+        length = self.min_operation_sub_array(nums, total - x)
+
+        return len(nums) - length if length != -1 else -1
+
+    def min_operation_sub_array(self, nums, target):
+        curr = 0
         left = 0
-        current = 0
+        res = -1
 
-        for right in range(n):
-            current += nums[right]
-            while current > total - x and left <= right:
-                current -= nums[left]
+        for right in range(len(nums)):
+            curr += nums[right]
+            while curr > target and left <= right:
+                curr -= nums[left]
                 left += 1
-            if current == total - x:
-                maxi = max(maxi, right - left + 1)
+            if curr == target:
+                res = max(res, right - left + 1)
 
-        return n - maxi if maxi != -1 else -1
-
-
-# two pointer, directly
-class Solution:
-    def minOperations(self, nums: List[int], x: int) -> int:
-        current = sum(nums)
-        n = len(nums)
-        mini = inf
-        left = 0
-
-        for right in range(n):
-            current -= nums[right]
-            while current < x and left <= right:
-                current += nums[left]
-                left += 1
-            if current == x:
-                mini = min(mini, left + (n - right - 1))
-
-        return mini if mini != inf else -1
+        return res
