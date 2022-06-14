63084429f5167767ea776e9c90dcfa710aa8f0f0
Carson <dragonchen66@gmail.com>
2022-06-14 10:40:38 -0400

    practice: daily challenge and revisit

--- python/1960.py
+++ python/1960.py
@@ -16,7 +16,7 @@ class Solution:
 
         for i in range(1, len(a) - 1):
             if i < right:
-                z[i] = min(right - i, z[2 * center - 1])
+                z[i] = min(right - i, z[2 * center - i])
             while a[i + z[i] + 1] == a[i - z[i] - 1]:
                 z[i] += 1
             if i + z[i] > right:
