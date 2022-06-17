1967c1d97012e2de4c258242b9e062fd39883e54
Carson <dragonchen66@gmail.com>
2022-06-17 10:05:41 -0400

    practice: daily challenge and revisit

--- python/968.py
+++ python/968.py
@@ -1,9 +1,13 @@
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
+IS_LEAF = 0
+IS_PARENT_OR_LEAF_WITH_CAMERA = 1
+IS_COVERED_WITHOUT_CAMERA = 2
 
 
 # https://leetcode.com/problems/binary-tree-cameras/discuss/211180/JavaC%2B%2BPython-Greedy-DFS
@@ -14,21 +18,20 @@ class Solution:
 
     def minCameraCover(self, root: TreeNode) -> int:
         ret = self.dfs(root)
-        return self.res + (ret == 0)
+        return self.res + (ret == IS_LEAF)
 
     def dfs(self, node):
         if node is None:
-            return 2
+            return IS_COVERED_WITHOUT_CAMERA
 
         left = self.dfs(node.left)
         right = self.dfs(node.right)
 
-        if left == 0 or right == 0:
+        if left == IS_LEAF or right == IS_LEAF:
             self.res += 1
-            return 1
+            return IS_PARENT_OR_LEAF_WITH_CAMERA
 
-        if left == 1 or right == 1:
-            return 2
+        if left == IS_PARENT_OR_LEAF_WITH_CAMERA or right == IS_PARENT_OR_LEAF_WITH_CAMERA:
+            return IS_COVERED_WITHOUT_CAMERA
 
-        return 0
-
+        return IS_LEAF
