13173264bf21b3496842e3a176d7bd2f958f52e2
Carson <dragonchen66@gmail.com>
2022-06-19 11:05:36 -0400

    practice: daily challenge and revisit

--- python/1585.py
+++ python/1585.py
@@ -1,19 +1,21 @@
+from collections import deque
+
+
 # https://leetcode.com/problems/check-if-string-is-transformable-with-substring-sort-operations/discuss/843917/C%2B%2BJavaPython-O(n)
 class Solution:
     def isTransformable(self, s: str, t: str) -> bool:
-        index = [[] for _ in range(10)]
-        position = [0] * 10
+        index = [deque() for _ in range(10)]
 
         for i, ch in enumerate(s):
             index[int(ch)].append(i)
 
         for ch in t:
             digit = int(ch)
-            if position[digit] >= len(index[digit]):
+            if not index[digit]:
                 return False
             for i in range(digit):
-                if position[i] < len(index[i]) and index[i][position[i]] < index[digit][position[digit]]:
+                if index[i] and index[i][0] < index[digit][0]:
                     return False
-            position[digit] += 1
+            index[digit].popleft()
 
         return True
