32d960d1525cb9c94820a53db07909b31e4ecfe9
Carson <dragonchen66@gmail.com>
2022-06-20 10:08:35 -0400

    practice: daily challenge and revisit

--- python/820.py
+++ python/820.py
@@ -5,21 +5,25 @@ from typing import List
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
         good = set(words)
+
         for word in set(words):
-            for k in range(1, len(word)):
-                good.discard(word[k:])
-        return sum(len(word) + 1 for word in good)
+            for i in range(1, len(word)):
+                good.discard(word[i:])
+
+        return sum(len(w) + 1 for w in good)
 
 
 # trie
 # https://leetcode.com/problems/short-encoding-of-words/discuss/125784/Trie-Solution
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
-        root = dict()
+        trie = dict()
         leaves = []
+
         for word in set(words):
-            cur = root
-            for i in word[::-1]:
-                cur[i] = cur = cur.get(i, dict())
-            leaves.append((cur, len(word) + 1))
+            curr = trie
+            for ch in word[::-1]:
+                curr[ch] = curr = curr.get(ch, dict())
+            leaves.append((curr, len(word) + 1))
+
         return sum(depth for node, depth in leaves if len(node) == 0)
