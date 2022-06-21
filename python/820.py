74458fc06279dcaf59be701c162a30731163ee81
Carson <dragonchen66@gmail.com>
2022-06-21 10:05:33 -0400

    practice: daily challenge and revisit

--- python/820.py
+++ python/820.py
@@ -1,7 +1,6 @@
 from typing import List
 
 
-# remove suffix words
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
         good = set(words)
@@ -11,19 +10,3 @@ class Solution:
                 good.discard(word[i:])
 
         return sum(len(w) + 1 for w in good)
-
-
-# trie
-# https://leetcode.com/problems/short-encoding-of-words/discuss/125784/Trie-Solution
-class Solution:
-    def minimumLengthEncoding(self, words: List[str]) -> int:
-        trie = dict()
-        leaves = []
-
-        for word in set(words):
-            curr = trie
-            for ch in word[::-1]:
-                curr[ch] = curr = curr.get(ch, dict())
-            leaves.append((curr, len(word) + 1))
-
-        return sum(depth for node, depth in leaves if len(node) == 0)
