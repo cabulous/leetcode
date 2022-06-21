383287c1ab1e5a6e2ebb7777c43bc8b80ca719a6
Carson <dragonchen66@gmail.com>
2022-06-21 10:46:37 -0400

    practice: daily challenge and revisit

--- python/2127.py
+++ python/2127.py
@@ -10,22 +10,24 @@ class Solution:
         seen = [False] * n
 
         for i in range(n):
-            if not seen[i]:
-                start = i
-                curr_people = i
-                curr_set = set()
+            if seen[i]:
+                continue
 
-                while not seen[curr_people]:
-                    seen[curr_people] = True
-                    curr_set.add(curr_people)
-                    curr_people = favorite[curr_people]
+            start = i
+            people = i
+            seen_people = set()
 
-                if curr_people in curr_set:
-                    curr_count = len(curr_set)
-                    while start != curr_people:
-                        curr_count -= 1
-                        start = favorite[start]
-                    max_count = max(max_count, curr_count)
+            while not seen[people]:
+                seen[people] = True
+                seen_people.add(people)
+                people = favorite[people]
+
+            if people in seen_people:
+                count = len(seen_people)
+                while start != people:
+                    count -= 1
+                    start = favorite[start]
+                max_count = max(max_count, count)
 
         pair = []
         visited = [False] * n
@@ -35,34 +37,30 @@ class Solution:
                 visited[i] = True
                 visited[favorite[i]] = True
 
-        child = defaultdict(list)
+        graph = defaultdict(list)
         for i in range(n):
-            child[favorite[i]].append(i)
+            graph[favorite[i]].append(i)
 
         res = 0
         for a, b in pair:
-            max_arm_a = 0
-            queue_a = deque()
-            for cand in child[a]:
-                if cand != b:
-                    queue_a.append([cand, 1])
-            while queue_a:
-                people, count = queue_a.popleft()
-                max_arm_a = max(max_arm_a, count)
-                for next_people in child[people]:
-                    queue_a.append([next_people, count + 1])
-
-            max_arm_b = 0
-            queue_b = deque()
-            for cand in child[b]:
-                if cand != a:
-                    queue_b.append([cand, 1])
-            while queue_b:
-                people, count = queue_b.popleft()
-                max_arm_b = max(max_arm_b, count)
-                for next_people in child[people]:
-                    queue_b.append([next_people, count + 1])
-
+            max_arm_a = self.arm_length(graph, a, b)
+            max_arm_b = self.arm_length(graph, b, a)
             res += 2 + max_arm_a + max_arm_b
 
         return max(max_count, res)
+
+    def arm_length(self, graph, start, exclude):
+        max_arm = 0
+        queue = deque()
+
+        for cand in graph[start]:
+            if cand != exclude:
+                queue.append([cand, 1])
+
+        while queue:
+            people, count = queue.popleft()
+            max_arm = max(max_arm, count)
+            for next_people in graph[people]:
+                queue.append([next_people, count + 1])
+
+        return max_arm
