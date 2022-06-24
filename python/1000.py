dd4ef29b623c6bcb24bdab44a541bdfb3ffd6569
Carson <dragonchen66@gmail.com>
2022-06-24 10:44:21 -0400

    practice: daily challenge and revisit

--- python/1000.py
+++ python/1000.py
@@ -10,15 +10,14 @@ class Solution:
 
     def mergeStones(self, stones: List[int], k: int) -> int:
         n = len(stones)
-
         if (n - 1) % (k - 1) != 0:
             return -1
 
-        self.k = k
         self.prefix_sum = [0] * (n + 1)
         for i in range(n):
             self.prefix_sum[i + 1] = self.prefix_sum[i] + stones[i]
 
+        self.k = k
         return self.dp(0, n - 1)
 
     @lru_cache(None)
@@ -26,7 +25,10 @@ class Solution:
         if right - left + 1 < self.k:
             return 0
 
-        res = min(self.dp(left, mid) + self.dp(mid + 1, right) for mid in range(left, right, self.k - 1))
+        res = min(
+            self.dp(left, mid) + self.dp(mid + 1, right)
+            for mid in range(left, right, self.k - 1)
+        )
 
         if (right - left) % (self.k - 1) == 0:
             res += self.prefix_sum[right + 1] - self.prefix_sum[left]
