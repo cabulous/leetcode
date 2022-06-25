46f00503765b9d7366d9010ea0c6e875d3b9d2ad
Carson <dragonchen66@gmail.com>
2022-06-25 10:01:04 -0400

    practice: daily challenge and revisit

--- python/665.py
+++ python/665.py
@@ -3,14 +3,17 @@ from typing import List
 
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        cnt = 0
+        count = 0
+
         for i in range(1, len(nums)):
             if nums[i - 1] > nums[i]:
-                cnt += 1
-                if cnt > 1:
+                count += 1
+                if count >= 2:
                     return False
+
                 if i < 2 or nums[i - 2] <= nums[i]:
                     nums[i - 1] = nums[i]
                 else:
                     nums[i] = nums[i - 1]
+
         return True
