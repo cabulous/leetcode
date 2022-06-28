aaf7682a695ff610ada30d7f4eee3f55c3a5df00
Carson <dragonchen66@gmail.com>
2022-06-28 10:16:49 -0400

    practice: daily challenge and revisit

--- python/1163.py
+++ python/1163.py
@@ -1,18 +1,19 @@
 # https://leetcode.com/problems/last-substring-in-lexicographical-order/discuss/363662/Short-python-code-O(n)-time-and-O(1)-space-with-proof-and-visualization
 class Solution:
     def lastSubstring(self, s: str) -> str:
-        i, j, k = 0, 1, 0
-        n = len(s)
+        i = 0
+        j = 1
+        delta = 0
 
-        while j + k < n:
-            if s[i + k] == s[j + k]:
-                k += 1
+        while j + delta < len(s):
+            if s[i + delta] == s[j + delta]:
+                delta += 1
                 continue
-            elif s[i + k] > s[j + k]:
-                j = j + k + 1
+            elif s[i + delta] > s[j + delta]:
+                j = j + delta + 1
             else:
-                i = max(i + k + 1, j)
+                i = max(i + delta + 1, j)
                 j = i + 1
-            k = 0
+            delta = 0
 
         return s[i:]
