4eb41d5c6b14f676afeec185062680726e63c7cd
Carson <dragonchen66@gmail.com>
2022-06-28 10:02:11 -0400

    practice: daily challenge and revisit

---
+++ python/1647.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        freq = [0] * 26
+        for ch in s:
+            freq[ord(ch) - ord('a')] += 1
+
+        freq.sort(reverse=True)
+
+        max_freq_allowed = len(s)
+        res = 0
+
+        for num in freq:
+            if num > max_freq_allowed:
+                res += num - max_freq_allowed
+            max_freq_allowed = max(0, min(num, max_freq_allowed) - 1)
+
+        return res
