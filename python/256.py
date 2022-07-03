f8f34f8b6b7af2be35aad31258961dcb9fa25132
Carson <dragonchen66@gmail.com>
2022-07-02 22:18:25 -0400

    practice: daily challenge and revisit

--- python/256.py
+++ python/256.py
@@ -5,16 +5,23 @@ from typing import List
 
 # memoization
 class Solution:
+
     def __init__(self):
         self.costs = []
 
     def minCost(self, costs: List[List[int]]) -> int:
         if not costs:
             return 0
-        self.costs = costs
-        return min(self.paint_cost(0, 0), self.paint_cost(0, 1), self.paint_cost(0, 2))
 
-    @lru_cache(2000)
+        self.costs = costs
+
+        return min(
+            self.paint_cost(0, 0),
+            self.paint_cost(0, 1),
+            self.paint_cost(0, 2),
+        )
+
+    @lru_cache(None)
     def paint_cost(self, house_idx, color):
         cost = self.costs[house_idx][color]
         if house_idx == len(self.costs) - 1:
@@ -34,12 +41,13 @@ class Solution:
         if not costs:
             return 0
 
-        for n in reversed(range(len(costs) - 1)):
-            costs[n][0] += min(costs[n + 1][1], costs[n + 1][2])
-            costs[n][1] += min(costs[n + 1][0], costs[n + 1][2])
-            costs[n][2] += min(costs[n + 1][0], costs[n + 1][1])
+        curr = copy.deepcopy(costs)
+        for i in reversed(range(len(costs) - 1)):
+            curr[i][0] += min(curr[i + 1][1], curr[i + 1][2])
+            curr[i][1] += min(curr[i + 1][0], curr[i + 1][2])
+            curr[i][2] += min(curr[i + 1][0], curr[i + 1][1])
 
-        return min(costs[0])
+        return min(curr[0])
 
 
 class Solution:
@@ -49,26 +57,11 @@ class Solution:
 
         prev = costs[-1]
 
-        for n in reversed(range(len(costs) - 1)):
-            curr = copy.deepcopy(costs[n])
+        for i in reversed(range(len(costs) - 1)):
+            curr = copy.deepcopy(costs[i])
             curr[0] += min(prev[1], prev[2])
             curr[1] += min(prev[0], prev[2])
             curr[2] += min(prev[0], prev[1])
             prev = curr
-
-        return min(prev)
-
-
-# https://leetcode.com/problems/paint-house/discuss/68209/1%2B-lines-Ruby-Python
-class Solution:
-    def minCost(self, costs: List[List[int]]) -> int:
-        if not costs:
-            return 0
-
-        cols = len(costs[0])
-        prev = [0] * cols
-
-        for curr in costs:
-            prev = [curr[i] + min(prev[:i] + prev[i + 1:]) for i in range(cols)]
 
         return min(prev)
