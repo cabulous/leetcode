93f35d045910dc21babfbaf810c23b8121ba4360
Carson <dragonchen66@gmail.com>
2022-07-05 10:34:43 -0400

    practice: daily challenge and revisit

--- python/2262.py
+++ python/2262.py
@@ -4,18 +4,6 @@ from collections import defaultdict
 # https://leetcode.com/problems/total-appeal-of-a-string/discuss/1996390/JavaC%2B%2BPython-Easy-and-Concise-with-Explanation
 class Solution:
     def appealSum(self, s: str) -> int:
-        last = {}
-        res = 0
-
-        for index, ch in enumerate(s):
-            last[ch] = index + 1
-            res += sum(last.values())
-
-        return res
-
-
-class Solution:
-    def appealSum(self, s: str) -> int:
         last = defaultdict(lambda: -1)
         n = len(s)
         res = 0
@@ -23,5 +11,17 @@ class Solution:
         for index, ch in enumerate(s):
             res += (index - last[ch]) * (n - index)
             last[ch] = index
+
+        return res
+
+
+class Solution:
+    def appealSum(self, s: str) -> int:
+        last = defaultdict(int)
+        res = 0
+
+        for index, ch in enumerate(s):
+            last[ch] = index + 1
+            res += sum(last.values())
 
         return res
