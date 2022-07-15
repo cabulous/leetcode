45ef3209c5c4139b49e946166d7f89ae9c4b5817
Carson <dragonchen66@gmail.com>
2022-07-15 10:01:55 -0400

    practice: daily challenge and revisit

--- python/695.py
+++ python/695.py
@@ -1,22 +1,34 @@
 from typing import List
 
 
-# dfs
 class Solution:
+
+    def __init__(self):
+        self.seen = set()
+        self.grid = []
+        self.rows = 0
+        self.cols = 0
+
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        assert len(grid) > 0 and len(grid[0]) > 0
+        if not any(grid):
+            return 0
 
-        max_row, max_col = len(grid), len(grid[0])
-        seen = set()
+        self.grid = grid
+        self.rows = len(grid)
+        self.cols = len(grid[0])
 
-        def dfs(row, col):
-            if row < 0 or row >= max_row or col < 0 or col >= max_col:
-                return 0
-            if (row, col) in seen:
-                return 0
-            seen.add((row, col))
-            if grid[row][col] == 0:
-                return 0
-            return 1 + dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
+        return max(self.helper(r, c) for r in range(self.rows) for c in range(self.cols))
 
-        return max(dfs(row, col) for row in range(max_row) for col in range(max_col))
+    def helper(self, r, c):
+        if r < 0 or self.rows <= r or c < 0 or self.cols <= c:
+            return 0
+
+        if (r, c) in self.seen:
+            return 0
+
+        self.seen.add((r, c))
+
+        if self.grid[r][c] == 0:
+            return 0
+
+        return 1 + self.helper(r - 1, c) + self.helper(r + 1, c) + self.helper(r, c - 1) + self.helper(r, c + 1)
