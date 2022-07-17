2c36ee123c9c9a0894bc43df4d468d9f2b050751
Carson <dragonchen66@gmail.com>
2022-07-17 15:04:13 -0400

    practice: daily challenge and revisit

--- python/629.py
+++ python/629.py
@@ -1,3 +1,6 @@
+MOD = 10 ** 9 + 7
+
+
 # https://leetcode.com/problems/k-inverse-pairs-array/discuss/104824/Python-concise-solution
 class Solution:
     def kInversePairs(self, n: int, k: int) -> int:
@@ -6,13 +9,11 @@ class Solution:
         if k == 0:
             return 1
 
-        modulo = 10 ** 9 + 7
         dp = [1] + [0] * k
-
         for i in range(2, n + 1):
             for j in range(1, k + 1):
                 dp[j] += dp[j - 1]
             for j in range(k, 0, -1):
                 dp[j] -= dp[j - i] if j - i >= 0 else 0
 
-        return dp[k] % modulo
+        return dp[k] % MOD
