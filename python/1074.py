07742ff17bbd18190f54c2a9c1767865786a1ce0
Carson <dragonchen66@gmail.com>
2022-07-18 10:04:29 -0400

    practice: daily challenge and revisit

--- python/1074.py
+++ python/1074.py
@@ -4,22 +4,23 @@ from collections import defaultdict
 
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
-        r, c = len(matrix), len(matrix[0])
-        ps = [[0] * (c + 1) for _ in range(r + 1)]
+        rows = len(matrix)
+        cols = len(matrix[0])
+        ps = [[0] * (cols + 1) for _ in range(rows + 1)]
 
-        for i in range(1, r + 1):
-            for j in range(1, c + 1):
-                ps[i][j] = ps[i - 1][j] + ps[i][j - 1] - ps[i - 1][j - 1] + matrix[i - 1][j - 1]
+        for r in range(1, rows + 1):
+            for c in range(1, cols + 1):
+                ps[r][c] = ps[r - 1][c] + ps[r][c - 1] - ps[r - 1][c - 1] + matrix[r - 1][c - 1]
 
-        count = 0
+        res = 0
 
-        for r1 in range(1, r + 1):
-            for r2 in range(r1, r + 1):
-                h = defaultdict(int)
-                h[0] = 1
-                for col in range(1, c + 1):
-                    curr_sum = ps[r2][col] - ps[r1 - 1][col]
-                    count += h[curr_sum - target]
-                    h[curr_sum] += 1
+        for r1 in range(1, rows + 1):
+            for r2 in range(r1, rows + 1):
+                count = defaultdict(int)
+                count[0] = 1
+                for c in range(1, cols + 1):
+                    curr_sum = ps[r2][c] - ps[r1 - 1][c]
+                    res += count[curr_sum - target]
+                    count[curr_sum] += 1
 
-        return count
+        return res
