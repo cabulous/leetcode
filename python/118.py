6e57882ec99aa7b29b039403c07a5f392eb7f121
Carson <dragonchen66@gmail.com>
2022-07-19 09:46:30 -0400

    practice: daily challenge and revisit

--- python/118.py
+++ python/118.py
@@ -3,12 +3,13 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = []
+        res = []
 
-        for row_num in range(numRows):
-            row = [1] * (row_num + 1)
-            for col in range(1, len(row) - 1):
-                row[col] = triangle[row_num - 1][col - 1] + triangle[row_num - 1][col]
-            triangle.append(row)
+        for r in range(numRows):
+            col_count = r + 1
+            row = [1] * col_count
+            for c in range(1, col_count - 1):
+                row[c] = res[r - 1][c - 1] + res[r - 1][c]
+            res.append(row)
 
-        return triangle
+        return res
