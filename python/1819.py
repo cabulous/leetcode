cf410376da4e96db9c18273985b1fa299442aec7
Carson <dragonchen66@gmail.com>
2022-07-21 10:16:44 -0400

    practice: daily challenge and revisit

---
+++ python/1819.py
@@ -0,0 +1,22 @@
+from math import gcd
+from typing import List
+
+
+# https://leetcode.com/problems/number-of-different-subsequences-gcds/discuss/1141361/Python-Python-2-solutions-%2B-2liner-explained
+class Solution:
+    def countDifferentSubsequenceGCDs(self, nums: List[int]) -> int:
+        max_num = max(nums)
+        nums_set = set(nums)
+        res = 0
+
+        for x in range(1, max_num + 1):
+            curr = 0
+            for y in range(x, max_num + 1, x):
+                if y in nums_set:
+                    curr = gcd(curr, y)
+                if curr == x:
+                    break
+            if curr == x:
+                res += 1
+
+        return res
--- python/92.py
+++ python/92.py
@@ -12,13 +12,11 @@ class Solution:
 
         sentinel = ListNode(0, head)
         tail = sentinel
-
         for _ in range(left - 1):
             tail = tail.next
 
         prev = tail
         curr = tail.next
-
         for _ in range(right - left + 1):
             curr.next, prev, curr = prev, curr, curr.next
 
