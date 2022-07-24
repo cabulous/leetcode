3bf25b7c11ad2b0aa160d28215af7d4f3ea3a915
Carson <dragonchen66@gmail.com>
2022-07-24 09:53:10 -0400

    practice: daily challenge and revisit

--- python/240.py
+++ python/240.py
@@ -3,18 +3,20 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:
+        if not any(matrix):
             return False
 
-        max_row, max_col = len(matrix), len(matrix[0])
-        r, c = max_row - 1, 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+        r = rows - 1
+        c = 0
 
-        while r >= 0 and c < max_col:
+        while r >= 0 and c < cols:
+            if matrix[r][c] == target:
+                return True
             if matrix[r][c] > target:
                 r -= 1
             elif matrix[r][c] < target:
                 c += 1
-            else:
-                return True
 
         return False
