64b608d87a685c924eeb225fb27f420b0c02d695
Carson <dragonchen66@gmail.com>
2022-07-25 10:48:03 -0400

    practice: daily challenge and revisit

--- python/502.py
+++ python/502.py
@@ -8,18 +8,18 @@ class Solution:
             return w + sum(heapq.nlargest(k, profits))
 
         projects = sorted(list(zip(capital, profits)), key=lambda x: -x[0])
-        projects_taken = []
-        projects_count = 0
+        candidates = []
+        taken_count = 0
         res = w
 
-        while projects_count < k:
+        while taken_count < k:
             while projects and projects[-1][0] <= res:
-                heapq.heappush(projects_taken, -projects.pop()[1])
-            if len(projects_taken) > 0:
-                max_profit = -heapq.heappop(projects_taken)
+                heapq.heappush(candidates, -projects.pop()[1])
+            if len(candidates) > 0:
+                max_profit = -heapq.heappop(candidates)
                 res += max_profit
             else:
                 break
-            projects_count += 1
+            taken_count += 1
 
         return res
