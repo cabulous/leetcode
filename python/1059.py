64a5c9e168400ddd4d39788aa040891c2b151cd4
Carson <dragonchen66@gmail.com>
2022-07-26 10:31:41 -0400

    practice: daily challenge and revisit

--- python/1059.py
+++ python/1059.py
@@ -7,7 +7,6 @@ class Solution:
 
     def __init__(self):
         self.graph = defaultdict(list)
-        self.seen = set()
         self.destination = 0
 
     def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
@@ -16,20 +15,20 @@ class Solution:
 
         self.destination = destination
 
-        return self.reachable(source)
+        return self.reachable(source, set())
 
-    def reachable(self, node):
+    def reachable(self, node, seen):
         if len(self.graph[node]) == 0:
             return node == self.destination
 
-        self.seen.add(node)
+        seen.add(node)
 
         for next_node in self.graph[node]:
-            if next_node in self.seen:
+            if next_node in seen:
                 return False
-            if not self.reachable(next_node):
+            if not self.reachable(next_node, seen):
                 return False
 
-        self.seen.discard(node)
+        seen.remove(node)
 
         return True
