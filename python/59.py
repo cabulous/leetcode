2efff6035cb4f1c73daa454477085ba5e08bf2fd
Carson <dragonchen66@gmail.com>
2022-07-28 19:32:15 -0400

    practice: daily challenge and revisit

--- python/59.py
+++ python/59.py
@@ -4,7 +4,8 @@ from typing import List
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         res = [[0] * n for _ in range(n)]
-        row, col = 0, 0
+        row = 0
+        col = 0
         dr, dc = 0, 1
 
         for num in range(1, n * n + 1):
