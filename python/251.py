08e2f6387c43246efdf78fe0efbf63e925274fb8
Carson <dragonchen66@gmail.com>
2022-07-29 10:47:57 -0400

    practice: daily challenge and revisit

---
+++ python/251.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+# https://leetcode.com/problems/flatten-2d-vector/discuss/168602/Python-iterator-Solution/934803
+class Vector2D:
+
+    def __init__(self, vec: List[List[int]]):
+        self.gen = self._generator(vec)
+        self.next_val = next(self.gen, None)
+
+    def _generator(self, vec: List[List[int]]):
+        for row in vec:
+            for c in row:
+                yield c
+
+    def next(self) -> int:
+        res = self.next_val
+        self.next_val = next(self.gen, None)
+        return res
+
+    def hasNext(self) -> bool:
+        return self.next_val is not None
