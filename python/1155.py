319ab15950283450cbc2fea330524c73360a53fa
Carson <dragonchen66@gmail.com>
2022-07-30 19:03:32 -0400

    practice: daily challenge and revisit

--- python/1155.py
+++ python/1155.py
@@ -1,6 +1,7 @@
 MOD = 10 ** 9 + 7
 
 
+# https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/discuss/355894/Python-DP-with-memoization-explained
 class Solution:
 
     def __init__(self):
@@ -11,17 +12,17 @@ class Solution:
         self.k = k
         return self.dp(n, target) % MOD
 
-    def dp(self, dice_remain, target_remain):
-        if dice_remain == 0:
-            return 0 if target_remain > 0 else 1
+    def dp(self, dice_count, target):
+        if dice_count == 0:
+            return 0 if target > 0 else 1
 
-        if (dice_remain, target_remain) in self.memo:
-            return self.memo[dice_remain, target_remain]
+        if (dice_count, target) in self.memo:
+            return self.memo[dice_count, target]
 
         res = 0
-        for k in range(max(0, target_remain - self.k), target_remain):
-            res += self.dp(dice_remain - 1, k)
+        for next_target in range(max(0, target - self.k), target):
+            res += self.dp(dice_count - 1, next_target)
 
-        self.memo[dice_remain, target_remain] = res
+        self.memo[dice_count, target] = res
 
         return res
