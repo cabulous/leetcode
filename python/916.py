67a3f24cee541bc18647f9126ebb104c0d6f73c7
Carson <dragonchen66@gmail.com>
2022-07-30 16:07:17 -0400

    practice: daily challenge and revisit

--- python/916.py
+++ python/916.py
@@ -6,8 +6,8 @@ class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
         counter = Counter()
         for word in words2:
-            next_counter = Counter(word)
-            for ch, count in next_counter.items():
+            word_counter = Counter(word)
+            for ch, count in word_counter.items():
                 counter[ch] = max(counter[ch], count)
 
         return [w for w in words1 if not counter - Counter(w)]
@@ -16,8 +16,8 @@ class Solution:
 # https://leetcode.com/problems/word-subsets/discuss/175854/JavaC%2B%2BPython-Straight-Forward
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        count = Counter()
-        for w in words2:
-            count |= Counter(w)
+        counter = Counter()
+        for word in words2:
+            counter |= Counter(word)
 
-        return [w for w in words1 if not count - Counter(w)]
+        return [w for w in words1 if not counter - Counter(w)]
