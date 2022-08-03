6a2836bc39da866e3114ab6f0244b6ee2526aecb
Carson <dragonchen66@gmail.com>
2022-08-03 11:27:58 -0400

    practice: daily challenge and revisit

--- python/1071.py
+++ python/1071.py
@@ -1,8 +1,11 @@
 # https://leetcode.com/problems/greatest-common-divisor-of-strings/discuss/303781/JavaPython-3-3-codes-each%3A-Recursive-iterative-and-regex-w-brief-comments-and-analysis.
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if not str1 or not str2:
-            return str1 if str1 else str2
+        if not str1:
+            return str2
+
+        if not str2:
+            return str1
 
         if len(str1) < len(str2):
             return self.gcdOfStrings(str2, str1)
