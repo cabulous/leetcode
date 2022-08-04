6c79df4b287b15702d5aed52584bc9abab645bc0
Carson <dragonchen66@gmail.com>
2022-08-04 12:52:45 -0400

    practice: daily challenge and revisit

--- python/916.py
+++ python/916.py
@@ -4,20 +4,20 @@ from collections import Counter
 
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        counter = Counter()
+        count = Counter()
         for word in words2:
-            word_counter = Counter(word)
-            for ch, count in word_counter.items():
-                counter[ch] = max(counter[ch], count)
+            curr_count = Counter(word)
+            for ch, ch_count in curr_count.items():
+                count[ch] = max(count[ch], ch_count)
 
-        return [w for w in words1 if not counter - Counter(w)]
+        return [w for w in words1 if not count - Counter(w)]
 
 
 # https://leetcode.com/problems/word-subsets/discuss/175854/JavaC%2B%2BPython-Straight-Forward
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        counter = Counter()
+        count = Counter()
         for word in words2:
-            counter |= Counter(word)
+            count |= Counter(word)
 
-        return [w for w in words1 if not counter - Counter(w)]
+        return [w for w in words1 if not count - Counter(w)]
