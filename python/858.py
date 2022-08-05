c4b8eebf4b3aa0b840e7e3359812044b436c98bc
Carson <dragonchen66@gmail.com>
2022-08-05 09:59:27 -0400

    practice: daily challenge and revisit

--- python/858.py
+++ python/858.py
@@ -4,11 +4,11 @@ import math
 class Solution:
     def mirrorReflection(self, p: int, q: int) -> int:
         bounces = p // math.gcd(p, q)
-        if bounces % 2 == 1:
-            if bounces * q // p % 2 == 1:
-                return 1
-            return 0
-        return 2
+        if bounces % 2 == 0:
+            return 2
+        if bounces * q // p % 2 == 1:
+            return 1
+        return 0
 
 
 # https://leetcode.com/problems/mirror-reflection/discuss/141773/C%2B%2BJavaPython-1-line-without-using-any-package-or
