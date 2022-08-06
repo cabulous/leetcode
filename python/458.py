1ee259bc5603b6624399fc781aa42190908daa52
Carson <dragonchen66@gmail.com>
2022-08-05 21:30:32 -0400

    practice: daily challenge and revisit

--- python/458.py
+++ python/458.py
@@ -5,6 +5,6 @@ import math
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
         pigs = 0
-        while (minutesToTest // minutesToDie + 1) ** pigs < buckets:
+        while ((minutesToTest // minutesToDie) + 1) ** pigs < buckets:
             pigs += 1
         return pigs
