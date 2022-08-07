5a0f8901bca73c1b688ef79c33c7e996fbc0004f
Carson <dragonchen66@gmail.com>
2022-08-07 15:13:42 -0400

    practice: daily challenge and revisit

---
+++ python/1466.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from typing import List
+
+
+# https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/discuss/661774/Python3-Easy-Short-DFS
+class Solution:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.roads = set()
+        self.res = 0
+
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        for u, v in connections:
+            self.roads.add((u, v))
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+
+        self.dfs(-1, 0)
+
+        return self.res
+
+    def dfs(self, prev_node, curr_node):
+        self.res += (prev_node, curr_node) in self.roads
+        for next_node in self.graph[curr_node]:
+            if next_node == prev_node:
+                continue
+            self.dfs(curr_node, next_node)
