497f064d00a013d691b4891df91e572efb7e6391
Carson <dragonchen66@gmail.com>
2022-08-08 10:12:23 -0400

    practice: daily challenge and revisit

--- python/314.py
+++ python/314.py
@@ -22,3 +22,27 @@ class Solution:
                 queue.append((node.right, idx + 1))
 
         return [cols[i] for i in sorted(cols)]
+
+
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        cols = defaultdict(list)
+        min_col = 0
+        max_col = 0
+        queue = deque([(root, 0)])
+
+        while queue:
+            node, idx = queue.popleft()
+            if node is not None:
+                cols[idx].append(node.val)
+
+                min_col = min(min_col, idx)
+                max_col = max(max_col, idx)
+
+                queue.append((node.left, idx - 1))
+                queue.append((node.right, idx + 1))
+
+        return [cols[i] for i in range(min_col, max_col + 1)]
