9e64e34651d1521bbd558dffb184897de56c76e3
Carson <dragonchen66@gmail.com>
2022-08-08 10:32:25 -0400

    practice: daily challenge and revisit

--- python/629.py
+++ python/629.py
@@ -14,7 +14,7 @@ class Solution:
             for j in range(1, k + 1):
                 dp[j] += dp[j - 1]
             for j in range(k, 0, -1):
-                if j >= i:
+                if j - i >= 0:
                     dp[j] -= dp[j - i]
 
         return dp[k] % MOD
