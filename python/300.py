b7c7c0cda5ca28d7b070885d05cb7c7d4496a1da
Carson <dragonchen66@gmail.com>
2022-08-08 09:52:30 -0400

    practice: daily challenge and revisit

--- python/300.py
+++ python/300.py
@@ -1,29 +1,27 @@
 import bisect
 
 
-# DP
+class Solution:
+    def lengthOfLIS(self, nums):
+        res = []
+
+        for num in nums:
+            idx = bisect.bisect_left(res, num)
+            if idx == len(res):
+                res.append(num)
+            else:
+                res[idx] = num
+
+        return len(res)
+
+
 class Solution:
     def lengthOfLIS(self, nums):
         dp = [1] * len(nums)
 
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+        for end in range(1, len(nums)):
+            for start in range(end):
+                if nums[start] < nums[end]:
+                    dp[end] = max(dp[end], dp[start] + 1)
 
         return max(dp)
-
-
-# binary search
-class Solution:
-    def lengthOfLIS(self, nums):
-        sub = []
-
-        for num in nums:
-            idx = bisect.bisect_left(sub, num)
-            if idx == len(sub):
-                sub.append(num)
-            else:
-                sub[idx] = num
-
-        return len(sub)
