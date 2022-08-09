421b7e6ec9249a2d2a7fd33e3613eaecdbdddfc6
Carson <dragonchen66@gmail.com>
2022-08-09 10:00:41 -0400

    practice: daily challenge and revisit

--- python/823.py
+++ python/823.py
@@ -1,14 +1,13 @@
 from typing import List
-from collections import defaultdict
 
 
 # https://leetcode.com/problems/binary-trees-with-factors/discuss/125794/C%2B%2BJavaPython-DP-solution
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         mod = 10 ** 9 + 7
-        dp = defaultdict(int)
+        dp = {}
 
         for a in sorted(arr):
-            dp[a] = 1 + sum(dp[b] * dp.get(a / b, 0) for b in dp if a % b == 0)
+            dp[a] = 1 + sum(dp[b] * dp.get(a // b, 0) for b in dp if a % b == 0)
 
         return sum(dp.values()) % mod
