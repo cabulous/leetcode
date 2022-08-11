d8f5c09d1ac7eb824760e0a751b457b83a9c6e36
Carson <dragonchen66@gmail.com>
2022-08-11 09:43:25 -0400

    practice: daily challenge and revisit

--- python/98.py
+++ python/98.py
@@ -1,57 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 import math
+from typing import Optional
 
 
-#  BST
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    def isValidBST(self, root) -> bool:
-        def bst(node, lo=-math.inf, hi=math.inf):
-            if not node:
-                return True
-            if node.val <= lo or node.val >= hi:
-                return False
-            return bst(node.left, lo, node.val) and bst(node.right, node.val, hi)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.helper(root, -math.inf, math.inf)
 
-        return bst(root)
-
-
-# Traveral
-class Solution:
-    def isValidBST(self, root) -> bool:
-        if not root:
+    def helper(self, node, lo, hi):
+        if node is None:
             return True
 
-        stack = [(root, -math.inf, math.inf)]
-        while stack:
-            node, lo, hi = stack.pop()
-            if not node:
-                continue
-            if node.val <= lo or node.val >= hi:
-                return False
-            stack.append((node.left, lo, node.val))
-            stack.append((node.right, node.val, hi))
-        return True
+        if node.val <= lo or hi <= node.val:
+            return False
 
-
-# inorder
-class Solution:
-    def __init__(self):
-        self.prev = -math.inf
-
-    def isValidBST(self, root) -> bool:
-        def inorder(node):
-            if not node:
-                return True
-            if not inorder(node.left):
-                return False
-            if node.val <= self.prev:
-                return False
-            self.prev = node.val
-            return inorder(node.right)
-
-        return inorder(root)
+        return self.helper(node.left, lo, node.val) and self.helper(node.right, node.val, hi)
