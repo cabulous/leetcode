c1bbb0e13a1f157b168f348843021e21150cefef
Carson <dragonchen66@gmail.com>
2022-08-13 10:11:30 -0400

    practice: daily challenge and revisit

--- python/30.py
+++ python/30.py
@@ -5,24 +5,24 @@ from typing import List
 # https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/13669/99ms-Python-O(kmn)-Solution
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        word_counter = Counter(words)
-        target_word_count = len(words)
+        word_count = Counter(words)
         word_len = len(words[0])
+        target_word_count = len(words)
         res = []
 
         for i in range(word_len):
             left = i
-            sub_counter = Counter()
+            curr_count = Counter()
             count = 0
 
             for j in range(i, len(s) - word_len + 1, word_len):
                 word = s[j:j + word_len]
 
-                if word in word_counter:
-                    sub_counter[word] += 1
+                if word in word_count:
+                    curr_count[word] += 1
                     count += 1
-                    while sub_counter[word] > word_counter[word]:
-                        sub_counter[s[left:left + word_len]] -= 1
+                    while curr_count[word] > word_count[word]:
+                        curr_count[s[left:left + word_len]] -= 1
                         count -= 1
                         left += word_len
                     if count == target_word_count:
@@ -30,7 +30,7 @@ class Solution:
 
                 else:
                     left = j + word_len
-                    sub_counter = Counter()
+                    curr_count = Counter()
                     count = 0
 
         return res
