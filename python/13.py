b256c430b0871df6f1b86e32c3c5c18fe21e3749
Carson <dragonchen66@gmail.com>
2022-08-15 10:00:39 -0400

    practice: daily challenge and revisit

--- python/13.py
+++ python/13.py
@@ -1,50 +1,51 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
         }
 
-        total = values[s[-1]]
-        for i in reversed(range(len(s) - 1)):
+        res = values[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
             if values[s[i]] < values[s[i + 1]]:
-                total -= values[s[i]]
+                res -= values[s[i]]
             else:
-                total += values[s[i]]
-        return total
+                res += values[s[i]]
+
+        return res
 
 
 class Solution:
     def romanToInt(self, s: str) -> int:
         values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
-            "IV": 4,
-            "IX": 9,
-            "XL": 40,
-            "XC": 90,
-            "CD": 400,
-            "CM": 900
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            'IV': 4,
+            'IX': 9,
+            'XL': 40,
+            'XC': 90,
+            'CD': 400,
+            'CM': 900
         }
 
-        total = 0
+        res = 0
         i = 0
-        n = len(s)
-        while i < n:
-            if i < n - 1 and s[i:i + 2] in values:
-                total += values[s[i:i + 2]]
+        while i < len(s):
+            if i < len(s) - 1 and s[i:i + 2] in values:
+                res += values[s[i:i + 2]]
                 i += 2
             else:
-                total += values[s[i]]
+                res += values[s[i]]
                 i += 1
-        return total
+
+        return res
