5278288a0c69050587ed2f5b1d910e44cc1a98f6
Carson <dragonchen66@gmail.com>
2022-08-16 10:10:17 -0400

    practice: daily challenge and revisit

---
+++ python/1570.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.non_zeros = {}
+        for i, num in enumerate(nums):
+            if num != 0:
+                self.non_zeros[i] = num
+
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        res = 0
+
+        for i, num in self.non_zeros.items():
+            if i in vec.non_zeros:
+                res += num * vec.non_zeros[i]
+
+        return res
