51aba1d840ac15064d6d95825554ad91a2bd5ab8
Carson <dragonchen66@gmail.com>
2022-08-21 07:16:14 -0400

    practice: daily challenge and revisit

--- python/936.py
+++ python/936.py
@@ -1,32 +1,42 @@
 from typing import List
 
 
-# Work Backwards
 # https://leetcode.com/problems/stamping-the-sequence/discuss/189254/Python-Greedy-and-DFS
 class Solution:
-    def movesToStamp(self, stamp: str, target: str) -> List[int]:
-        m, n = len(stamp), len(target)
-        s, t = list(stamp), list(target)
-        res = []
 
-        def check(i):
-            changed = False
-            for j in range(m):
-                if t[i + j] == '?':
-                    continue
-                if t[i + j] != s[j]:
-                    return False
-                changed = True
-            if changed:
-                t[i:i + m] = ['?'] * m
-                res.append(i)
-            return changed
+    def __init__(self):
+        self.stamp = []
+        self.stamp_len = 0
+        self.target = []
+        self.target_len = 0
+        self.res = []
+
+    def movesToStamp(self, stamp: str, target: str) -> List[int]:
+        self.stamp = list(stamp)
+        self.stamp_len = len(stamp)
+        self.target = list(target)
+        self.target_len = len(target)
 
         changed = True
-
         while changed:
             changed = False
-            for i in range(n - m + 1):
-                changed |= check(i)
+            for i in range(self.target_len - self.stamp_len + 1):
+                changed |= self.check(i)
 
-        return res[::-1] if t == ['?'] * n else []
+        return self.res[::-1] if self.target == ['?'] * self.target_len else []
+
+    def check(self, curr_idx):
+        changed = False
+
+        for i in range(self.stamp_len):
+            if self.target[curr_idx + i] == '?':
+                continue
+            if self.target[curr_idx + i] != self.stamp[i]:
+                return False
+            changed = True
+
+        if changed:
+            self.target[curr_idx:curr_idx + self.stamp_len] = ['?'] * self.stamp_len
+            self.res.append(curr_idx)
+
+        return changed
