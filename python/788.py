f789e6160963ed903119165fcff52afa6da99569
Carson <dragonchen66@gmail.com>
2022-08-23 11:05:52 -0400

    practice: daily challenge and revisit

---
+++ python/788.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/rotated-digits/discuss/116530/JavaPython-O(logN)-Time-O(1)-Space
+class Solution:
+
+    def __init__(self):
+        self.s1 = {1, 8, 0}
+        self.s2 = {1, 8, 0, 2, 5, 6, 9}
+
+    def rotatedDigits(self, n: int) -> int:
+        return sum(self.is_good(num) for num in range(1, n + 1))
+
+    def is_good(self, num):
+        s = set([int(i) for i in str(num)])
+        return s.issubset(self.s2) and not s.issubset(self.s1)
