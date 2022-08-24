fa31eb192b187ef4964ff0a975eada5b6f20b82c
Carson <dragonchen66@gmail.com>
2022-08-24 09:50:41 -0400

    practice: daily challenge and revisit

--- python/788.py
+++ python/788.py
@@ -2,12 +2,12 @@
 class Solution:
 
     def __init__(self):
-        self.s1 = {1, 8, 0}
-        self.s2 = {1, 8, 0, 2, 5, 6, 9}
+        self.s1 = {0, 1, 8}
+        self.s2 = {0, 1, 8, 2, 5, 6, 9}
 
     def rotatedDigits(self, n: int) -> int:
         return sum(self.is_good(num) for num in range(1, n + 1))
 
     def is_good(self, num):
-        s = set([int(i) for i in str(num)])
+        s = set(int(i) for i in str(num))
         return s.issubset(self.s2) and not s.issubset(self.s1)
