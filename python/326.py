472cd1d07cdf00b64a847fe906a11ccb7696fee4
Carson <dragonchen66@gmail.com>
2022-08-24 10:16:53 -0400

    practice: daily challenge and revisit

--- python/326.py
+++ python/326.py
@@ -3,7 +3,7 @@ class Solution:
         if n < 1:
             return False
         while n % 3 == 0:
-            n /= 3
+            n //= 3
         return n == 1
 
 
--- python/871.py
+++ python/871.py
@@ -29,15 +29,15 @@ class Solution:
 # dp
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        dp = [startFuel] + [0] * len(stations)
+        dist = [startFuel] + [0] * len(stations)
 
         for i, (location, capacity) in enumerate(stations):
-            for t in range(i, -1, -1):
-                if dp[t] >= location:
-                    dp[t + 1] = max(dp[t + 1], dp[t] + capacity)
+            for refuel in range(i, -1, -1):
+                if dist[refuel] >= location:
+                    dist[refuel + 1] = max(dist[refuel + 1], dist[refuel] + capacity)
 
-        for i, d in enumerate(dp):
-            if d >= target:
-                return i
+        for refuel, location in enumerate(dist):
+            if location >= target:
+                return refuel
 
         return -1
