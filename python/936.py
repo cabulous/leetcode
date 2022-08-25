b19ca2830f780bda2a8c893a888a26a86b16307d
Carson <dragonchen66@gmail.com>
2022-08-25 09:33:25 -0400

    practice: daily challenge and revisit

--- python/936.py
+++ python/936.py
@@ -6,29 +6,25 @@ class Solution:
 
     def __init__(self):
         self.stamp = []
-        self.stamp_len = 0
         self.target = []
-        self.target_len = 0
         self.res = []
 
     def movesToStamp(self, stamp: str, target: str) -> List[int]:
         self.stamp = list(stamp)
-        self.stamp_len = len(stamp)
         self.target = list(target)
-        self.target_len = len(target)
 
         changed = True
         while changed:
             changed = False
-            for i in range(self.target_len - self.stamp_len + 1):
+            for i in range(len(self.target) - len(self.stamp) + 1):
                 changed |= self.check(i)
 
-        return self.res[::-1] if self.target == ['?'] * self.target_len else []
+        return self.res[::-1] if self.target == ['?'] * len(self.target) else []
 
     def check(self, curr_idx):
         changed = False
 
-        for i in range(self.stamp_len):
+        for i in range(len(self.stamp)):
             if self.target[curr_idx + i] == '?':
                 continue
             if self.target[curr_idx + i] != self.stamp[i]:
@@ -36,7 +32,7 @@ class Solution:
             changed = True
 
         if changed:
-            self.target[curr_idx:curr_idx + self.stamp_len] = ['?'] * self.stamp_len
+            self.target[curr_idx:curr_idx + len(self.stamp)] = ['?'] * len(self.stamp)
             self.res.append(curr_idx)
 
         return changed
