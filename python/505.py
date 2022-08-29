788c9c7cf215020b0ad348f79c9e85c971a637a2
Carson <dragonchen66@gmail.com>
2022-08-29 10:03:48 -0400

    practice: daily challenge and revisit

--- python/505.py
+++ python/505.py
@@ -5,23 +5,26 @@ from typing import List
 # https://leetcode.com/problems/the-maze-ii/discuss/150536/Python-simple-and-elegant-PriorityQueue-solution-beats-97
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
-        max_row, max_col = len(maze), len(maze[0])
+        rows = len(maze)
+        cols = len(maze[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
         queue = [(0, start[0], start[1])]
         stopped = {(start[0], start[1]): 0}
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
         while queue:
-            dist, row, col = heapq.heappop(queue)
-            if [row, col] == destination:
+            dist, r, c = heapq.heappop(queue)
+            if [r, c] == destination:
                 return dist
+
             for dr, dc in directions:
-                nd, nr, nc = dist, row, col
-                while 0 <= nr + dr < max_row and 0 <= nc + dc < max_col and maze[nr + dr][nc + dc] == 0:
+                nd, nr, nc = dist, r, c
+                while 0 <= nr + dr < rows and 0 <= nc + dc < cols and maze[nr + dr][nc + dc] == 0:
+                    nd += 1
                     nr += dr
                     nc += dc
-                    nd += 1
-                if (nr, nc) not in stopped or nd < stopped[(nr, nc)]:
-                    stopped[(nr, nc)] = nd
+                if (nr, nc) not in stopped or nd < stopped[nr, nc]:
+                    stopped[nr, nc] = nd
                     heapq.heappush(queue, (nd, nr, nc))
 
         return -1
