5abe4a10a43d152e080c6b6a5e361bd0faa9cfd7
Carson <dragonchen66@gmail.com>
2022-08-30 09:54:12 -0400

    practice: daily challenge and revisit

--- python/48.py
+++ python/48.py
@@ -17,40 +17,15 @@ class Solution:
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix)
-        for i in range(n // 2):
-            for j in range(n - n // 2):
-                matrix[i][j], matrix[~j][i], matrix[~i][~j], matrix[j][~i] = \
-                    matrix[~j][i], matrix[~i][~j], matrix[j][~i], matrix[i][j]
-
-
-# Reverse on Diagonal and then Reverse Left to Right
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
         self.transpose(matrix)
         self.reflect(matrix)
 
     def transpose(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(i, n):
+        for i in range(len(matrix)):
+            for j in range(i, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     def reflect(self, matrix):
-        n = len(matrix)
-        for i in range(n):
-            for j in range(n // 2):
+        for i in range(len(matrix)):
+            for j in range(len(matrix) // 2):
                 matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
-
-
-# Rotate Groups of Four Cells
-class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        n = len(matrix[0])
-        for i in range(n // 2 + n % 2):
-            for j in range(n // 2):
-                tmp = matrix[n - 1 - j][j]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = tmp
