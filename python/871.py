11566f558b295b36fc01436c4d3178810a33aa3d
Carson <dragonchen66@gmail.com>
2022-09-12 10:24:54 -0400

    practice: daily challenge and revisit

--- python/871.py
+++ python/871.py
@@ -8,19 +8,19 @@ class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         stations.append([target, math.inf])
 
-        max_heap = []
         tank = startFuel
         prev_location = 0
+        fuel_max_heap = []
         res = 0
 
         for location, fuel in stations:
             tank -= location - prev_location
-            while tank < 0 and max_heap:
-                tank += -heapq.heappop(max_heap)
+            while tank < 0 and fuel_max_heap:
+                tank += -heapq.heappop(fuel_max_heap)
                 res += 1
             if tank < 0:
                 return -1
-            heapq.heappush(max_heap, -fuel)
+            heapq.heappush(fuel_max_heap, -fuel)
             prev_location = location
 
         return res
@@ -31,10 +31,10 @@ class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         dist = [startFuel] + [0] * len(stations)
 
-        for i, (location, capacity) in enumerate(stations):
+        for i, (location, fuel) in enumerate(stations):
             for refuel in range(i, -1, -1):
                 if dist[refuel] >= location:
-                    dist[refuel + 1] = max(dist[refuel + 1], dist[refuel] + capacity)
+                    dist[refuel + 1] = max(dist[refuel + 1], dist[refuel] + fuel)
 
         for refuel, location in enumerate(dist):
             if location >= target:
