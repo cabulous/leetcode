039ca5e3d57508985a224572eec6822bcd538d66
Carson <dragonchen66@gmail.com>
2022-09-15 10:00:49 -0400

    practice: daily challenge and revisit

---
+++ python/2007.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import List
+
+
+# https://leetcode.com/problems/find-original-array-from-doubled-array/discuss/1470959/JavaC%2B%2BPython-Match-from-the-Smallest-or-Biggest-100
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 == 1:
+            return []
+
+        count = Counter(changed)
+        for num in sorted(count):
+            if count[num] > count[2 * num]:
+                return []
+            count[2 * num] -= count[num] // 2 if num == 0 else count[num]
+
+        return list(count.elements())
