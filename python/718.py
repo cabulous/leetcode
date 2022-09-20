11777f788fcbaeb879d0fa44bcada58669da5d10
Carson <dragonchen66@gmail.com>
2022-09-20 09:36:53 -0400

    practice: daily challenge and revisit

--- python/718.py
+++ python/718.py
@@ -3,11 +3,10 @@ from typing import List
 
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        m, n = len(nums1), len(nums2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
 
-        for i in reversed(range(m)):
-            for j in reversed(range(n)):
+        for i in range(len(nums1) - 1, -1, -1):
+            for j in range(len(nums2) - 1, -1, -1):
                 if nums1[i] == nums2[j]:
                     dp[i][j] = dp[i + 1][j + 1] + 1
 
