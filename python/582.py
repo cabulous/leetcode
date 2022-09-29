5dd9ac9d05ea46e949eb1810c9acb6ea0626b9d5
Carson <dragonchen66@gmail.com>
2022-09-29 10:08:14 -0400

    practice: daily challenge and revisit

--- python/582.py
+++ python/582.py
@@ -5,10 +5,18 @@ from collections import defaultdict
 # https://leetcode.com/problems/kill-process/discuss/103170/Simple-Python-BFS-Solution
 class Solution:
     def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
-        d = defaultdict(list)
-        for c, p in zip(pid, ppid):
-            d[p].append(c)
-        bfs = [kill]
-        for i in bfs:
-            bfs.extend(d[i])
-        return bfs
+        graph = defaultdict(list)
+        for parent, child in zip(ppid, pid):
+            graph[parent].append(child)
+
+        res = []
+        queue = [kill]
+        while queue:
+            next_queue = []
+            for node in queue:
+                res.append(node)
+                for child in graph[node]:
+                    next_queue.append(child)
+            queue = next_queue
+
+        return res
