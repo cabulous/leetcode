dfdc7811113bc70ccc56d186e60d7c527679c70e
Carson <dragonchen66@gmail.com>
2022-10-01 20:07:59 -0400

    practice: daily challenge and revisit

--- python/531.py
+++ python/531.py
@@ -4,10 +4,10 @@ from typing import List
 # https://leetcode.com/problems/lonely-pixel-i/discuss/790470/Python3-3-lines-Lonely-Pixel-I
 class Solution:
     def findLonelyPixel(self, picture: List[List[str]]) -> int:
-        rows = [sum(col == 'B' for col in row) for row in picture]
-        cols = [sum(row == 'B' for row in col) for col in zip(*picture)]
+        rows = [sum(cell == 'B' for cell in row) for row in picture]
+        cols = [sum(cell == 'B' for cell in col) for col in zip(*picture)]
         return sum(
-            picture[r][c] == 'B' and rows[r] == 1 and cols[c] == 1
+            picture[r][c] == 'B' and rows[r] == cols[c] == 1
             for r in range(len(picture))
             for c in range(len(picture[r]))
         )
