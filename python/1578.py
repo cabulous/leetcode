3e0df604577dd3528991023cf2419b3659d97575
Carson <dragonchen66@gmail.com>
2022-10-03 09:49:19 -0400

    practice: daily challenge and revisit

--- python/1578.py
+++ python/1578.py
@@ -4,10 +4,10 @@ from typing import List
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
         res = 0
-        curr_max_time = neededTime[0]
+        curr_max_time = 0
 
-        for i in range(1, len(colors)):
-            if colors[i] != colors[i - 1]:
+        for i in range(len(colors)):
+            if i > 0 and colors[i] != colors[i - 1]:
                 curr_max_time = 0
             res += min(curr_max_time, neededTime[i])
             curr_max_time = max(curr_max_time, neededTime[i])
