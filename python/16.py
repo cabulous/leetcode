44d8742926be2a283d182c0a98dfce1e105f6e77
Carson <dragonchen66@gmail.com>
2022-10-08 09:57:56 -0400

    practice: daily challenge and revisit

--- python/16.py
+++ python/16.py
@@ -1,23 +1,24 @@
+import math
 from typing import List
 
 
-# two pointers
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        diff = float('inf')
         nums.sort()
 
+        diff = math.inf
         for i in range(len(nums)):
-            lo, hi = i + 1, len(nums) - 1
+            lo = i + 1
+            hi = len(nums) - 1
             while lo < hi:
                 total = nums[i] + nums[lo] + nums[hi]
                 if abs(target - total) < abs(diff):
                     diff = target - total
+                if diff == 0:
+                    return target
                 if total < target:
                     lo += 1
                 else:
                     hi -= 1
-            if diff == 0:
-                break
 
         return target - diff
