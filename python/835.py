dc230990b115b2856e18b0710a9a58fba9817e3a
Carson <dragonchen66@gmail.com>
2022-10-27 10:31:55 -0400

    practice: daily challenge and revisit

--- python/835.py
+++ python/835.py
@@ -10,13 +10,44 @@ class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         self.dimension = len(img1)
 
-        max_overlaps = 0
+        transformation_count = Counter()
+        res = 0
+
+        img1_ones = self.non_zero_cells(img1)
+        img2_ones = self.non_zero_cells(img2)
+
+        for x_img1, y_img1 in img1_ones:
+            for x_img2, y_img2 in img2_ones:
+                vec = (x_img2 - x_img1, y_img2 - y_img1)
+                transformation_count[vec] += 1
+                res = max(res, transformation_count[vec])
+
+        return res
+
+    def non_zero_cells(self, matrix):
+        res = []
+        for x in range(self.dimension):
+            for y in range(self.dimension):
+                if matrix[x][y] == 1:
+                    res.append((x, y))
+        return res
+
+
+class Solution:
+
+    def __init__(self):
+        self.dimension = 0
+
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        self.dimension = len(img1)
+
+        res = 0
         for y_shift in range(self.dimension):
             for x_shift in range(self.dimension):
-                max_overlaps = max(max_overlaps, self.shift_and_count(x_shift, y_shift, img1, img2))
-                max_overlaps = max(max_overlaps, self.shift_and_count(x_shift, y_shift, img2, img1))
+                res = max(res, self.shift_and_count(x_shift, y_shift, img1, img2))
+                res = max(res, self.shift_and_count(x_shift, y_shift, img2, img1))
 
-        return max_overlaps
+        return res
 
     def shift_and_count(self, x_shift, y_shift, matrix, reference):
         left_shift_count = 0
@@ -30,34 +61,3 @@ class Solution:
                     right_shift_count += 1
 
         return max(left_shift_count, right_shift_count)
-
-
-class Solution:
-
-    def __init__(self):
-        self.dimension = 0
-
-    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        self.dimension = len(img1)
-
-        transformation_count = Counter()
-        max_overlaps = 0
-
-        img1_ones = self.non_zero_cells(img1)
-        img2_ones = self.non_zero_cells(img2)
-
-        for (x_img1, y_img1) in img1_ones:
-            for (x_img2, y_img2) in img2_ones:
-                vec = (x_img2 - x_img1, y_img2 - y_img1)
-                transformation_count[vec] += 1
-                max_overlaps = max(max_overlaps, transformation_count[vec])
-
-        return max_overlaps
-
-    def non_zero_cells(self, matrix):
-        res = []
-        for x in range(self.dimension):
-            for y in range(self.dimension):
-                if matrix[x][y] == 1:
-                    res.append((x, y))
-        return res
