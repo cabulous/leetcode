c04ce132a9be251fee4779de919ccf596d09a0c3
Carson <dragonchen66@gmail.com>
2022-10-28 09:57:21 -0400

    practice: daily challenge and revisit

--- python/49.py
+++ python/49.py
@@ -5,17 +5,8 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
+
         for s in strs:
             res[tuple(sorted(s))].append(s)
-        return list(res.values())
 
-
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
         return list(res.values())
--- python/835.py
+++ python/835.py
@@ -3,18 +3,12 @@ from typing import List
 
 
 class Solution:
-
-    def __init__(self):
-        self.dimension = 0
-
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        self.dimension = len(img1)
+        img1_ones = self.non_zero_cells(img1)
+        img2_ones = self.non_zero_cells(img2)
 
         transformation_count = Counter()
         res = 0
-
-        img1_ones = self.non_zero_cells(img1)
-        img2_ones = self.non_zero_cells(img2)
 
         for x_img1, y_img1 in img1_ones:
             for x_img2, y_img2 in img2_ones:
@@ -26,38 +20,8 @@ class Solution:
 
     def non_zero_cells(self, matrix):
         res = []
-        for x in range(self.dimension):
-            for y in range(self.dimension):
-                if matrix[x][y] == 1:
-                    res.append((x, y))
+        for r in range(len(matrix)):
+            for c in range(len(matrix[r])):
+                if matrix[r][c] == 1:
+                    res.append((r, c))
         return res
-
-
-class Solution:
-
-    def __init__(self):
-        self.dimension = 0
-
-    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        self.dimension = len(img1)
-
-        res = 0
-        for y_shift in range(self.dimension):
-            for x_shift in range(self.dimension):
-                res = max(res, self.shift_and_count(x_shift, y_shift, img1, img2))
-                res = max(res, self.shift_and_count(x_shift, y_shift, img2, img1))
-
-        return res
-
-    def shift_and_count(self, x_shift, y_shift, matrix, reference):
-        left_shift_count = 0
-        right_shift_count = 0
-
-        for r_row, m_row in enumerate(range(y_shift, self.dimension)):
-            for r_col, m_col in enumerate(range(x_shift, self.dimension)):
-                if matrix[m_row][m_col] == reference[r_row][r_col] == 1:
-                    left_shift_count += 1
-                if matrix[m_row][r_col] == reference[r_row][m_col] == 1:
-                    right_shift_count += 1
-
-        return max(left_shift_count, right_shift_count)
