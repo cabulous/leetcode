81f0ed7fd7467d05da0e2d3ce964b1417cfaa701
Carson <dragonchen66@gmail.com>
2022-10-29 09:17:08 -0400

    practice: daily challenge and revisit

---
+++ python/2136.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        curr_plant_time = 0
+        res = 0
+
+        sorted_flowers = sorted(range(len(plantTime)), key=lambda x: -growTime[x])
+        for flower in sorted_flowers:
+            curr_plant_time += plantTime[flower]
+            res = max(res, curr_plant_time + growTime[flower])
+
+        return res
