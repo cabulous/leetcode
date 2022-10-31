b2b4f93e1da0a3df1a3772db27d97c557471b2a5
Carson <dragonchen66@gmail.com>
2022-10-31 10:18:08 -0400

    practice: daily challenge and revisit

--- python/835.py
+++ python/835.py
@@ -4,24 +4,24 @@ from typing import List
 
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        img1_ones = self.non_zero_cells(img1)
-        img2_ones = self.non_zero_cells(img2)
+        img1_ones = self.one_cells(img1)
+        img2_ones = self.one_cells(img2)
 
         transformation_count = Counter()
         res = 0
 
-        for x_img1, y_img1 in img1_ones:
-            for x_img2, y_img2 in img2_ones:
-                vec = (x_img2 - x_img1, y_img2 - y_img1)
+        for x1, y1 in img1_ones:
+            for x2, y2 in img2_ones:
+                vec = (x2 - x1, y2 - y1)
                 transformation_count[vec] += 1
                 res = max(res, transformation_count[vec])
 
         return res
 
-    def non_zero_cells(self, matrix):
+    def one_cells(self, matrix):
         res = []
-        for r in range(len(matrix)):
-            for c in range(len(matrix[r])):
-                if matrix[r][c] == 1:
+        for r, row in enumerate(matrix):
+            for c, val in enumerate(row):
+                if val == 1:
                     res.append((r, c))
         return res
