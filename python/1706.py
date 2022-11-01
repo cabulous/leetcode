49d36dfdef1c6335ed4360b6d0148935fe0dba97
Carson <dragonchen66@gmail.com>
2022-11-01 09:48:13 -0400

    practice: daily challenge and revisit

---
+++ python/1706.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+# https://leetcode.com/problems/where-will-the-ball-fall/discuss/988576/JavaC%2B%2BPython-Solution-with-Explanation
+class Solution:
+
+    def __init__(self):
+        self.grid = []
+        self.rows = 0
+        self.cols = 0
+
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        self.grid = grid
+        self.rows = len(grid)
+        self.cols = len(grid[0])
+
+        return list(map(self.help, range(self.cols)))
+
+    def help(self, col):
+        curr_col = col
+        for row in range(self.rows):
+            next_col = curr_col + self.grid[row][curr_col]
+            if next_col < 0 or self.cols <= next_col or self.grid[row][next_col] != self.grid[row][curr_col]:
+                return -1
+            curr_col = next_col
+        return curr_col
