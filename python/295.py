5164aac5c2503c0c85322ec1c07ae5ece75eb0a1
Carson <dragonchen66@gmail.com>
2022-11-12 10:17:34 -0500

    practice: daily challenge and revisit

--- python/295.py
+++ python/295.py
@@ -3,17 +3,18 @@ import heapq
 
 # https://leetcode.com/problems/find-median-from-data-stream/discuss/74047/JavaPython-two-heap-solution-O(log-n)-add-O(1)-find
 class MedianFinder:
+
     def __init__(self):
-        self.small = []
-        self.large = []
+        self.small_max_heap = []
+        self.large_min_heap = []
 
     def addNum(self, num: int) -> None:
-        if len(self.small) == len(self.large):
-            heapq.heappush(self.large, -heapq.heappushpop(self.small, -num))
+        if len(self.small_max_heap) == len(self.large_min_heap):
+            heapq.heappush(self.large_min_heap, -heapq.heappushpop(self.small_max_heap, -num))
         else:
-            heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
+            heapq.heappush(self.small_max_heap, -heapq.heappushpop(self.large_min_heap, num))
 
     def findMedian(self) -> float:
-        if len(self.small) == len(self.large):
-            return float((self.large[0] - self.small[0]) / 2.0)
-        return float(self.large[0])
+        if len(self.small_max_heap) == len(self.large_min_heap):
+            return (-self.small_max_heap[0] + self.large_min_heap[0]) / 2.0
+        return float(self.large_min_heap[0])
