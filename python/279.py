d0f03afefa2ca495a44659a6cac0a7d72c1089fb
Carson <dragonchen66@gmail.com>
2022-11-22 10:01:19 -0500

    practice: daily challenge and revisit

--- python/279.py
+++ python/279.py
@@ -1,20 +1,6 @@
 import math
 
 
-class Solution:
-    def numSquares(self, n: int) -> int:
-        square_nums = [i ** 2 for i in range(int(math.sqrt(n)) + 1)]
-        dp = [0.0] + [float('inf')] * n
-
-        for i in range(1, n + 1):
-            for square_num in square_nums:
-                if i < square_num:
-                    break
-                dp[i] = min(dp[i], 1 + dp[i - square_num])
-
-        return int(dp[-1])
-
-
 # n=4^k(8m+7)
 class Solution:
     def numSquares(self, n: int) -> int:
@@ -35,3 +21,17 @@ class Solution:
 
     def is_square(self, n):
         return n == int(math.sqrt(n)) ** 2
+
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        square_nums = [i ** 2 for i in range(int(math.sqrt(n)) + 1)]
+        dp = [0.0] + [float('inf')] * n
+
+        for i in range(1, n + 1):
+            for square_num in square_nums:
+                if i < square_num:
+                    break
+                dp[i] = min(dp[i], 1 + dp[i - square_num])
+
+        return int(dp[-1])
