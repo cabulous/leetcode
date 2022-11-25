8efd36b956b14b64114c7be62a423985ac695433
Carson <dragonchen66@gmail.com>
2022-11-25 10:03:03 -0500

    practice: daily challenge and revisit

---
+++ python/907.py
@@ -0,0 +1,22 @@
+from typing import List
+
+MOD = 10 ** 9 + 7
+
+
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+
+        stack = []
+        dp = [0] * len(arr)
+
+        for curr in range(len(arr)):
+            while stack and arr[curr] <= arr[stack[-1]]:
+                stack.pop()
+            if stack:
+                prev = stack[-1]
+                dp[curr] = dp[prev] + (curr - prev) * arr[curr]
+            else:
+                dp[curr] = (curr + 1) * arr[curr]
+            stack.append(curr)
+
+        return sum(dp) % MOD
