164af99921d9a8641f369b38b000c5b9c531867a
Carson <dragonchen66@gmail.com>
2022-12-09 09:48:27 -0500

    practice: daily challenge and revisit

--- python/323.py
+++ python/323.py
@@ -8,19 +8,16 @@ class UnionFind:
         self.count = size
 
     def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
         return self.root[x]
 
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
         if root_x != root_y:
-            for i in range(len(self.root)):
-                if self.root[i] == root_y:
-                    self.root[i] = root_x
+            self.root[root_y] = root_x
             self.count -= 1
-
-    def is_connected(self, x, y):
-        return self.find(x) == self.find(y)
 
     def union_count(self):
         return self.count
@@ -30,6 +27,5 @@ class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         uf = UnionFind(n)
         for x, y in edges:
-            if not uf.is_connected(x, y):
-                uf.union(x, y)
+            uf.union(x, y)
         return uf.union_count()
