8ecd5e223970c6d5bb7ddf119a810eefcb1d9db1
Carson <dragonchen66@gmail.com>
2022-12-11 09:47:35 -0500

    practice: daily challenge and revisit

--- python/124.py
+++ python/124.py
@@ -18,11 +18,11 @@ class Solution:
         return int(self.res)
 
     def helper(self, node):
-        if not node:
+        if node is None:
             return 0
 
-        left_sum = max(self.helper(node.left), 0)
-        right_sum = max(self.helper(node.right), 0)
+        left_sum = max(0, self.helper(node.left))
+        right_sum = max(0, self.helper(node.right))
         curr_sum = left_sum + node.val + right_sum
 
         self.res = max(self.res, curr_sum)
