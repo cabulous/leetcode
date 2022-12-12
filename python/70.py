849fb756cf4fc8166684f8cbd91b096f460d6ab8
Carson <dragonchen66@gmail.com>
2022-12-12 10:16:40 -0500

    practice: daily challenge and revisit

--- python/70.py
+++ python/70.py
@@ -1,25 +1,26 @@
-# Recursion with memoization - forward
+class Solution:
+
+    def __init__(self):
+        self.memo = {}
+
+    def climbStairs(self, n):
+        return self.helper(n)
+
+    def helper(self, curr):
+        if curr <= 2:
+            return curr
+
+        if curr in self.memo:
+            return self.memo[curr]
+
+        self.memo[curr] = self.helper(curr - 1) + self.helper(curr - 2)
+
+        return self.memo[curr]
+
+
 class Solution:
     def climbStairs(self, n):
-        memo = {}
-
-        def helper(cur):
-            if cur > n:
-                return 0
-            if cur == 1 or cur == 2:
-                return cur
-            if cur in memo:
-                return memo[cur]
-            memo[cur] = helper(cur - 1) + helper(cur - 2)
-            return memo[cur]
-
-        return helper(n)
-
-
-# Dynamic programming
-class Solution:
-    def climbStairs(self, n):
-        if n == 1 or n == 2:
+        if n <= 2:
             return n
 
         pre, cur = 1, 2
@@ -28,27 +29,3 @@ class Solution:
             pre, cur = cur, pre + cur
 
         return cur
-
-
-# Binets Method
-class Solution:
-    def climbStairs(self, n):
-        def multiply(a, b):
-            c = [[0] * 2 for _ in range(2)]
-            for i in range(2):
-                for j in range(2):
-                    c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j]
-            return c
-
-        def powMatrix(a, n):
-            ret = [[1, 0], [0, 1]]
-            while n > 0:
-                if n & 1 == 1:
-                    ret = multiply(ret, a)
-                n >>= 1
-                a = multiply(a, a)
-            return ret
-
-        q = [[1, 1], [1, 0]]
-        res = powMatrix(q, n)
-        return res[0][0]
