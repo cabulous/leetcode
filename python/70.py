11045f3b3700a5b0fb490c895e05b7c09170728b
Carson <dragonchen66@gmail.com>
2022-12-12 10:33:11 -0500

    practice: daily challenge and revisit

--- python/70.py
+++ python/70.py
@@ -6,16 +6,16 @@ class Solution:
     def climbStairs(self, n):
         return self.helper(n)
 
-    def helper(self, curr):
-        if curr <= 2:
-            return curr
+    def helper(self, n):
+        if n <= 2:
+            return n
 
-        if curr in self.memo:
-            return self.memo[curr]
+        if n in self.memo:
+            return self.memo[n]
 
-        self.memo[curr] = self.helper(curr - 1) + self.helper(curr - 2)
+        self.memo[n] = self.helper(n - 1) + self.helper(n - 2)
 
-        return self.memo[curr]
+        return self.memo[n]
 
 
 class Solution:
@@ -24,7 +24,6 @@ class Solution:
             return n
 
         pre, cur = 1, 2
-
         for _ in range(3, n + 1):
             pre, cur = cur, pre + cur
 
