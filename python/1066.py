610627a9831ffe547bc1ce82d246ee75e5253038
Carson <dragonchen66@gmail.com>
2022-12-15 10:17:05 -0500

    practice: daily challenge and revisit

--- python/1066.py
+++ python/1066.py
@@ -2,27 +2,35 @@ import heapq
 from typing import List
 
 
-# https://leetcode.com/problems/campus-bikes-ii/discuss/303422/Python-Priority-Queue
 class Solution:
+
     def __init__(self):
         self.workers = []
         self.bikes = []
 
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
-        self.workers, self.bikes = workers, bikes
-        queue = [[0, 0, 0]]
+        self.workers = workers
+        self.bikes = bikes
+
+        queue = [(0, 0, 0)]
         seen = set()
 
-        while True:
-            cost, i, taken = heapq.heappop(queue)
-            if i == len(workers):
+        while queue:
+            cost, worker_idx, taken = heapq.heappop(queue)
+            if worker_idx == len(self.workers):
                 return cost
-            if (i, taken) in seen:
+            if (worker_idx, taken) in seen:
                 continue
-            seen.add((i, taken))
-            for j in range(len(bikes)):
-                if taken & (1 << j) == 0:
-                    heapq.heappush(queue, [cost + self.manhattan_dist(i, j), i + 1, taken | (1 << j)])
+            seen.add((worker_idx, taken))
+            for bike_idx in range(len(bikes)):
+                if taken & (1 << bike_idx) == 0:
+                    next_cost = cost + self.manhattan_dist(worker_idx, bike_idx)
+                    next_taken = taken | (1 << bike_idx)
+                    heapq.heappush(queue, (next_cost, worker_idx + 1, next_taken))
 
-    def manhattan_dist(self, i, j):
-        return abs(self.workers[i][0] - self.bikes[j][0]) + abs(self.workers[i][1] - self.bikes[j][1])
+        return -1
+
+    def manhattan_dist(self, worker_idx, bike_idx):
+        worker_x, worker_y = self.workers[worker_idx]
+        bike_x, bike_y = self.bikes[bike_idx]
+        return abs(worker_x - bike_x) + abs(worker_y - bike_y)
--- python/1143.py
+++ python/1143.py
@@ -1,10 +1,9 @@
-# https://leetcode.com/problems/longest-common-subsequence/discuss/351689/JavaPython-3-Two-DP-codes-of-O(mn)-and-O(min(m-n))-spaces-w-picture-and-analysis
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
 
-        for i, c1 in enumerate(text1):
-            for j, c2 in enumerate(text2):
-                dp[i + 1][j + 1] = 1 + dp[i][j] if c1 == c2 else max(dp[i + 1][j], dp[i][j + 1])
+        for i, ch1 in enumerate(text1):
+            for j, ch2 in enumerate(text2):
+                dp[i + 1][j + 1] = 1 + dp[i][j] if ch1 == ch2 else max(dp[i + 1][j], dp[i][j + 1])
 
         return dp[-1][-1]
