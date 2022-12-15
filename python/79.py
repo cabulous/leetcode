56a385e530f2ae7e53cc7c1e2ee66007c8d3c0d7
Carson <dragonchen66@gmail.com>
2022-12-15 10:50:38 -0500

    practice: daily challenge and revisit

--- python/79.py
+++ python/79.py
@@ -1,7 +1,64 @@
+from collections import defaultdict, Counter
 from typing import List
 
 
 class Solution:
+
+    def __init__(self):
+        self.word = ''
+        self.word_len = 0
+        self.lookup = defaultdict(set)
+        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.word = word
+        self.word_len = len(word)
+
+        char_count = Counter(word)
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                ch = board[r][c]
+                if ch in char_count:
+                    self.lookup[ch].add((r, c))
+
+        if len(char_count) != len(self.lookup):
+            return False
+
+        for ch, count in char_count.items():
+            if len(self.lookup[ch]) < count:
+                return False
+
+        if len(self.lookup[word[-1]]) < len(self.lookup[word[0]]):
+            self.word = self.word[::-1]
+
+        for (r, c) in list(self.lookup[self.word[0]]):
+            self.lookup[self.word[0]].remove((r, c))
+            if self.dfs(r, c, 1):
+                return True
+            self.lookup[self.word[0]].add((r, c))
+
+        return False
+
+    def dfs(self, row, col, word_idx):
+        if word_idx == self.word_len:
+            return True
+
+        ch = self.word[word_idx]
+
+        for dr, dc in self.directions:
+            nr, nc = row + dr, col + dc
+            if (nr, nc) in self.lookup[ch]:
+                self.lookup[ch].remove((nr, nc))
+                if self.dfs(nr, nc, word_idx + 1):
+                    return True
+                self.lookup[ch].add((nr, nc))
+
+        return False
+
+
+class Solution:
+
     def __init__(self):
         self.rows = 0
         self.cols = 0
