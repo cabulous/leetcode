39a392fc6480c0eac746cd867911e2d21d843198
Carson <dragonchen66@gmail.com>
2022-12-17 10:12:04 -0500

    practice: daily challenge and revisit

--- python/150.py
+++ python/150.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operation_lookup = {
+        lookup = {
             '+': lambda a, b: a + b,
             '-': lambda a, b: a - b,
             '*': lambda a, b: a * b,
@@ -12,10 +12,10 @@ class Solution:
         stack = []
 
         for token in tokens:
-            if token in operation_lookup:
+            if token in lookup:
                 num2 = stack.pop()
                 num1 = stack.pop()
-                operation = operation_lookup[token]
+                operation = lookup[token]
                 stack.append(operation(num1, num2))
             else:
                 stack.append(int(token))
