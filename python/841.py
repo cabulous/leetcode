b0dc1def7ab05311f0965d11648dd4eed612af66
Carson <dragonchen66@gmail.com>
2022-12-20 09:58:04 -0500

    practice: daily challenge and revisit

--- python/841.py
+++ python/841.py
@@ -1,14 +1,17 @@
+from collections import deque
 from typing import List
 
 
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         seen = [True] + [False] * (len(rooms) - 1)
-        stack = [0]
-        while stack:
-            idx = stack.pop()
+        queue = deque([0])
+
+        while queue:
+            idx = queue.popleft()
             for key in rooms[idx]:
                 if not seen[key]:
                     seen[key] = True
-                    stack.append(key)
+                    queue.append(key)
+
         return all(seen)
