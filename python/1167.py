a623298e21b2b1659abcee1c307b36e5d555c6b5
Carson <dragonchen66@gmail.com>
2022-12-22 10:52:29 -0500

    practice: daily challenge and revisit

--- python/1167.py
+++ python/1167.py
@@ -6,8 +6,11 @@ class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
         heapq.heapify(sticks)
         res = 0
+
         while len(sticks) > 1:
-            stick1, stick2 = heapq.heappop(sticks), heapq.heappop(sticks)
+            stick1 = heapq.heappop(sticks)
+            stick2 = heapq.heappop(sticks)
             res += stick1 + stick2
             heapq.heappush(sticks, stick1 + stick2)
+
         return res
