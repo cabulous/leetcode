4e33d8e3ae67bf6e10fbbb48db3b97629b8074b6
Carson <dragonchen66@gmail.com>
2023-01-17 09:52:52 -0500

    practice: daily challenge and revisit

--- python/926.py
+++ python/926.py
@@ -1,18 +1,14 @@
-import math
-from itertools import accumulate
-
-
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
         prefix_sum = [0]
-
         for digit in s:
             prefix_sum.append(prefix_sum[-1] + int(digit))
 
-        res = math.inf
-
+        res = len(s)
         for i in range(len(prefix_sum)):
-            cur = prefix_sum[i] + (len(s) - i - (prefix_sum[-1] - prefix_sum[i]))
-            res = min(res, cur)
+            flip_to_zero = prefix_sum[i]
+            flip_to_one = (len(s) - i) - (prefix_sum[-1] - prefix_sum[i])
+            curr = flip_to_zero + flip_to_one
+            res = min(res, curr)
 
         return res
