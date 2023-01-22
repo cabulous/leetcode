128c3343364f89e6eb2cc6b4c9a18f82aef867de
Carson <dragonchen66@gmail.com>
2023-01-22 10:06:52 -0500

    practice: daily challenge and revisit

--- python/93.py
+++ python/93.py
@@ -14,8 +14,9 @@ class Solution:
 
     def backtrack(self, index, curr, dots):
         if dots == 3:
-            if self.is_valid(self.s[index:]):
-                self.res.append(curr + self.s[index:])
+            remain = self.s[index:]
+            if self.is_valid(remain):
+                self.res.append(curr + remain)
             return
 
         for end in range(index, min(index + 3, len(self.s))):
@@ -23,11 +24,11 @@ class Solution:
             if self.is_valid(piece):
                 self.backtrack(end + 1, curr + piece + '.', dots + 1)
 
-    def is_valid(self, s):
-        if len(s) == 0 or 3 < len(s):
+    def is_valid(self, piece):
+        if len(piece) == 0 or 3 < len(piece):
             return False
-        if len(s) > 1 and s[0] == '0':
+        if len(piece) > 1 and piece[0] == '0':
             return False
-        if int(s) > 255:
+        if int(piece) > 255:
             return False
         return True
