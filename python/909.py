aef53c50fe1ebe857846a926ad881e9dd655e5dd
Carson <dragonchen66@gmail.com>
2023-01-24 09:44:07 -0500

    practice: daily challenge and revisit

--- python/909.py
+++ python/909.py
@@ -6,13 +6,13 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         rows_count = len(board)
         cells = [None] * (rows_count ** 2 + 1)
-        label = 1
+        curr_cell = 1
         cols = list(range(rows_count))
 
         for r in range(rows_count - 1, -1, -1):
             for c in cols:
-                cells[label] = (r, c)
-                label += 1
+                cells[curr_cell] = (r, c)
+                curr_cell += 1
             cols.reverse()
 
         dist = [-1] * (rows_count ** 2 + 1)
@@ -21,9 +21,9 @@ class Solution:
 
         while queue:
             curr = queue.popleft()
-            for next_label in range(curr + 1, min(curr + 6, rows_count ** 2) + 1):
-                r, c = cells[next_label]
-                destination = (board[r][c] if board[r][c] != -1 else next_label)
+            for next_cell in range(curr + 1, min(curr + 6, rows_count ** 2) + 1):
+                r, c = cells[next_cell]
+                destination = board[r][c] if board[r][c] != -1 else next_cell
                 if dist[destination] == -1:
                     dist[destination] = dist[curr] + 1
                     queue.append(destination)
