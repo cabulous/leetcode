a27ac34b2bb0852cc82c361fabba63fbd14e973a
Carson <dragonchen66@gmail.com>
2023-02-15 10:08:11 -0500

    practice: daily challenge and revisit

--- python/305.py
+++ python/305.py
@@ -1,41 +1,34 @@
-from typing import List
-
-
-# https://leetcode.com/problems/number-of-islands-ii/discuss/75459/JavaPython-clear-solution-with-UnionFind-Class-(Weighting-and-Path-compression)
 class UnionFind:
 
     def __init__(self):
-        self.parent = {}
+        self._root = {}
         self.count = 0
 
     def find(self, x):
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        if x != self._root[x]:
+            self._root[x] = self.find(self._root[x])
+        return self._root[x]
 
     def union(self, x, y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        if x_root != y_root:
-            self.parent[x_root] = y_root
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self._root[root_x] = root_y
             self.count -= 1
 
     def contains(self, row, col):
-        return (row, col) in self.parent
+        return (row, col) in self._root
 
     def add(self, row, col):
         if self.contains(row, col):
             return
-        self.parent[row, col] = row, col
+        self._root[row, col] = row, col
         self.count += 1
-
-    def get_count(self):
-        return self.count
 
 
 class Solution:
-    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    def numIslands2(self, m: int, n: int, positions: list[list[int]]) -> list[int]:
+        directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
         islands = UnionFind()
         res = []
 
@@ -45,6 +38,6 @@ class Solution:
                 nr, nc = r + dr, c + dc
                 if islands.contains(nr, nc):
                     islands.union((r, c), (nr, nc))
-            res.append(islands.get_count())
+            res.append(islands.count)
 
         return res
