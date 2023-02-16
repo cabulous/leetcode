18ac26627a66bb2a56f6046d5cab7d85a74d3f44
Carson <dragonchen66@gmail.com>
2023-02-16 09:57:53 -0500

    practice: daily challenge and revisit

--- python/104.py
+++ python/104.py
@@ -1,4 +1,3 @@
-from collections import deque
 from typing import Optional
 
 
@@ -11,32 +10,13 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        return self.helper(root)
+        return self.dfs(root)
 
-    def helper(self, node):
+    def dfs(self, node):
         if node is None:
             return 0
 
-        left_depth = self.helper(node.left)
-        right_depth = self.helper(node.right)
+        left_depth = self.dfs(node.left)
+        right_depth = self.dfs(node.right)
 
         return 1 + max(left_depth, right_depth)
-
-
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-
-        stack = deque([(root, 1)])
-        res = 1
-
-        while stack:
-            node, depth = stack.popleft()
-            res = max(res, depth)
-            if node.left:
-                stack.append((node.left, depth + 1))
-            if node.right:
-                stack.append((node.right, depth + 1))
-
-        return res
