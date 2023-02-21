cdb34ebeecfd09f28d14ca0da8e44a6d2200fb3c
Carson <dragonchen66@gmail.com>
2023-02-21 09:39:23 -0500

    practice: daily challenge and revisit

--- python/540.py
+++ python/540.py
@@ -3,7 +3,8 @@ from typing import List
 
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        lo, hi = 0, len(nums) - 1
+        lo = 0
+        hi = len(nums) - 1
 
         while lo < hi:
             mi = lo + (hi - lo) // 2
@@ -12,6 +13,6 @@ class Solution:
             if nums[mi] == nums[mi + 1]:
                 lo = mi + 2
             else:
-                hi = mi - 1
+                hi = mi
 
         return nums[lo]
