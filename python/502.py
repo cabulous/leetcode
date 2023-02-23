84642969d028ef5fbdcaefe705df28fd8bce4369
Carson <dragonchen66@gmail.com>
2023-02-23 09:33:27 -0500

    practice: daily challenge and revisit

--- python/502.py
+++ python/502.py
@@ -15,10 +15,9 @@ class Solution:
         while taken_count < k:
             while projects and projects[-1][0] <= res:
                 heapq.heappush(candidates, -projects.pop()[1])
-            if len(candidates) == 0:
+            if not candidates:
                 return res
-            max_profit = -heapq.heappop(candidates)
-            res += max_profit
+            res += -heapq.heappop(candidates)
             taken_count += 1
 
         return res
