7f7b07e059868720a03ef59da13bff0d22179a53
Carson <dragonchen66@gmail.com>
2023-02-24 09:53:31 -0500

    practice: daily challenge and revisit

--- python/1675.py
+++ python/1675.py
@@ -9,22 +9,20 @@ class Solution:
 
         for num in nums:
             if num % 2 == 0:
-                evens.append(-num)
+                heapq.heappush(evens, -num)
                 num_min = min(num_min, num)
             else:
-                evens.append(-num * 2)
+                heapq.heappush(evens, -num * 2)
                 num_min = min(num_min, num * 2)
 
-        heapq.heapify(evens)
         res = float('inf')
 
         while evens:
             num_max = -heapq.heappop(evens)
             res = min(res, num_max - num_min)
-            if num_max % 2 == 0:
-                heapq.heappush(evens, -num_max // 2)
-                num_min = min(num_min, num_max // 2)
-            else:
+            if num_max % 2 == 1:
                 return res
+            heapq.heappush(evens, -num_max // 2)
+            num_min = min(num_min, num_max // 2)
 
         return res
