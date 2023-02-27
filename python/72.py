fae34f27989f7d9c6e045912767ba72623a9a61d
Carson <dragonchen66@gmail.com>
2023-02-26 21:07:23 -0500

    practice: daily challenge and revisit

--- python/72.py
+++ python/72.py
@@ -1,20 +1,17 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m = len(word1)
-        n = len(word2)
+        if len(word1) * len(word2) == 0:
+            return len(word1) + len(word2)
 
-        if m * n == 0:
-            return m + n
+        dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
 
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-        for i in range(m + 1):
+        for i in range(len(word1) + 1):
             dp[i][0] = i
-        for j in range(n + 1):
+        for j in range(len(word2) + 1):
             dp[0][j] = j
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
                 dp[i][j] = min(
                     dp[i - 1][j] + 1,
                     dp[i][j - 1] + 1,
