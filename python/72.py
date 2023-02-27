07aa73e8db14419937be1cca64467e42d83315bd
Carson <dragonchen66@gmail.com>
2023-02-26 21:04:20 -0500

    practice: daily challenge and revisit

--- python/72.py
+++ python/72.py
@@ -16,9 +16,9 @@ class Solution:
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 dp[i][j] = min(
-                    dp[i - 1][j - 1] + (0 if word1[i - 1] == word2[j - 1] else 1),
                     dp[i - 1][j] + 1,
                     dp[i][j - 1] + 1,
+                    dp[i - 1][j - 1] + (0 if word1[i - 1] == word2[j - 1] else 1),
                 )
 
-        return dp[m][n]
+        return dp[-1][-1]
