b05fff47eaaf191b18cd9a890073903a78c0ee44
Carson <dragonchen66@gmail.com>
2023-03-14 10:27:11 -0400

    practice: daily challenge and revisit

--- python/129.py
+++ python/129.py
@@ -9,70 +9,22 @@ class TreeNode:
 
 
 class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total = 0
-        stack = [(root, 0)]
 
-        while stack:
-            node, curr_sum = stack.pop()
-            if node is None:
-                continue
-            curr_sum = curr_sum * 10 + node.val
-            if node.left is None and node.right is None:
-                total += curr_sum
-            stack.append((node.left, curr_sum))
-            stack.append((node.right, curr_sum))
-
-        return total
-
-
-class Solution:
     def __init__(self):
-        self.total = 0
+        self.res = 0
 
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         self.preorder(root, 0)
-        return self.total
+        return self.res
 
-    def preorder(self, node, cur_sum):
-        if node is None:
+    def preorder(self, node, total):
+        if not node:
             return
 
-        cur_sum = cur_sum * 10 + node.val
+        total = total * 10 + node.val
 
-        if node.left is None and node.right is None:
-            self.total += cur_sum
+        if not node.left and not node.right:
+            self.res += total
 
-        self.preorder(node.left, cur_sum)
-        self.preorder(node.right, cur_sum)
-
-
-class Solution:
-    def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        total = curr_sum = 0
-
-        while root:
-            if root.left:
-                predecessor = root.left
-                steps = 1
-                while predecessor.right and predecessor.right is not root:
-                    predecessor = predecessor.right
-                    steps += 1
-                if predecessor.right is None:
-                    curr_sum = curr_sum * 10 + root.val
-                    predecessor.right = root
-                    root = root.left
-                else:
-                    if predecessor.left is None:
-                        total += curr_sum
-                    for _ in range(steps):
-                        curr_sum //= 10
-                    predecessor.right = None
-                    root = root.right
-            else:
-                curr_sum = curr_sum * 10 + root.val
-                if root.right is None:
-                    total += curr_sum
-                root = root.right
-
-        return total
+        self.preorder(node.left, total)
+        self.preorder(node.right, total)
--- python/1675.py
+++ python/1675.py
@@ -4,25 +4,25 @@ from typing import List
 
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
-        evens = []
+        max_heap = []
         num_min = float('inf')
 
         for num in nums:
             if num % 2 == 0:
-                heapq.heappush(evens, -num)
+                heapq.heappush(max_heap, -num)
                 num_min = min(num_min, num)
             else:
-                heapq.heappush(evens, -num * 2)
+                heapq.heappush(max_heap, -num * 2)
                 num_min = min(num_min, num * 2)
 
         res = float('inf')
 
-        while evens:
-            num_max = -heapq.heappop(evens)
+        while max_heap:
+            num_max = -heapq.heappop(max_heap)
             res = min(res, num_max - num_min)
             if num_max % 2 == 1:
                 return res
-            heapq.heappush(evens, -num_max // 2)
+            heapq.heappush(max_heap, -num_max // 2)
             num_min = min(num_min, num_max // 2)
 
         return res
