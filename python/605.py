46862da45b3f58623dc4963b91335c3b54ff932d
Carson <dragonchen66@gmail.com>
2023-03-20 09:54:36 -0400

    practice: daily challenge and revisit

--- python/605.py
+++ python/605.py
@@ -6,12 +6,12 @@ class Solution:
         if n == 0:
             return True
 
-        flowerbed = [0] + flowerbed + [0]
+        states = [0] + flowerbed + [0]
         remaining = n
 
-        for i in range(1, len(flowerbed) - 1):
-            if flowerbed[i - 1] == flowerbed[i] == flowerbed[i + 1] == 0:
-                flowerbed[i] = 1
+        for i in range(1, len(states) - 1):
+            if states[i - 1] == states[i] == states[i + 1] == 0:
+                states[i] = i
                 remaining -= 1
                 if remaining == 0:
                     return True
