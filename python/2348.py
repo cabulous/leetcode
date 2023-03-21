ff3d01647d789820d606974e3591e6bbc19b1766
Carson <dragonchen66@gmail.com>
2023-03-21 10:08:20 -0400

    practice: daily challenge and revisit

---
+++ python/2348.py
@@ -0,0 +1,13 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: list[int]) -> int:
+        curr = 0
+        res = 0
+
+        for num in nums:
+            if num == 0:
+                curr += 1
+                res += curr
+            else:
+                curr = 0
+
+        return res
