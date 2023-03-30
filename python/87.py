30631e03a037dfb6fc756b61946654b60fbd832d
Carson <dragonchen66@gmail.com>
2023-03-30 10:03:34 -0400

    practice: daily challenge and revisit

--- python/87.py
+++ python/87.py
@@ -18,8 +18,10 @@ class Solution:
 
         for i in range(1, len(s1)):
             if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
+                self.memo[s1, s2] = True
                 return True
             if self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i]):
+                self.memo[s1, s2] = True
                 return True
 
         self.memo[s1, s2] = False
