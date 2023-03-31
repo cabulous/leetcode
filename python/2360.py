85de37884fc20c940d0982b71581cbb381e1ab14
Carson <dragonchen66@gmail.com>
2023-03-31 10:05:01 -0400

    practice: daily challenge and revisit

--- python/2360.py
+++ python/2360.py
@@ -7,12 +7,14 @@ class Solution:
         for node in range(len(edges)):
             if steps[node] > 0:
                 continue
+
             start_step = curr_step
             curr_node = node
             while curr_node != -1 and steps[curr_node] == 0:
                 steps[curr_node] = curr_step
                 curr_step += 1
                 curr_node = edges[curr_node]
+
             if curr_node != -1 and steps[curr_node] >= start_step:
                 res = max(res, curr_step - steps[curr_node])
 
