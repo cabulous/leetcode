66ca41e558eddd5488374ecbf1b9dcb684e11124
Carson <dragonchen66@gmail.com>
2023-04-03 09:57:49 -0400

    practice: daily challenge and revisit

--- python/651.py
+++ python/651.py
@@ -2,7 +2,7 @@ class Solution:
     def maxA(self, n: int) -> int:
         dp = [0] * (n + 1)
 
-        for i in range(n + 1):
+        for i in range(len(dp)):
             dp[i] = i
             for j in range(1, i - 3):
                 dp[i] = max(dp[i], dp[j] * (i - j - 1))
--- python/881.py
+++ python/881.py
@@ -5,9 +5,13 @@ from typing import List
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort(reverse=True)
-        left, right = 0, len(people) - 1
+
+        left = 0
+        right = len(people) - 1
+
         while left <= right:
             if people[left] + people[right] <= limit:
                 right -= 1
             left += 1
+
         return left
