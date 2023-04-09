d23ce2e491299b29d4b56b49f5883344a9ed33d6
Carson <dragonchen66@gmail.com>
2023-04-09 08:46:16 -0400

    practice: daily challenge and revisit

---
+++ python/1857.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: list[list[int]]) -> int:
+        in_degree = [0] * len(colors)
+        graph = defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            in_degree[v] += 1
+
+        zero_in_degree = [i for i in range(len(colors)) if in_degree[i] == 0]
+        counts = [[0] * 26 for _ in range(len(colors))]
+
+        while zero_in_degree:
+            node = zero_in_degree.pop()
+            counts[node][ord(colors[node]) - ord('a')] += 1
+            for next_node in graph[node]:
+                counts[next_node] = list(map(max, counts[next_node], counts[node]))
+                in_degree[next_node] -= 1
+                if in_degree[next_node] == 0:
+                    zero_in_degree.append(next_node)
+
+        return -1 if sum(in_degree) > 0 else max(max(node) for node in counts)
