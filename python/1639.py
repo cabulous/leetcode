dc6e3d7190d31a645619721c88cf30a33beacd53
Carson <dragonchen66@gmail.com>
2023-04-16 09:44:48 -0400

    practice: daily challenge and revisit

--- python/1639.py
+++ python/1639.py
@@ -7,12 +7,12 @@ MOD = 10 ** 9 + 7
 # https://leetcode.com/problems/number-of-ways-to-form-a-target-string-given-a-dictionary/discuss/917779/JavaC%2B%2BPython-Space-O(N)
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        n = len(target)
-        res = [1] + [0] * n
+        res = [0] * (len(target) + 1)
+        res[0] = 1
 
         for i in range(len(words[0])):
             count = Counter(w[i] for w in words)
-            for j in range(n - 1, -1, -1):
+            for j in range(len(target) - 1, -1, -1):
                 res[j + 1] += res[j] * count[target[j]] % MOD
 
-        return res[n] % MOD
+        return res[-1] % MOD
