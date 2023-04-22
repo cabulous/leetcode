ba652c4c94cca9becb280157fe3e30b76199e2e6
Carson <dragonchen66@gmail.com>
2023-04-22 10:00:51 -0400

    practice: daily challenge and revisit

--- python/727.py
+++ python/727.py
@@ -6,43 +6,42 @@ class Solution:
         self.s2 = ''
 
     def minWindow(self, s1: str, s2: str) -> str:
-        self.s1, self.s2 = s1, s2
+        self.s1 = s1
+        self.s2 = s2
 
-        s1_pointer = 0
+        s1_reader = 0
         min_len = float('inf')
         res = ''
 
-        while s1_pointer < len(s1):
-            end = self.get_end_pointer(s1_pointer)
-
+        while s1_reader < len(s1):
+            end = self.get_end(s1_reader)
             if end is None:
-                break
+                return res
 
-            start = self.get_best_start_pointer(end)
-
+            start = self.get_start(end)
             curr_len = end - start + 1
             if curr_len < min_len:
                 min_len = curr_len
                 res = s1[start:end + 1]
 
-            s1_pointer = start + 1
+            s1_reader = start + 1
 
         return res
 
-    def get_end_pointer(self, s1_pointer):
-        s2_pointer = 0
-        while s1_pointer < len(self.s1):
-            if self.s1[s1_pointer] == self.s2[s2_pointer]:
-                s2_pointer += 1
-                if s2_pointer == len(self.s2):
-                    return s1_pointer
-            s1_pointer += 1
+    def get_end(self, s1_reader):
+        s2_reader = 0
+        while s1_reader < len(self.s1):
+            if self.s1[s1_reader] == self.s2[s2_reader]:
+                s2_reader += 1
+                if s2_reader == len(self.s2):
+                    return s1_reader
+            s1_reader += 1
         return None
 
-    def get_best_start_pointer(self, s1_pointer):
-        s2_pointer = len(self.s2) - 1
-        while s2_pointer >= 0:
-            if self.s1[s1_pointer] == self.s2[s2_pointer]:
-                s2_pointer -= 1
-            s1_pointer -= 1
-        return s1_pointer + 1
+    def get_start(self, s1_reader):
+        s2_reader = len(self.s2) - 1
+        while s2_reader >= 0:
+            if self.s1[s1_reader] == self.s2[s2_reader]:
+                s2_reader -= 1
+            s1_reader -= 1
+        return s1_reader + 1
