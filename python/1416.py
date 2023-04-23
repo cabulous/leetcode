bc2fedd5356c3cb746009d44b2465f44cfcfde08
Carson <dragonchen66@gmail.com>
2023-04-23 17:30:26 -0400

    practice: daily challenge and revisit

---
+++ python/1416.py
@@ -0,0 +1,35 @@
+MOD = 10 ** 9 + 7
+
+
+class Solution:
+
+    def __init__(self):
+        self.s = ''
+        self.k = 0
+        self.memo = {}
+
+    def numberOfArrays(self, s: str, k: int) -> int:
+        self.s = s
+        self.k = k
+        return self.dfs(0)
+
+    def dfs(self, start_idx):
+        if start_idx == len(self.s):
+            return 1
+        if self.s[start_idx] == '0':
+            return 0
+        if start_idx in self.memo:
+            return self.memo[start_idx]
+
+        num = 0
+        res = 0
+        for end_idx in range(start_idx, len(self.s)):
+            num = num * 10 + int(self.s[end_idx])
+            if num > self.k:
+                break
+            res += self.dfs(end_idx + 1)
+            res %= MOD
+
+        self.memo[start_idx] = res
+
+        return res
