a49c1b01e0fcd3ac067668723fc831231680f746
Carson <dragonchen66@gmail.com>
2023-04-26 10:14:09 -0400

    practice: daily challenge and revisit

--- python/258.py
+++ python/258.py
@@ -5,8 +5,3 @@ class Solution:
         if num % 9 == 0:
             return 9
         return num % 9
-
-
-class Solution:
-    def addDigits(self, num: int) -> int:
-        return (num - 1) % 9 + 1 if num > 0 else 0
