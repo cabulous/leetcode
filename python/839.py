383a92503e27fb29d0480e57f08199b9fba546e6
Carson <dragonchen66@gmail.com>
2023-04-28 09:40:24 -0400

    practice: daily challenge and revisit

--- python/839.py
+++ python/839.py
@@ -5,18 +5,18 @@ from typing import List
 class UnionFind:
 
     def __init__(self, size):
-        self.parent = list(range(size))
+        self.root = list(range(size))
 
     def find(self, x):
-        if x != self.parent[x]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
 
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
         if root_x != root_y:
-            self.parent[root_x] = root_y
+            self.root[root_x] = root_y
 
 
 class Solution:
