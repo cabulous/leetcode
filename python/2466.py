352a7a0fa3f1981af495bb4b05c354863aa11ec1
Carson <dragonchen66@gmail.com>
2023-05-13 09:55:18 -0400

    practice: daily challenge and revisit

---
+++ python/2466.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+MOD = 10 ** 9 + 7
+
+
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        dp = Counter({0: 1})
+
+        for i in range(1, high + 1):
+            dp[i] = (dp[i - zero] + dp[i - one]) % MOD
+
+        res = sum(dp[i] for i in range(low, high + 1))
+
+        return res % MOD
