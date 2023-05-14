9f952af8406f0fd2f8579bca0a315243512e09e5
Carson <dragonchen66@gmail.com>
2023-05-14 09:42:13 -0400

    practice: daily challenge and revisit

---
+++ python/1799.py
@@ -0,0 +1,31 @@
+from functools import lru_cache
+
+import math
+
+
+class Solution:
+
+    def __init__(self):
+        self.nums = []
+
+    def maxScore(self, nums: list[int]) -> int:
+        self.nums = nums
+        return self.dfs(1, 0)
+
+    @lru_cache(None)
+    def dfs(self, idx, mask):
+        if idx > len(self.nums) // 2:
+            return 0
+
+        res = 0
+
+        for i in range(len(self.nums)):
+            for j in range(i + 1, len(self.nums)):
+                next_mask = (1 << i) + (1 << j)
+                if not mask & next_mask:
+                    res = max(
+                        res,
+                        idx * (math.gcd(self.nums[i], self.nums[j])) + self.dfs(idx + 1, mask + next_mask),
+                    )
+
+        return res
