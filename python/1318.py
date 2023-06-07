b21da94c2e91392935752ff1c3a3b6b377b1909a
Carson <dragonchen66@gmail.com>
2023-06-07 09:52:21 -0400

    practice: daily challenge and revisit

---
+++ python/1318.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/solutions/477690/java-python-3-bit-manipulation-w-explanation-and-analysis/
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        diff = (a | b) ^ c
+        res = 0
+
+        for i in range(31):
+            mask = 1 << i
+            if diff & mask > 0:
+                if (a & mask) == (b & mask) and (c & mask) == 0:
+                    res += 2
+                else:
+                    res += 1
+
+        return res
