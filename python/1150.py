06b354dff03fcfe04072d8a910fb6c192d5d35ae
Carson <dragonchen66@gmail.com>
2023-06-08 10:12:38 -0400

    practice: daily challenge and revisit

---
+++ python/1150.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isMajorityElement(self, nums: list[int], target: int) -> bool:
+        if nums[len(nums) // 2] != target:
+            return False
+
+        left = self.binary_search(nums, target)
+        right = self.binary_search(nums, target + 1)
+
+        return right - left > len(nums) // 2
+
+    def binary_search(self, nums, target):
+        left = 0
+        right = len(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
---
+++ python/1351.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countNegatives(self, grid: list[list[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        r = 0
+        c = cols - 1
+        res = 0
+        while r < rows and c >= 0:
+            if grid[r][c] < 0:
+                res += rows - r
+                c -= 1
+            else:
+                r += 1
+
+        return res
