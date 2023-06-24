0e4ff098d8d5a5d098e4e1d2304923accb309e50
Carson <dragonchen66@gmail.com>
2023-06-24 15:16:51 -0400

    practice: daily challenge and revisit

--- python/956.py
+++ python/956.py
@@ -1,21 +1,16 @@
-from collections import defaultdict
-from typing import List
+from collections import Counter
 
 
 # https://leetcode.com/problems/tallest-billboard/discuss/203181/JavaC%2B%2BPython-DP-min(O(SN2)-O(3N2-*-N)
 class Solution:
-    def tallestBillboard(self, rods: List[int]) -> int:
-        dp = defaultdict(int)
-        dp[0] = 0
+    def tallestBillboard(self, rods: list[int]) -> int:
+        dp = Counter({0: 0})
 
-        for next_height in rods:
+        for delta in rods:
             next_dp = dp.copy()
             for diff, height in dp.items():
-                next_dp[diff + next_height] = max(next_dp[diff + next_height], height)
-                next_dp[abs(diff - next_height)] = max(
-                    next_dp[abs(diff - next_height)],
-                    height + min(diff, next_height),
-                )
+                next_dp[diff + delta] = max(next_dp[diff + delta], height)
+                next_dp[abs(diff - delta)] = max(next_dp[abs(diff - delta)], height + min(diff, delta))
             dp = next_dp
 
         return dp[0]
