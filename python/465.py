943b584599c946168810bc3cc20672fdb87e6ccd
Carson <dragonchen66@gmail.com>
2023-07-01 10:53:02 -0400

    practice: daily challenge and revisit

--- python/465.py
+++ python/465.py
@@ -1,41 +1,41 @@
-from collections import defaultdict
+from collections import Counter
 from typing import List
 
 
 # https://leetcode.com/problems/optimal-account-balancing/discuss/95363/Easy-backtracking-%2B-greedy-solution-with-explanation-(Python-Accepted)
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
-        balances = defaultdict(int)
+        count = Counter()
         for source, target, amount in transactions:
-            balances[source] -= amount
-            balances[target] += amount
+            count[source] -= amount
+            count[target] += amount
 
-        net_profit = []
-        for amount in balances.values():
+        balances = []
+        for amount in count.values():
             if amount != 0:
-                net_profit.append(amount)
+                balances.append(amount)
 
-        return self.backtrack(net_profit, 0, 0)
+        return self.backtrack(balances, 0, 0)
 
-    def backtrack(self, net_profit, idx, count):
-        while idx < len(net_profit) and net_profit[idx] == 0:
+    def backtrack(self, balance, idx, count):
+        while idx < len(balance) and balance[idx] == 0:
             idx += 1
 
-        if idx == len(net_profit):
+        if idx == len(balance):
             return count
 
-        for i in range(idx + 1, len(net_profit)):
-            if net_profit[i] + net_profit[idx] == 0:
-                net_profit[i] += net_profit[idx]
-                res = self.backtrack(net_profit, idx + 1, count + 1)
-                net_profit[i] -= net_profit[idx]
+        for i in range(idx + 1, len(balance)):
+            if balance[i] + balance[idx] == 0:
+                balance[i] += balance[idx]
+                res = self.backtrack(balance, idx + 1, count + 1)
+                balance[i] -= balance[idx]
                 return res
 
         res = float('inf')
-        for i in range(idx + 1, len(net_profit)):
-            if net_profit[i] * net_profit[idx] < 0:
-                net_profit[i] += net_profit[idx]
-                res = min(res, self.backtrack(net_profit, idx + 1, count + 1))
-                net_profit[i] -= net_profit[idx]
+        for i in range(idx + 1, len(balance)):
+            if balance[i] * balance[idx] < 0:
+                balance[i] += balance[idx]
+                res = min(res, self.backtrack(balance, idx + 1, count + 1))
+                balance[i] -= balance[idx]
 
         return res
