1426908c3cad0db15abd13e7b97b16730a362f86
Carson <dragonchen66@gmail.com>
2023-07-03 10:49:46 -0400

    practice: daily challenge and revisit

--- python/1601.py
+++ python/1601.py
@@ -5,10 +5,10 @@ from itertools import combinations
 
 class Solution:
     def maximumRequests(self, n: int, requests: list[list[int]]) -> int:
-        for i in range(len(requests), 0, -1):
-            for comb in combinations(requests, i):
+        for i in range(len(requests) - 1, -1, -1):
+            for comb in combinations(requests, i + 1):
                 source = Counter(s for s, t in comb)
                 target = Counter(t for s, t in comb)
                 if source == target:
-                    return i
+                    return i + 1
         return 0
