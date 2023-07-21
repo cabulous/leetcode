660b3cc6533438425c29d7b7dbe5baacd807dc40
Carson <dragonchen66@gmail.com>
2023-07-21 09:28:03 -0400

    practice: daily challenge and revisit

---
+++ python/673.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findNumberOfLIS(self, nums: list[int]) -> int:
+        if len(nums) <= 1:
+            return len(nums)
+
+        dp = [1] * len(nums)
+        counts = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        counts[i] = counts[j]
+                    elif dp[j] + 1 == dp[i]:
+                        counts[i] += counts[j]
+
+        max_length = max(dp)
+        return sum(count for length, count in zip(dp, counts) if length == max_length)
