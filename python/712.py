cb20835727b4a6cf3959205e4c5794e9ff4efa7f
Carson <dragonchen66@gmail.com>
2023-07-31 09:16:10 -0400

    practice: daily challenge and revisit

---
+++ python/712.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+
+        dp = [0] * (len(s2) + 1)
+        for j in range(1, len(s2) + 1):
+            dp[j] = dp[j - 1] + ord(s2[j - 1])
+
+        for i in range(1, len(s1) + 1):
+            next_dp = [dp[0] + ord(s1[i - 1])]
+            for j in range(1, len(s2) + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    next_dp.append(dp[j - 1])
+                else:
+                    next_dp.append(min(
+                        dp[j] + ord(s1[i - 1]),
+                        next_dp[j - 1] + ord(s2[j - 1]),
+                    ))
+            dp = next_dp
+
+        return dp[-1]
