1663861093451f35e9eaef843f01c1ba22c89d33
Carson <dragonchen66@gmail.com>
2023-08-04 09:07:15 -0400

    practice: daily challenge and revisit

---
+++ python/139.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        dp = [True] + [False] * len(s)
+        max_len = max(map(len, wordDict))
+
+        for end in range(1, len(s) + 1):
+            for start in range(end - 1, max(-1, end - 1 - max_len), - 1):
+                if dp[start] and s[start:end] in wordDict:
+                    dp[end] = True
+                    break
+
+        return dp[-1]
