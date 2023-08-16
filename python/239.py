5d0f189278b02136c542b477a7596c156a59c2cc
Carson <dragonchen66@gmail.com>
2023-08-16 08:30:33 -0400

    practice: daily challenge and revisit

--- python/239.py
+++ python/239.py
@@ -1,67 +1,19 @@
-# deque
 from collections import deque
 
 
+# https://leetcode.com/problems/sliding-window-maximum/solutions/65901/9-lines-ruby-11-lines-python-o-n/
 class Solution:
-    def maxSlidingWindow(self, nums: [int], k: int) -> [int]:
-        n = len(nums)
-        if n * k == 0:
-            return []
-        if k == 1:
-            return nums
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        queue = deque()
+        res = []
 
-        def clean_deque(i):
-            if deq and deq[0] <= i - k:
-                deq.popleft()
-            while deq and nums[i] > nums[deq[-1]]:
-                deq.pop()
+        for i, num in enumerate(nums):
+            while queue and nums[queue[-1]] < num:
+                queue.pop()
+            queue.append(i)
+            if queue[0] == i - k:
+                queue.popleft()
+            if i >= k - 1:
+                res.append(nums[queue[0]])
 
-        deq = deque()
-        max_idx = 0
-        for i in range(k):
-            clean_deque(i)
-            deq.append(i)
-            if nums[i] > nums[max_idx]:
-                max_idx = i
-
-        output = [nums[max_idx]]
-
-        for i in range(k, n):
-            clean_deque(i)
-            deq.append(i)
-            output.append(nums[deq[0]])
-
-        return output
-
-
-# dp
-class Solution:
-    def maxSlidingWindow(self, nums: [int], k: int) -> [int]:
-        n = len(nums)
-        if n * k == 0:
-            return []
-        if k == 1:
-            return nums
-
-        left = [0] * n
-        left[0] = nums[0]
-        right = [0] * n
-        right[-1] = nums[-1]
-
-        for i in range(1, n):
-            if i % k == 0:
-                left[i] = nums[i]
-            else:
-                left[i] = max(left[i - 1], nums[i])
-            j = n - i - 1
-            if (j + 1) % k == 0:
-                right[j] = nums[j]
-            else:
-                right[j] = max(right[j + 1], nums[j])
-
-        output = []
-
-        for i in range(n - k + 1):
-            output.append(max(left[i + k - 1], right[i]))
-
-        return output
+        return res
--- python/81.py
+++ python/81.py
@@ -24,3 +24,29 @@ class Solution:
                 right -= 1
 
         return False
+
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> bool:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return True
+
+            if nums[left] < nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            elif nums[left] > nums[mid]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                left += 1
+
+        return False
