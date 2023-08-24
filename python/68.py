84c709a9ada95df3ed6e61dac3e4d2eafc999e9e
Carson <dragonchen66@gmail.com>
2023-08-24 09:10:38 -0400

    practice: daily challenge and revisit

--- python/68.py
+++ python/68.py
@@ -1,21 +1,18 @@
-from typing import List
-
-
 # https://leetcode.com/problems/text-justification/discuss/24891/Concise-python-solution-10-lines.
 class Solution:
-    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        letters_count = 0
-        curr_line = []
+    def fullJustify(self, words: list[str], maxWidth: int) -> list[str]:
+        count = 0
+        curr = []
         res = []
 
         for word in words:
-            if letters_count + len(curr_line) + len(word) > maxWidth:
-                for i in range(maxWidth - letters_count):
-                    curr_line[i % (len(curr_line) - 1 or 1)] += ' '
-                res.append(''.join(curr_line))
-                letters_count = 0
-                curr_line = []
-            letters_count += len(word)
-            curr_line.append(word)
+            if count + len(curr) + len(word) > maxWidth:
+                for i in range(maxWidth - count):
+                    curr[i % (len(curr) - 1 or 1)] += ' '
+                res.append(''.join(curr))
+                count = 0
+                curr = []
+            count += len(word)
+            curr.append(word)
 
-        return res + [' '.join(curr_line).ljust(maxWidth)]
+        return res + [' '.join(curr).ljust(maxWidth)]
