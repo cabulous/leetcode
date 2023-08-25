08ffde34228773c9c8f4e4786abdd90c5dca9ee1
Carson <dragonchen66@gmail.com>
2023-08-25 09:20:56 -0400

    practice: daily challenge and revisit

--- python/97.py
+++ python/97.py
@@ -8,16 +8,13 @@ class Solution:
 
         queue = deque([(0, 0)])
         seen = {(0, 0)}
-
         while queue:
             pt1, pt2 = queue.popleft()
             if pt1 + pt2 == len(s3):
                 return True
-
             if pt1 < len(s1) and s1[pt1] == s3[pt1 + pt2] and (pt1 + 1, pt2) not in seen:
                 seen.add((pt1 + 1, pt2))
                 queue.append((pt1 + 1, pt2))
-
             if pt2 < len(s2) and s2[pt2] == s3[pt1 + pt2] and (pt1, pt2 + 1) not in seen:
                 seen.add((pt1, pt2 + 1))
                 queue.append((pt1, pt2 + 1))
@@ -32,12 +29,15 @@ class Solution:
 
         dp = [True] * (len(s2) + 1)
 
-        for i in range(1, len(s2) + 1):
-            dp[i] = dp[i - 1] and s2[i - 1] == s3[i - 1]
+        for pt2 in range(1, len(s2) + 1):
+            dp[pt2] = dp[pt2 - 1] and s2[pt2 - 1] == s3[pt2 - 1]
 
-        for i in range(1, len(s1) + 1):
-            dp[0] = dp[0] and s1[i - 1] == s3[i - 1]
-            for j in range(1, len(s2) + 1):
-                dp[j] = (dp[j] and s1[i - 1] == s3[i - 1 + j]) or (dp[j - 1] and s2[j - 1] == s3[i - 1 + j])
+        for pt1 in range(1, len(s1) + 1):
+            dp[0] = dp[0] and s1[pt1 - 1] == s3[pt1 - 1]
+            for pt2 in range(1, len(s2) + 1):
+                dp[pt2] = (
+                        dp[pt2] and s1[pt1 - 1] == s3[pt1 - 1 + pt2]
+                        or dp[pt2 - 1] and s2[pt2 - 1] == s3[pt1 - 1 + pt2]
+                )
 
         return dp[-1]
