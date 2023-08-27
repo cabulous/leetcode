494eaafc7ce67bbbc2541d28eadef843036b82f8
Carson <dragonchen66@gmail.com>
2023-08-27 10:12:14 -0400

    practice: daily challenge and revisit

--- python/403.py
+++ python/403.py
@@ -1,19 +1,16 @@
-from typing import List
-
-
 # https://leetcode.com/problems/frog-jump/discuss/88800/Python-Documented-solution-that-is-easy-to-understand/93592
 class Solution:
-    def canCross(self, stones: List[int]) -> bool:
+    def canCross(self, stones: list[int]) -> bool:
         if stones[1] != 1:
             return False
 
-        d = {stone: set() for stone in stones}
-        d[1].add(1)
+        dp = {stone: set() for stone in stones}
+        dp[1].add(1)
 
         for stone in stones[:-1]:
-            for last_jump in d[stone]:
-                for jump in range(last_jump - 1, last_jump + 2):
-                    if jump > 0 and (stone + jump) in d:
-                        d[stone + jump].add(jump)
+            for prev_jump in dp[stone]:
+                for jump in range(prev_jump - 1, prev_jump + 2):
+                    if jump > 0 and stone + jump in dp:
+                        dp[stone + jump].add(jump)
 
-        return bool(d[stones[-1]])
+        return len(dp[stones[-1]]) > 0
