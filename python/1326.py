c89757d3f9a3183439db12f8f2755dff8039a950
Carson <dragonchen66@gmail.com>
2023-08-31 09:23:17 -0400

    practice: daily challenge and revisit

--- python/1326.py
+++ python/1326.py
@@ -1,15 +1,12 @@
-from typing import List
-
-
 # https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/discuss/484235/JavaC%2B%2BPython-Similar-to-LC1024
 class Solution:
-    def minTaps(self, n: int, ranges: List[int]) -> int:
+    def minTaps(self, n: int, ranges: list[int]) -> int:
         dp = [0] + [n + 2] * n
 
         for i, tap_range in enumerate(ranges):
-            left = max(i - tap_range, 0) + 1
-            right = min(i + tap_range, n) + 1
+            left = max(0, i - tap_range) + 1
+            right = min(n, i + tap_range) + 1
             for j in range(left, right):
-                dp[j] = min(dp[j], dp[max(i - tap_range, 0)] + 1)
+                dp[j] = min(dp[j], dp[max(0, i - tap_range)] + 1)
 
         return dp[-1] if dp[-1] < n + 2 else -1
--- python/646.py
+++ python/646.py
@@ -1,9 +1,9 @@
 class Solution:
     def findLongestChain(self, pairs: list[list[int]]) -> int:
-        prev = float('-inf')
+        prev_end = float('-inf')
         res = 0
-        for n1, n2 in sorted(pairs, key=lambda x: x[1]):
-            if prev < n1:
-                prev = n2
+        for start, end in sorted(pairs, key=lambda x: x[1]):
+            if prev_end < start:
+                prev_end = end
                 res += 1
         return res
