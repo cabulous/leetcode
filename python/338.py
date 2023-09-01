00662a97d5eca877dea61d3385f8da9ffc5d1b23
Carson <dragonchen66@gmail.com>
2023-09-01 08:22:14 -0400

    practice: daily challenge and revisit

--- python/338.py
+++ python/338.py
@@ -1,20 +1,6 @@
 class Solution:
-    def countBits(self, num):
-        res = []
-        for n in range(num + 1):
-            count = 0
-            while n != 0:
-                count += 1
-                n &= (n - 1)
-            res.append(count)
-        return res
-
-
-# DP: least significant bits
-# P(x)=P(x / 2) + (x mod 2)
-class Solution:
-    def countBits(self, num):
-        res = [0] * (num + 1)
-        for i in range(len(res)):
-            res[i] = res[i // 2] + (i % 2)
+    def countBits(self, n: int) -> list[int]:
+        res = [0]
+        for i in range(1, n + 1):
+            res.append(res[i >> 1] + i % 2)
         return res
