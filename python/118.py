8f7e96846db6655c3703e60e1450b50ee6fe836c
Carson <dragonchen66@gmail.com>
2023-09-08 08:29:28 -0400

    practice: daily challenge and revisit

--- python/118.py
+++ python/118.py
@@ -1,8 +1,5 @@
-from typing import List
-
-
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+    def generate(self, numRows: int) -> list[list[int]]:
         res = []
 
         for r in range(numRows):
