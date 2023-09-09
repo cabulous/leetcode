a835980fdd51906f272428a2b4b9707f7f5f27bd
Carson <dragonchen66@gmail.com>
2023-09-09 10:33:12 -0400

    practice: daily challenge and revisit

--- python/377.py
+++ python/377.py
@@ -1,32 +1,33 @@
-from typing import List
-from functools import lru_cache
-
-
 class Solution:
 
     def __init__(self):
+        self.memo = {}
         self.nums = []
 
-    def combinationSum4(self, nums: List[int], target: int) -> int:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
         self.nums = sorted(nums)
         return self.helper(target)
 
-    @lru_cache(None)
     def helper(self, target):
         if target == 0:
             return 1
 
+        if target in self.memo:
+            return self.memo[target]
+
         res = 0
         for num in self.nums:
             if target - num < 0:
-                return res
+                self.memo[target] = res
+                return self.memo[target]
             res += self.helper(target - num)
 
-        return res
+        self.memo[target] = res
+        return self.memo[target]
 
 
 class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
         dp = [1] + [0] * target
         nums.sort()
 
@@ -36,4 +37,4 @@ class Solution:
                     break
                 dp[comb_sum] += dp[comb_sum - num]
 
-        return dp[target]
+        return dp[-1]
