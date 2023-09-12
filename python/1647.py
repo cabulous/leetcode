40c30b604e0e9c6a24bb4b4797464092b479393b
Carson <dragonchen66@gmail.com>
2023-09-12 09:21:47 -0400

    practice: daily challenge and revisit

--- python/1647.py
+++ python/1647.py
@@ -4,14 +4,13 @@ from collections import Counter
 class Solution:
     def minDeletions(self, s: str) -> int:
         count = Counter(s)
-        freq = sorted(list(count.values()), reverse=True)
-
-        max_freq_allowed = len(s)
+        all_freq = sorted(list(count.values()), reverse=True)
+        max_freq = len(s)
         res = 0
 
-        for num in freq:
-            if num > max_freq_allowed:
-                res += num - max_freq_allowed
-            max_freq_allowed = max(0, min(max_freq_allowed, num) - 1)
+        for freq in all_freq:
+            if freq > max_freq:
+                res += freq - max_freq
+            max_freq = max(0, min(max_freq, freq) - 1)
 
         return res
