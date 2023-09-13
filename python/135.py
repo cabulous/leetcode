0afb1e7579a4c7427d94aeb3f3ee2ef0ad5bcd46
Carson <dragonchen66@gmail.com>
2023-09-13 09:12:56 -0400

    practice: daily challenge and revisit

--- python/135.py
+++ python/135.py
@@ -1,13 +1,10 @@
-from typing import List
-
-
 # https://leetcode.com/problems/candy/discuss/42881/Python-two-pass-solution-(left-to-right-then-right-to-left).
 class Solution:
-    def candy(self, ratings: List[int]) -> int:
+    def candy(self, ratings: list[int]) -> int:
         res = [1] * len(ratings)
 
         for i in range(1, len(ratings)):
-            if ratings[i - 1] < ratings[i]:
+            if ratings[i] > ratings[i - 1]:
                 res[i] = res[i - 1] + 1
 
         for i in range(len(ratings) - 2, -1, -1):
