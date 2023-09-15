21f720b52fee9a6f2d8c537259a24a0c178e7a70
Carson <dragonchen66@gmail.com>
2023-09-15 08:43:50 -0400

    practice: daily challenge and revisit

--- python/1584.py
+++ python/1584.py
@@ -1,36 +1,31 @@
-import math
-from typing import List
-
-
 class Solution:
-    def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        n = len(points)
-        edges_used = 0
+    def minCostConnectPoints(self, points: list[list[int]]) -> int:
+        visited = [False] * len(points)
+        min_dist = [float('inf')] * len(points)
+        min_dist[0] = 0
+        used = 0
         res = 0
 
-        visited = [False] * n
-        min_dist = [math.inf] * n
-        min_dist[0] = 0
-
-        while edges_used < n:
-            curr_min_dist = math.inf
+        while used < len(points):
+            curr_min_dist = float('inf')
             curr_node = -1
-
-            for node in range(n):
+            for node in range(len(points)):
                 if not visited[node] and min_dist[node] < curr_min_dist:
                     curr_min_dist = min_dist[node]
                     curr_node = node
 
+            used += 1
             res += curr_min_dist
-            edges_used += 1
             visited[curr_node] = True
 
-            for next_node in range(n):
-                dist = self.get_manhattan_dist(points[curr_node], points[next_node])
-                if not visited[next_node] and dist < min_dist[next_node]:
-                    min_dist[next_node] = dist
+            for next_node in range(len(points)):
+                curr_dist = self.get_dist(points[curr_node], points[next_node])
+                if not visited[next_node] and curr_dist < min_dist[next_node]:
+                    min_dist[next_node] = curr_dist
 
         return res
 
-    def get_manhattan_dist(self, x, y):
-        return abs(x[0] - y[0]) + abs(x[1] - y[1])
+    def get_dist(self, p1, p2):
+        x1, y1 = p1
+        x2, y2 = p2
+        return abs(x1 - x2) + abs(y1 - y2)
