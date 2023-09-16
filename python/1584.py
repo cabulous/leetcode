881df669c27e55ce7d88306700229e130d2058bd
Carson <dragonchen66@gmail.com>
2023-09-16 10:20:34 -0400

    practice: daily challenge and revisit

--- python/1584.py
+++ python/1584.py
@@ -1,6 +1,3 @@
-import heapq
-
-
 class Solution:
     def minCostConnectPoints(self, points: list[list[int]]) -> int:
         visited = [False] * len(points)
@@ -21,10 +18,10 @@ class Solution:
             used += 1
             res += curr_min_dist
 
+            x1, y1 = points[curr_node]
             for next_node in range(len(points)):
                 if visited[next_node]:
                     continue
-                x1, y1 = points[curr_node]
                 x2, y2 = points[next_node]
                 next_dist = abs(x1 - x2) + abs(y1 - y2)
                 if next_dist < min_dist[next_node]:
