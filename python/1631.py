99515129f54c30a1e993bb4f496148fa58fc84eb
Carson <dragonchen66@gmail.com>
2023-09-16 09:50:36 -0400

    practice: daily challenge and revisit

--- python/1631.py
+++ python/1631.py
@@ -1,23 +1,16 @@
 import heapq
-import math
-from typing import List
 
 
 # Dijkstra's Algorithm
 # https://leetcode.com/problems/path-with-minimum-effort/discuss/909017/JavaPython-Dijikstra-Clean-and-Concise-O(MNlogMN)
 class Solution:
-
-    def __init__(self):
-        self.rows = 0
-        self.cols = 0
-
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        self.rows = len(heights)
-        self.cols = len(heights[0])
-
-        dist_min = [[math.inf] * self.cols for _ in range(self.rows)]
+    def minimumEffortPath(self, heights: list[list[int]]) -> int:
+        rows = len(heights)
+        cols = len(heights[0])
+        dist_min = [[float('inf')] * cols for _ in range(rows)]
         queue = [(0, 0, 0)]
-        target = (self.rows - 1, self.cols - 1)
+        target = (rows - 1, cols - 1)
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
         while queue:
             dist, row, col = heapq.heappop(queue)
@@ -25,13 +18,12 @@ class Solution:
                 return dist
             if dist > dist_min[row][col]:
                 continue
-            for next_row, next_col in self.get_next_cell(row, col):
-                next_dist = max(dist, abs(heights[next_row][next_col] - heights[row][col]))
-                if next_dist < dist_min[next_row][next_col]:
-                    dist_min[next_row][next_col] = next_dist
-                    heapq.heappush(queue, (next_dist, next_row, next_col))
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    nd = max(dist, abs(heights[nr][nc] - heights[row][col]))
+                    if nd < dist_min[nr][nc]:
+                        dist_min[nr][nc] = nd
+                        heapq.heappush(queue, (nd, nr, nc))
 
-    def get_next_cell(self, row, col):
-        for next_row, next_col in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
-            if 0 <= next_row < self.rows and 0 <= next_col < self.cols:
-                yield next_row, next_col
+        return -1
