cdd1069069390776682b17b7f7c680de9efa7fce
Carson <dragonchen66@gmail.com>
2023-09-18 08:59:18 -0400

    practice: daily challenge and revisit

--- python/1337.py
+++ python/1337.py
@@ -1,28 +1,5 @@
-from typing import List
-
-
 class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        res = []
-
-        for col in range(len(mat[0])):
-            for row in range(len(mat)):
-                if len(res) == k:
-                    return res
-                if mat[row][col] == 0 and (col == 0 or mat[row][col - 1] == 1):
-                    res.append(row)
-
-        for row in range(len(mat)):
-            if len(res) == k:
-                return res
-            if row not in res:
-                res.append(row)
-
-        return res
-
-
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+    def kWeakestRows(self, mat: list[list[int]], k: int) -> list[int]:
         res = [(sum(row), i) for i, row in enumerate(mat)]
         res.sort()
-        return [i for _, i in res[:k]]
+        return [i for __, i in res[:k]]
--- python/847.py
+++ python/847.py
@@ -8,20 +8,20 @@ class Solution:
         visited_state = [{masks[i]} for i in range(len(graph))]
         all_visited = (1 << len(graph)) - 1
         queue = deque([(i, masks[i]) for i in range(len(graph))])
-        steps = 0
+        res = 0
 
         while queue:
-            for _ in range(len(queue)):
+            for __ in range(len(queue)):
                 node, visited = queue.popleft()
                 if visited == all_visited:
-                    return steps
+                    return res
                 for next_node in graph[node]:
                     next_visited = visited | masks[next_node]
                     if next_visited == all_visited:
-                        return steps + 1
+                        return res + 1
                     if next_visited not in visited_state[next_node]:
                         visited_state[next_node].add(next_visited)
                         queue.append((next_node, next_visited))
-            steps += 1
+            res += 1
 
         return -1
