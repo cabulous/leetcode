ad9765a3012790e188e5c94c83f5ff5b5e60db2b
Carson <dragonchen66@gmail.com>
2023-09-19 08:51:44 -0400

    practice: daily challenge and revisit

--- python/287.py
+++ python/287.py
@@ -1,27 +1,9 @@
-from typing import List
-
-
 # https://leetcode.com/problems/find-the-duplicate-number/discuss/704693/Python-2-solutions%3A-Linked-List-Cycle-O(n)-and-BS-O(n-log-n)-explained
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = nums[0], nums[0]
-        while True:
-            slow, fast = nums[slow], nums[nums[fast]]
-            if slow == fast:
-                break
-
-        slow = nums[0]
-        while slow != fast:
-            slow, fast = nums[slow], nums[fast]
-
-        return slow
-
-
-# https://leetcode.com/problems/find-the-duplicate-number/discuss/704693/Python-2-solutions:-Linked-List-Cycle-O(n)-and-BS-O(n-log-n)-explained/593157
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        bits = 0
+    def findDuplicate(self, nums: list[int]) -> int:
+        seen = set()
         for num in nums:
-            if bits & (1 << num) != 0:
+            if num in seen:
                 return num
-            bits |= (1 << num)
+            seen.add(num)
+        return -1
